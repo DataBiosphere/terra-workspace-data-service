d355736b42b677320191bd3b179df293a039713f
Jordan Safer <jordansafer2@gmail.com>
2022-06-16 16:36:48 -0400

    rename and error message fixes

--- scripts/render-config.sh
+++ scripts/render-config.sh
@@ -1,5 +1,5 @@
 # Simple script for pulling local vault credentials. All arguments are optional.
-# Usage is: ./scripts/write-config.sh [env] [execution env] [vaulttoken]
+# Usage is: ./scripts/render-config.sh [env] [execution env] [vaulttoken]
 
 ENV=${1:-dev}
 EXECUTION_ENV=${2:-docker}
@@ -7,6 +7,15 @@ VAULT_TOKEN=${3:-$(cat "$HOME"/.vault-token)}
 
 VAULT_ADDR="https://clotho.broadinstitute.org:8200"
 WDS_VAULT_PATH="secret/dsde/$ENV/workspacedata"
+
+case $EXECUTION_ENV in 
+    local | docker) ;;
+    *)
+        echo "Invalid input: Execution env must be docker or local.\n" \
+            "For example: /scripts/render-config.sh <env> local"
+        exit 0
+        ;;
+esac
 
 function runVaultInEnv {
     case $EXECUTION_ENV in
@@ -17,9 +26,6 @@ function runVaultInEnv {
         docker)
             docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.3 \
                 vault read -format=json "$1"
-            ;;
-        *)
-            echo "Invalid input: Execution env must be docker or local. For example: $0 <env> <token> local"
             ;;
     esac
 }
