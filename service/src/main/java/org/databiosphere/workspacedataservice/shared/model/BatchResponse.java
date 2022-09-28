84728fad784aaf0e0d7fa4ea5aba8a83783637c6
Drew Hite <drew@drewhite.org>
2022-09-28 11:05:51 -0400

    sonar changes

--- service/src/main/java/org/databiosphere/workspacedataservice/shared/model/BatchResponse.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/shared/model/BatchResponse.java
@@ -1,7 +1,5 @@
 package org.databiosphere.workspacedataservice.shared.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public record BatchResponse(int recordsModified, String success) {
 
 }
--- service/src/main/java/org/databiosphere/workspacedataservice/shared/model/Record.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/shared/model/Record.java
@@ -1,6 +1,5 @@
 package org.databiosphere.workspacedataservice.shared.model;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
