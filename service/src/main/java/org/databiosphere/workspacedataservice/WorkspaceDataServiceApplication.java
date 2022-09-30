2f45f82ec35f5898a3c9c267b9161eca48972d73
David An <davidan@broadinstitute.org>
2022-09-30 14:45:28 -0400

    enable CORS

--- service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WorkspaceDataServiceApplication {
@@ -14,6 +16,18 @@ public class WorkspaceDataServiceApplication {
 	public ObjectMapper objectMapper() {
 		return JsonMapper.builder().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS).build();
 	}
+
+	// CORS: allow Ajax requests from anywhere for all endpoints.
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkspaceDataServiceApplication.class, args);
 	}
