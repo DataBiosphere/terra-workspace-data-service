5f0e6b2304804c1687113dc4831038550419e78a
David An <davidan@broadinstitute.org>
2022-10-07 17:26:50 -0400

    code comments

--- service/src/main/java/org/databiosphere/workspacedataservice/service/MDCFilter.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/MDCFilter.java
@@ -19,18 +19,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MDCFilter implements Filter {
 
+    // where the unique request id is stored in the MDC context
     static final String MDC_KEY = "requestId";
+    // the response header containing the unique request id
     public static final String RESPONSE_HEADER = "x-b3-traceid";
-
-    // add other headers here, such as: x-vcap-request-id, X─B3─ParentSpanId, X─B3─SpanId, X─B3─Sampled
+    // the list of request headers, in order, we will search for an incoming unique request id
+    // we could add other headers here, such as: x-vcap-request-id, X─B3─ParentSpanId, X─B3─SpanId, X─B3─Sampled
     static final List<String> INCOMING_HEADERS = Arrays.asList("x-b3-traceid", "x-request-id", "trace-id", "x-amzn-trace-id");
 
-
-    // TODO: I think this has a problem with streaming responses
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         String uniqueId = null;
-        // look for a unique requestid in the request
+        // look for a non-blank unique request id in the incoming request headers
         if (servletRequest instanceof HttpServletRequest httpServletRequest) {
             for (String hdr : INCOMING_HEADERS) {
                 String foundIncoming = httpServletRequest.getHeader(hdr);
@@ -52,6 +52,7 @@ public class MDCFilter implements Filter {
         log.trace("Request IP address is {}", servletRequest.getRemoteAddr());
         log.trace("Request content type is {}", servletRequest.getContentType());
 
+        // add id to the response
         if (servletResponse instanceof HttpServletResponse httpServletResponse) {
             httpServletResponse.setHeader(RESPONSE_HEADER, uniqueId);
             log.trace("Response header is set with uuid {}", uniqueId);
