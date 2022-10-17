5c5275cb9f54828488b9a818050e7e2f77aacdf8
Drew Hite <drew@drewhite.org>
2022-10-17 15:17:08 -0400

    sonar

--- service/src/main/java/org/databiosphere/workspacedataservice/service/model/DataTypeMapping.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/model/DataTypeMapping.java
@@ -2,7 +2,6 @@ package org.databiosphere.workspacedataservice.service.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
