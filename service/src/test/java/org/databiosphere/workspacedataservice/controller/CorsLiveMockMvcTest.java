712a4a9a5f1af5d74256af235d73fab2d19a1f4c
David An <davidan@broadinstitute.org>
2022-11-17 09:44:57 -0500

    disable CORS headers for non-local development

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/CorsLiveMockMvcTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/CorsLiveMockMvcTest.java
@@ -10,40 +10,45 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * This class tests CORS behavior for live deployments. WDS in live deployments sits behind
+ * Azure Relay, and Relay handles CORS headers. Therefore, WDS should *not* reply with CORS
+ * headers, as it will cause a conflict.
+ *
+ * See also CorsLocalMockMvcTest for testing CORS behavior in the "local" Spring profile
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
-class CorsMockMvcTest {
+class CorsLiveMockMvcTest {
 
 	@Autowired
 	MockMvc mockMvc;
 
 	private static final String versionId = "v0.2";
 
-	@ParameterizedTest(name = "CORS response headers for {0} should be correct")
+	@ParameterizedTest(name = "CORS response headers for non-local profile to {0} should be correct")
 	@ValueSource(strings = {"/instances/{version}/{instanceId}", "/{instanceid}/types/{v}",
 			"/{instanceid}/tsv/{v}/{type}", "/{instanceid}/search/{v}/{type}", "/{instanceid}/records/{v}/{type}/{id}"})
 	void testCorsResponseHeaders(String urlTemplate) throws Exception {
 		UUID uuid = UUID.randomUUID();
 
+		// for the non-"local" profile, OPTIONS requests should return 403 and not include
+		// Access-Control-Allow-Origin or Access-Control-Allow-Methods headers.
+
 		MvcResult mvcResult = mockMvc.perform(options(urlTemplate, versionId, uuid, "sometype", "someid")
 				// Access-Control-Request-Method and Origin required to trigger CORS response
 				.header("Access-Control-Request-Method", "POST").header("Origin", "http://www.example.com"))
-				.andExpect(status().isOk()).andReturn();
+				.andExpect(status().isForbidden()).andReturn();
 
 		String actualOrigin = mvcResult.getResponse().getHeader("Access-Control-Allow-Origin");
-		assertNotNull(actualOrigin, "Access-Control-Allow-Origin not present in CORS response for " + urlTemplate);
-		assertEquals("*", actualOrigin, "bad Access-Control-Allow-Origin in CORS response for " + urlTemplate);
+		assertNull(actualOrigin, "Access-Control-Allow-Origin should not be present in CORS response for " + urlTemplate);
 
 		String actualMethods = mvcResult.getResponse().getHeader("Access-Control-Allow-Methods");
-		assertNotNull(actualMethods, "Access-Control-Allow-Methods not present in CORS response for " + urlTemplate);
-		assertEquals("DELETE,GET,HEAD,PATCH,POST,PUT", actualMethods,
-				"bad Access-Control-Allow-Methods in CORS response for " + urlTemplate);
-
+		assertNull(actualMethods, "Access-Control-Allow-Methods should not be present in CORS response for " + urlTemplate);
 	}
 
 }
