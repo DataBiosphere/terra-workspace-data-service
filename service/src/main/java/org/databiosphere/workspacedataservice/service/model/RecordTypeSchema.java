2b9b88e45cdd2c6180999a719cf992d99d7757b0
Drew Hite <drew@drewhite.org>
2022-11-29 10:15:58 -0500

    switch field name from uniqueRowIdentifier to primaryKey

--- service/src/main/java/org/databiosphere/workspacedataservice/service/model/RecordTypeSchema.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/model/RecordTypeSchema.java
@@ -4,5 +4,5 @@ import org.databiosphere.workspacedataservice.shared.model.RecordType;
 
 import java.util.List;
 
-public record RecordTypeSchema(RecordType name, List<AttributeSchema> attributes, int count, String uniqueRowIdentifier) {
+public record RecordTypeSchema(RecordType name, List<AttributeSchema> attributes, int count, String primaryKey) {
 }
