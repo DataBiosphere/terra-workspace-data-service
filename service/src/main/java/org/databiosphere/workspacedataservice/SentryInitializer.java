c3369baffd714511b1dacdb3e6a50129a1d7fec3
Bria Morgan <bmorgan@broadinstitute.org>
2023-01-20 15:29:09 -0500

    change servername to resource group

--- service/src/main/java/org/databiosphere/workspacedataservice/SentryInitializer.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/SentryInitializer.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySources({@PropertySource("classpath:git.properties"), @PropertySource("classpath:application.properties")})
+@PropertySource("classpath:git.properties")
+@PropertySource("classpath:application.properties")
 public class SentryInitializer  {
 
 	@Value("${sentry.dsn}")
@@ -29,7 +29,7 @@ public class SentryInitializer  {
 
 	@Bean
 	public SmartInitializingSingleton initialize() {
-		return () -> {
+		return () ->
         Sentry.init(options -> {
 				options.setDsn(dsn);
 				options.setEnvironment(env);
@@ -37,6 +37,5 @@ public class SentryInitializer  {
 				options.setRelease(release);
 				options.setTag("workspaceId", workspaceId);
 			});
-		};
 	}
 }
