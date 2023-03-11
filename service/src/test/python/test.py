e386d7bf2ae51e7ff59a2656884467d19832de33
yuliadub <yulia.dubinina@gmail.com>
2023-03-11 12:07:01 -0800

    add test file

---
+++ service/src/test/python/test.py
@@ -0,0 +1,11 @@
+# test.py
+
+from unittest import TestCase
+import  wds_client
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
