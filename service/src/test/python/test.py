70cc44689c0a42b727a65bc5189442d31fae7bb7
Yulia Dubinina <yuliadub@users.noreply.github.com>
2023-03-15 16:52:40 -0700

    do tests only run if def starts with test?

--- service/src/test/python/test.py
+++ service/src/test/python/test.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 import  wds_client
 
 class TryTesting(TestCase):
-    def check_version(self):
+    def test_check_version(self):
         api_client = wds_client.ApiClient()
         api_client.configuration.host = 'http://localhost:8080'
         generalInfo_instance = wds_client.GeneralWDSInformationApi(api_client)
