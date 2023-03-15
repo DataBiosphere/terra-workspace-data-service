afea6aed5cef775c195632f5756d6069f5a13ca3
Yulia Dubinina <yuliadub@users.noreply.github.com>
2023-03-15 16:49:15 -0700

    adding real test cases

--- service/src/test/python/test.py
+++ service/src/test/python/test.py
@@ -4,8 +4,18 @@ from unittest import TestCase
 import  wds_client
 
 class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_always_fails(self):
-        self.assertTrue(False)
+    def check_version(self):
+        api_client = wds_client.ApiClient()
+        api_client.configuration.host = 'http://localhost:8080'
+        generalInfo_instance = wds_client.GeneralWDSInformationApi(api_client)
+        response = generalInfo_instance.version_get()
+        print(response.build.version)
+        self.assertIsNotNone(response.build.version)
+ 
+    def check_status(self):
+        api_client = wds_client.ApiClient()
+        api_client.configuration.host = 'http://localhost:8080'
+        generalInfo_instance = wds_client.GeneralWDSInformationApi(api_client)
+        response = generalInfo_instance.status_get()
+        print(response.status)
+        self.assertEqual(response.status, "UP")
