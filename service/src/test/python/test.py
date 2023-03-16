f3fac3cb94afbdb22a19b8c75b1c66f742b2eaf3
Yulia Dubinina <yuliadub@users.noreply.github.com>
2023-03-16 11:10:19 -0700

    add one more test

--- service/src/test/python/test.py
+++ service/src/test/python/test.py
@@ -12,7 +12,7 @@ class TryTesting(TestCase):
         print(response.build.version)
         self.assertIsNotNone(response.build.version)
  
-    def check_status(self):
+    def test_check_status(self):
         api_client = wds_client.ApiClient()
         api_client.configuration.host = 'http://localhost:8080'
         generalInfo_instance = wds_client.GeneralWDSInformationApi(api_client)
