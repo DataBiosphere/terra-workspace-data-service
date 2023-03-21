a4cf66124cd4782ad65adc955c8e7f7adb482307
David An <davidan@broadinstitute.org>
2023-03-21 17:08:47 -0400

    unit test for 'cached plan must not change result type' error

---
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/ConcurrentDataTypeChangesTest.java
@@ -0,0 +1,122 @@
+package org.databiosphere.workspacedataservice.controller;
+
+import org.databiosphere.workspacedataservice.shared.model.RecordAttributes;
+import org.databiosphere.workspacedataservice.shared.model.RecordRequest;
+import org.databiosphere.workspacedataservice.shared.model.RecordType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles(profiles = "mock-sam")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ConcurrentDataTypeChangesTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private static HttpHeaders headers;
+    private static UUID instanceId;
+
+    private static final String recordId = "concurrent-changes";
+    private static final RecordType recordType = RecordType.valueOf("concurrency");
+    private static final String versionId = "v0.2";
+
+    @BeforeEach
+    void beforeEach() {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        instanceId = UUID.randomUUID();
+        ResponseEntity<String> response = restTemplate.exchange("/instances/{v}/{instanceid}", HttpMethod.POST,
+                new HttpEntity<>("", headers), String.class,
+                versionId, instanceId);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @AfterEach
+    void afterEach() {
+        ResponseEntity<String> response = restTemplate.exchange("/instances/{v}/{instanceid}", HttpMethod.DELETE,
+                new HttpEntity<>("", headers), String.class,
+                versionId, instanceId);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void concurrentWrites() {
+        // create the initial record, with no attributes
+        RecordRequest recordRequest = new RecordRequest(RecordAttributes.empty());
+        ResponseEntity<String> response = restTemplate.exchange("/{instanceid}/records/{v}/{type}/{id}", HttpMethod.PUT,
+                new HttpEntity<>(recordRequest, headers), String.class,
+                instanceId, versionId, recordType.getName(), recordId);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        int numIterations = 50;
+
+		/* for each iteration in numIterations,
+			create two API requests. The first API request is a GET to read the record,
+			the second API request is a PUT to add a new attribute to the record.
+			Under concurrent load, this is likely to generate an
+			"ERROR: cached plan must not change result type" exception.
+			Wrap these requests in CompletableFutures, so we can execute them in parallel.
+		 */
+        Stream<CompletableFuture<ResponseEntity<String>>> futures =
+                IntStream.rangeClosed(1, numIterations).boxed()
+                        .flatMap(i -> {
+                            CompletableFuture<ResponseEntity<String>> writeRequest =
+                                    CompletableFuture.supplyAsync(() -> writeOrThrow(i));
+                            CompletableFuture<ResponseEntity<String>> readRequest =
+                                    CompletableFuture.supplyAsync(() -> readOrThrow(i));
+                            return Stream.of(readRequest, writeRequest);
+                        });
+
+        // execute all the requests in parallel (as much parallelism as we can).
+        // we don't care about the responses, we only care if this throws errors.
+        CompletableFuture<Void> combinedFuture
+                = CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new));
+
+        assertDoesNotThrow(() -> combinedFuture.get());
+
+    }
+
+    private ResponseEntity<String> writeOrThrow(int iteration) {
+        // add a new attribute for each PUT request
+        RecordAttributes attrs = new RecordAttributes(Map.of("attr-" + iteration, "value-" + iteration));
+        RecordRequest req = new RecordRequest(attrs);
+
+        ResponseEntity<String> response = restTemplate.exchange("/{instanceid}/records/{v}/{type}/{id}", HttpMethod.PUT,
+                new HttpEntity<>(req, headers), String.class,
+                instanceId, versionId, recordType.getName(), recordId);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new RuntimeException("Exception on write iteration " + iteration + " with response body: " + response.getBody());
+        }
+        return response;
+    }
+
+    private ResponseEntity<String> readOrThrow(int iteration) {
+        ResponseEntity<String> response = restTemplate.exchange("/{instanceid}/records/{v}/{type}/{id}", HttpMethod.GET,
+                new HttpEntity<>(headers), String.class,
+                instanceId, versionId, recordType.getName(), recordId);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new RuntimeException("Exception on read iteration " + iteration + " with response body: " + response.getBody());
+        }
+        return response;
+    }
+
+
+}
