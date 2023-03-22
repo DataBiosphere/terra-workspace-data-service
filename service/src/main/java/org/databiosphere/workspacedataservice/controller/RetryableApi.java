740c4e3046964e2e0d7bc3cbab57fb8d44d8a7e2
David An <davidan@broadinstitute.org>
2023-03-22 16:40:28 -0400

    SQLException only, shorter initial retry

--- service/src/main/java/org/databiosphere/workspacedataservice/controller/RetryableApi.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/controller/RetryableApi.java
@@ -7,13 +7,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.sql.SQLException;
 
 /**
- * Annotation to indicate the the entire implementation of a WDS API can be retried.
+ * Annotation to indicate that the entire implementation of a WDS API can be retried,
+ * if it encounters a SQLException.
+ * <p>
  * The API must be idempotent. This implies that if the API performs any database writes,
  * such writes are encapsulated in a transaction and the transaction is rolled back
  * on an exception.
- *
+ * <p>
  * Some database errors are not retry-able via the lower-level retries
  * in @ReadTransaction and @WriteTransaction. This higher-level @RetryableApi
  * annotation allows retries to occur at the whole-API level.
@@ -22,5 +25,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Retryable(maxAttemptsExpression = "${api.retry.maxAttempts}",
         backoff = @Backoff(delayExpression = "${api.retry.backoff.delay}",
-                multiplierExpression = "${api.retry.backoff.multiplier}"))
+                multiplierExpression = "${api.retry.backoff.multiplier}"),
+        include = { SQLException.class })
 public @interface RetryableApi {}
