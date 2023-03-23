96bbdc79b0192e4b71ab47e330796cce16bff8a2
David An <davidan@broadinstitute.org>
2023-03-23 15:50:51 -0400

    retries for Sam

--- service/src/test/java/org/databiosphere/workspacedataservice/sam/HttpSamClientSupportTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/sam/HttpSamClientSupportTest.java
@@ -8,16 +8,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.databiosphere.workspacedataservice.sam.HttpSamClientSupport.*;
 
 /**
  * Tests for @see HttpSamClientSupport
  */
 @SpringBootTest(classes = SamConfig.class)
-class HttpSamClientSupportTest extends HttpSamClientSupport {
+@EnableRetry
+class HttpSamClientSupportTest {
+
+    @Autowired HttpSamClientSupport httpSamClientSupport;
 
     @DisplayName("When Sam throws an ApiException with standard http status code 401, HttpSamClientSupport should throw AuthenticationException")
     @Test
@@ -30,10 +39,10 @@ class HttpSamClientSupportTest extends HttpSamClientSupport {
             throw new ApiException(samCode, "");
         };
         assertThrows(AuthenticationException.class,
-                () -> withSamErrorHandling(samFunction, "AuthenticationException"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(samFunction, "AuthenticationException"),
                 "samFunction should throw AuthenticationException");
         assertThrows(AuthenticationException.class,
-                () -> withSamErrorHandling(voidSamFunction, "AuthenticationException"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(voidSamFunction, "AuthenticationException"),
                 "voidSamFunction should throw AuthenticationException");
     }
 
@@ -48,10 +57,10 @@ class HttpSamClientSupportTest extends HttpSamClientSupport {
             throw new ApiException(samCode, "");
         };
         assertThrows(AuthorizationException.class,
-                () -> withSamErrorHandling(samFunction, "AuthorizationException"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(samFunction, "AuthorizationException"),
                 "samFunction should throw AuthorizationException");
         assertThrows(AuthorizationException.class,
-                () -> withSamErrorHandling(voidSamFunction, "AuthorizationException"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(voidSamFunction, "AuthorizationException"),
                 "voidSamFunction should throw AuthorizationException");
     }
 
@@ -108,9 +117,67 @@ class HttpSamClientSupportTest extends HttpSamClientSupport {
         expectSamExceptionWithStatusCode(samCode, voidSamFunction);
     }
 
+
+    @ParameterizedTest(name = "withSamErrorHandling will retry on status code {0}")
+    @ValueSource(ints = {0, 500, 502, 503, 504})
+    void retryableExceptions(int samCode) {
+        AtomicInteger counter = new AtomicInteger(0);
+
+        HttpSamClientSupport.SamFunction<Boolean> samFunction = () -> {
+            counter.incrementAndGet();
+            throw new ApiException(samCode, "");
+        };
+
+        // this test doesn't care what exception is thrown; other tests verify that
+        assertThrows(Exception.class, () -> httpSamClientSupport.withRetryAndErrorHandling(samFunction, "retryableExceptions"));
+
+        // this test does care how many times the samFunction was invoked
+        assertEquals(5, counter.get(), "SamFunction should have retried");
+
+        // reset counter
+        counter.set(0);
+
+        HttpSamClientSupport.VoidSamFunction voidSamFunction = () -> {
+            counter.incrementAndGet();
+            throw new ApiException(samCode, "");
+        };
+        // this test doesn't care what exception is thrown; other tests verify that
+        assertThrows(Exception.class, () -> httpSamClientSupport.withRetryAndErrorHandling(voidSamFunction, "retryableExceptions"));
+        // this test does care how many times the samFunction was invoked
+        assertEquals(5, counter.get(), "VoidSamFunction should have retried");
+    }
+
+    @ParameterizedTest(name = "withSamErrorHandling will not retry on status code {0}")
+    @ValueSource(ints = {400, 401, 403, 404, 409, 501})
+    void nonRetryableExceptions(int samCode) {
+        AtomicInteger counter = new AtomicInteger(0);
+
+        HttpSamClientSupport.SamFunction<Boolean> samFunction = () -> {
+            counter.incrementAndGet();
+            throw new ApiException(samCode, "");
+        };
+
+        // this test doesn't care what exception is thrown; other tests verify that
+        assertThrows(Exception.class, () -> httpSamClientSupport.withRetryAndErrorHandling(samFunction, "retryableExceptions"));
+        // this test does care how many times the samFunction was invoked
+        assertEquals(1, counter.get(), "SamFunction should not have retried");
+
+        // reset counter
+        counter.set(0);
+
+        HttpSamClientSupport.VoidSamFunction voidSamFunction = () -> {
+            counter.incrementAndGet();
+            throw new ApiException(samCode, "");
+        };
+        // this test doesn't care what exception is thrown; other tests verify that
+        assertThrows(Exception.class, () -> httpSamClientSupport.withRetryAndErrorHandling(voidSamFunction, "retryableExceptions"));
+        // this test does care how many times the samFunction was invoked
+        assertEquals(1, counter.get(), "VoidSamFunction should not have retried");
+    }
+
     private void expectSamExceptionWithStatusCode(int expectedStatusCode, SamFunction<?> samFunction) {
         SamException actual = assertThrows(SamException.class,
-                () -> withSamErrorHandling(samFunction, "samFunction-unittest"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(samFunction, "samFunction-unittest"),
                 "samFunction should throw SamException");
 
         assertEquals(expectedStatusCode, actual.getRawStatusCode(), "samFunction: Incorrect status code in SamException");
@@ -118,7 +185,7 @@ class HttpSamClientSupportTest extends HttpSamClientSupport {
 
     private void expectSamExceptionWithStatusCode(int expectedStatusCode, VoidSamFunction voidSamFunction) {
         SamException actual = assertThrows(SamException.class,
-                () -> withSamErrorHandling(voidSamFunction, "samFunction-unittest"),
+                () -> httpSamClientSupport.withRetryAndErrorHandling(voidSamFunction, "samFunction-unittest"),
                 "voidSamFunction should throw SamException");
 
         assertEquals(expectedStatusCode, actual.getRawStatusCode(), "voidSamFunction: Incorrect status code in SamException");
