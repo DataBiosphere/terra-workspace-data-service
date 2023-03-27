2de39eeeef16c38e60e3e2a7def78291227f900a
David An <davidan@broadinstitute.org>
2023-03-27 11:04:49 -0400

    AJ-911: 'cached plan must not change result type' error (#194)

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/retry/RetryLoggingListener.java
@@ -0,0 +1,20 @@
+package org.databiosphere.workspacedataservice.retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.listener.RetryListenerSupport;
+import org.springframework.stereotype.Component;
+
+@Component("retryLoggingListener")
+public class RetryLoggingListener extends RetryListenerSupport {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+        logger.warn("Retryable method {} threw {}th exception {}",
+                context.getAttribute("context.name"), context.getRetryCount(), throwable.toString());
+    }
+}
