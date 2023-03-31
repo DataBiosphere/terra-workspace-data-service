8c54e2764d310df6dd5d7b3a930f4c3667f7c226
github-action-benchmark <github@users.noreply.github.com>
2023-03-31 14:49:13 +0000

    add Benchmark (jmh) benchmark result for 434ceca221a2b3df713a9bdda2c38870aef9a812

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680274152068,
+  "lastUpdate": 1680274153978,
   "repoUrl": "https://github.com/DataBiosphere/terra-workspace-data-service",
   "entries": {
     "Benchmark": [
@@ -50,6 +50,32 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.databiosphere.workspacedataservice.controller.ApiBenchmark.upsertRecord",
             "value": 380.6100763942394,
+            "unit": "ops/s",
+            "extra": "iterations: 2\nforks: 2\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DataBiosphere",
+            "username": "DataBiosphere"
+          },
+          "committer": {
+            "name": "DataBiosphere",
+            "username": "DataBiosphere"
+          },
+          "id": "434ceca221a2b3df713a9bdda2c38870aef9a812",
+          "message": "POC of JMH",
+          "timestamp": "2023-03-21T17:47:19Z",
+          "url": "https://github.com/DataBiosphere/terra-workspace-data-service/pull/196/commits/434ceca221a2b3df713a9bdda2c38870aef9a812"
+        },
+        "date": 1680274151422,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.databiosphere.workspacedataservice.controller.ApiBenchmark.upsertRecord",
+            "value": 397.90147504522855,
             "unit": "ops/s",
             "extra": "iterations: 2\nforks: 2\nthreads: 1"
           }
