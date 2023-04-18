3a533b7c8d9c7eacd588542f76596940b57831ed
yuliadub <yulia.dubinina@gmail.com>
2023-04-18 13:09:41 -0700

    add test with envs

--- service/src/test/java/org/databiosphere/workspacedataservice/process/LocalProcessLaunchTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/process/LocalProcessLaunchTest.java
@@ -3,8 +3,11 @@ package org.databiosphere.workspacedataservice.process;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocalProcessLaunchTest {
@@ -22,5 +25,29 @@ public class LocalProcessLaunchTest {
         // block until the child process exits
         int exitCode = localProcessLauncher.waitForTerminate();
         assertEquals(0, exitCode);
+    }
+
+    @Test
+    void runSimpleCommandWithEnvs() {
+        List<String> processCommand = new ArrayList<>();
+        Map<String, String> envVars = new HashMap<>();
+        envVars.put("TEST_VARIABLE", "Hello World");
+        processCommand.add("bash");
+        processCommand.add("-ce");
+        processCommand.add("echo $TEST_VARIABLE");
+
+        // launch the child process
+        LocalProcessLauncher localProcessLauncher = new LocalProcessLauncher();
+        localProcessLauncher.launchProcess(processCommand, envVars);
+
+        // stream the output to stdout/err
+        String output = localProcessLauncher.getOutputForProcess(LocalProcessLauncher.Output.OUT);
+        String error = localProcessLauncher.getOutputForProcess(LocalProcessLauncher.Output.ERROR);
+
+        // block until the child process exits
+        int exitCode = localProcessLauncher.waitForTerminate();
+        assertEquals(0, exitCode);
+        assertThat(output).isEqualTo("Hello World");
+        assertThat(error).isEmpty();
     }
 }
