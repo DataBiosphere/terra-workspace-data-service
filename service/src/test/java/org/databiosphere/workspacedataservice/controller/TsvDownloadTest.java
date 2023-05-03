8e7c3a0fe279844a8024a9243670a1827cae45e7
David An <davidan@broadinstitute.org>
2023-05-03 09:54:37 -0400

    cleanup

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/TsvDownloadTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/TsvDownloadTest.java
@@ -20,7 +20,6 @@ import org.springframework.http.*;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.servlet.function.ServerRequest;
 
 import java.io.IOException;
