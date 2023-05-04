56b01dce9a107e51b911c493f08930b0f95a075e
David An <davidan@broadinstitute.org>
2023-05-04 13:51:41 -0400

    update comment

--- service/src/main/java/org/databiosphere/workspacedataservice/SentryInitializer.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/SentryInitializer.java
@@ -36,7 +36,7 @@ public class SentryInitializer  {
 
 	private static final Pattern SAM_ENV_PATTERN = Pattern.compile("\\.dsde-(\\p{Alnum}+)\\.");
 	private static final String DEFAULT_ENV = "unknown";
-	//Environments we want to monitor on sentry - don't send errors from local or bees
+	//Environments we want to monitor on sentry - don't send errors from local, bees, or Github actions
 	private static final List<String> environments = List.of("prod", "alpha", "staging", "dev");
 
 	@Bean
