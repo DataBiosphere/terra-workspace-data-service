d2f746fc8ae9be7d1abf4c8cf8dd12963dcea575
Douglas Voet <dvoet@users.noreply.github.com>
2023-05-12 10:48:10 -0400

    nullproof

--- service/src/main/java/org/databiosphere/workspacedataservice/datarepo/DataRepoException.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/datarepo/DataRepoException.java
@@ -4,6 +4,10 @@ import bio.terra.datarepo.client.ApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 public class DataRepoException extends ResponseStatusException {
-  public DataRepoException(ApiException cause) { super(HttpStatus.valueOf(cause.getCode()), null, cause); }
+  public DataRepoException(ApiException cause) {
+    super(Optional.ofNullable(HttpStatus.resolve(cause.getCode())).orElse(HttpStatus.INTERNAL_SERVER_ERROR), null, cause);
+  }
 }
--- service/src/main/java/org/databiosphere/workspacedataservice/workspacemanager/WorkspaceManagerException.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/workspacemanager/WorkspaceManagerException.java
@@ -4,6 +4,10 @@ import bio.terra.workspace.client.ApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 public class WorkspaceManagerException extends ResponseStatusException {
-  public WorkspaceManagerException(ApiException cause) { super(HttpStatus.valueOf(cause.getCode()), null, cause); }
+  public WorkspaceManagerException(ApiException cause) {
+    super(Optional.ofNullable(HttpStatus.resolve(cause.getCode())).orElse(HttpStatus.INTERNAL_SERVER_ERROR), null, cause);
+  }
 }
