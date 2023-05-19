53ae80cb5d22622b012fb379aa7787e79351e888
David An <davidan@broadinstitute.org>
2023-05-19 16:51:36 -0400

    fix whitespace

--- service/src/main/java/org/databiosphere/workspacedataservice/activitylog/ActivityLogger.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/activitylog/ActivityLogger.java
@@ -70,6 +70,4 @@ public class ActivityLogger {
             LOGGER.info(sb.toString());
         }
     }
-
-
 }
--- service/src/main/java/org/databiosphere/workspacedataservice/activitylog/ActivityModels.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/activitylog/ActivityModels.java
@@ -32,11 +32,4 @@ public class ActivityModels {
             this.name = name;
         }
     }
-
-
-
-
-
-
-
 }
