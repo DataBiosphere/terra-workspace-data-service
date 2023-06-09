2638691b9cabf8d029ee0f668c2ecce7ad64c608
Adina Shanholtz <adinas@microsoft.com>
2023-06-09 09:45:11 -0700

    Fix added quotes, streamtolist, added variable, and exception

--- service/src/main/java/org/databiosphere/workspacedataservice/service/TsvSupport.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/TsvSupport.java
@@ -3,6 +3,7 @@ package org.databiosphere.workspacedataservice.service;
 import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.databiosphere.workspacedataservice.service.model.exception.UnexpectedTsvException;
 import org.databiosphere.workspacedataservice.shared.model.Record;
 
 import java.io.IOException;
@@ -22,7 +23,7 @@ public class TsvSupport {
 		CsvSchema tsvHeaderSchema = CsvSchema.emptySchema()
 		.withEscapeChar('\\')
 		.withColumnSeparator('\t')
-		.withNullValue("");
+		.withoutQuoteChar();
 
 		final CsvMapper tsvMapper = CsvMapper.builder()
 		.build();
@@ -32,18 +33,25 @@ public class TsvSupport {
 		seqW.write(headers);
 		// First header is Primary Key, and value is stored in record.id. Remove header here and add record.id manually.
 		headers.remove(0);
-		for (Record record : records.toList()) {
+		records.forEach(record -> writeRowToTsv(seqW, record, headers));
+		seqW.close();		
+	}
+
+	private static void writeRowToTsv(SequenceWriter seqW, Record record, List<String> headers) {
+		try {
 			List<String> row = recordToRow(record, headers);
 			seqW.write(row);
+		} catch (Exception e) {
+			throw new UnexpectedTsvException("Error writing TSV: " + e.getMessage());
 		}
-		seqW.close();		
 	}
 
 	private static List<String> recordToRow(Record record, List<String> headers) {
 		List<String> row = new ArrayList<String>();
 		row.add(record.getId());
 		headers.forEach(h -> {
-			row.add(record.getAttributeValue(h) == null ? "" : record.getAttributeValue(h).toString());
+			Object attr = record.getAttributeValue(h);
+			row.add(attr == null ? "" : attr.toString());
 		});
 		return row;
 	}
