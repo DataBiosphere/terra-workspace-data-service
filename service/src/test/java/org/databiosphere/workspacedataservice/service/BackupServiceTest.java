d54af2d2bd7a95abe9e946487ababc1f494d8cde
Adina Shanholtz <adinas@microsoft.com>
2023-06-21 13:20:04 -0700

    dangit knew i forgot something

--- service/src/test/java/org/databiosphere/workspacedataservice/service/BackupServiceTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/BackupServiceTest.java
@@ -17,14 +17,14 @@ class BackupServiceTest {
 
     @Test
     void CheckCommandLine() {
-        List<String> commandList = backupService.GenerateCommandList(true);
+        List<String> commandList = backupService.generateCommandList(true);
         String command = String.join(" ", commandList);
         assert(command).equals("/usr/bin/pg_dump -h localhost -p 5432 -U postgres -d wds -v -w");
     }
 
     @Test
     void VerifyBackupFileName() {
-        String blobName = backupService.GenerateBackupFilename();
+        String blobName = backupService.generateBackupFilename();
         assertThat(blobName).containsPattern("[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}-[0-9]{4}-[0-9]{2}-[0-9]{2}_([0-9]+(-[0-9]+)+)\\.sql");
     }
 }
