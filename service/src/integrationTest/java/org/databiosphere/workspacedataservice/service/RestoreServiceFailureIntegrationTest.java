876c209506ed1c355f41e3957a9dfb2923f61c96
Adina Shanholtz <adinas@microsoft.com>
2023-06-22 11:04:00 -0700

    trying something

--- service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/RestoreServiceFailureIntegrationTest.java
+++ service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/RestoreServiceFailureIntegrationTest.java
@@ -7,23 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-
 @SpringBootTest(properties = "spring.cache.type=NONE")
-public class RestoreServiceIntegrationTest {
+@TestPropertySource(properties = {"twds.instance.workspace-id=123e4567-e89b-12d3-a456-426614174000", "twds.instance.source-workspace-id=123e4567-e89b-12d3-a456-426614174000"})
+public class RestoreServiceFailureIntegrationTest {
     @Autowired
     private BackupRestoreService backupRestoreService;
 
     private LocalFileStorage storage = new LocalFileStorage();
-    
+
     @Test
-    void testRestoreAzureWDS() throws Exception {
-        // Create the local pg_dump file by calling backup first.
-        backupRestoreService.backupAzureWDS(storage, "v0.2");
-        var response = backupRestoreService.restoreAzureWDS(storage, "v0.2");
-        assertTrue(response.backupRestoreStatus(), response.message());
+    void testRestoreAzureWDSErrorHandling() {
+        BackupRestoreResponse response = backupRestoreService.backupAzureWDS(storage, "v0.2");
+        assertFalse(response.backupRestoreStatus());
     }
 }
-
