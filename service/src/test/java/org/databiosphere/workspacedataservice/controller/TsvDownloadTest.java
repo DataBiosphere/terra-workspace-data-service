023fed7ef63f4c6ae77689d810781be3b2bd3a5b
David An <davidan@broadinstitute.org>
2023-06-27 17:56:16 -0400

    json, array datatypes in TSV download

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/TsvDownloadTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/TsvDownloadTest.java
@@ -1,14 +1,21 @@
 package org.databiosphere.workspacedataservice.controller;
 
-import org.databiosphere.workspacedataservice.shared.model.BatchResponse;
-import org.databiosphere.workspacedataservice.shared.model.RecordAttributes;
-import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.databiosphere.workspacedataservice.dao.RecordDao;
+import org.databiosphere.workspacedataservice.service.model.DataTypeMapping;
+import org.databiosphere.workspacedataservice.shared.model.BatchResponse;
+import org.databiosphere.workspacedataservice.shared.model.RecordAttributes;
+import org.databiosphere.workspacedataservice.shared.model.RecordRequest;
+import org.databiosphere.workspacedataservice.shared.model.RecordResponse;
+import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,14 +30,20 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.databiosphere.workspacedataservice.service.model.DataTypeMapping.*;
 
 @ActiveProfiles(profiles = "mock-sam")
 @DirtiesContext
@@ -42,6 +55,8 @@ class TsvDownloadTest {
 
 	@Autowired
 	private RecordController recordController;
+	@Autowired
+	private RecordDao recordDao;
 	private String version;
 	private UUID instanceId;
 
@@ -80,6 +95,7 @@ class TsvDownloadTest {
 		assertThat(tsvIterator.hasNext()).isFalse();
 		reader.close();
 	}
+
 	@Test
 	void batchWriteFollowedByTsvDownload() throws IOException {
 		RecordType recordType = RecordType.valueOf("tsv-test");
@@ -103,4 +119,73 @@ class TsvDownloadTest {
 		assertThat(tsvIterator.hasNext()).isFalse();
 		reader.close();
 	}
+
+
+	private static Stream<Arguments> tsvDownloadValues() {
+        /* Arguments are sets:
+            - first value is the Object to insert as an attribute
+            - second value is the expected data type that the object creates
+            - third value is the text that would be contained in a TSV cell when downloading the attribute
+         */
+		return Stream.of(
+				Arguments.of(Boolean.TRUE,
+						BOOLEAN, "true"),
+				Arguments.of(Boolean.FALSE,
+						BOOLEAN, "false"),
+				Arguments.of("hello",
+						STRING, "hello"),
+				Arguments.of("embedded\ttab",
+						STRING, "\"embedded\ttab\""),
+				Arguments.of(BigDecimal.valueOf(789),
+						NUMBER, "789"),
+				Arguments.of("{\"foo\": \"bar\", \"arr\": [2,4,6]}",
+						JSON, "\"{\"\"arr\"\":[2,4,6],\"\"foo\"\":\"\"bar\"\"}\""),
+				Arguments.of(List.of("foo", "bar", "baz"),
+						ARRAY_OF_STRING, "\"[\"\"foo\"\",\"\"bar\"\",\"\"baz\"\"]\""),
+				Arguments.of(List.of(BigDecimal.valueOf(1), BigDecimal.valueOf(3), BigDecimal.valueOf(5)),
+						ARRAY_OF_NUMBER, "[1,3,5]")
+		);
+	}
+
+
+	@ParameterizedTest(name = "TSV download for a {1} should be correct")
+	@MethodSource("tsvDownloadValues")
+	void tsvDownloadVariousDataTypes(Object toUpload, DataTypeMapping expectedDataType, String expectedCellValue) throws IOException {
+		RecordType recordType = RecordType.valueOf(RandomStringUtils.randomAlphabetic(8));
+		String recordId = "123";
+		String attrName = RandomStringUtils.randomAlphabetic(8);
+
+		// upload a record containing the attribute in question
+		RecordAttributes uploadAttrs = new RecordAttributes(Map.of(attrName, toUpload));
+		RecordRequest recordRequest = new RecordRequest(uploadAttrs);
+
+		ResponseEntity<RecordResponse> response = recordController.upsertSingleRecord(instanceId, version, recordType, recordId, Optional.empty(), recordRequest);
+		assertThat(response.getStatusCodeValue()).isEqualTo(201);
+
+		// verify the backend datatype
+		Map<String, DataTypeMapping> tableSchema = recordDao.getExistingTableSchema(instanceId, recordType);
+		assertThat(tableSchema.keySet()).contains(attrName);
+		DataTypeMapping actualDataType = tableSchema.get(attrName);
+		assertThat(actualDataType).isEqualTo(expectedDataType);
+
+		// get the TSV
+		HttpHeaders headers = new HttpHeaders();
+		ResponseEntity<Resource> stream = restTemplate.exchange("/{instanceId}/tsv/{version}/{recordType}",
+				HttpMethod.GET, new HttpEntity<>(headers), Resource.class, instanceId, version, recordType);
+
+		// read the TSV contents. Note that this test intentionally reads the TSV as strings - we don't use
+		// a MappingIterator<RecordAttributes>, for example. This allows us to assert on the string representation
+		// of values within the TSV.
+		try (
+				InputStream inputStream = stream.getBody().getInputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+		) {
+			String firstLine = reader.readLine();
+			assertThat(firstLine).isEqualTo("sys_name\t" + attrName);
+
+			String secondLine = reader.readLine();
+			assertThat(secondLine).isEqualTo(recordId + "\t" + expectedCellValue);
+		}
+	}
+
 }
