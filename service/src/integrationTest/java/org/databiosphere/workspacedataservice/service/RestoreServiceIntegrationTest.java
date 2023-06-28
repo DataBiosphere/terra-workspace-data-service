a62c508332aae4dc642a10104f1aedbe80f45832
Adina Shanholtz <adinas@microsoft.com>
2023-06-28 15:15:59 -0700

    make restore test more robust

--- service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/RestoreServiceIntegrationTest.java
+++ service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/RestoreServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.databiosphere.workspacedataservice.service;
 
 import org.databiosphere.workspacedataservice.dao.InstanceDao;
+import org.databiosphere.workspacedataservice.dao.RecordDao;
+import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import org.databiosphere.workspacedataservice.storage.LocalFileStorage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.UUID;
 
-@SpringBootTest(properties = "spring.cache.type=NONE")
+@SpringBootTest
 @TestPropertySource(
     properties = {
         "twds.instance.workspace-id=123e4567-e89b-12d3-a456-426614174000", 
@@ -26,17 +30,44 @@ public class RestoreServiceIntegrationTest {
     @Autowired
 	InstanceDao instanceDao;
 
+    @Autowired
+    RecordDao recordDao;
+
     @Value("${twds.instance.workspace-id:}")
     private String workspaceId;
 
+    @Value("${twds.instance.source-workspace-id:}")
+    private String sourceWorkspaceId;
+
     private LocalFileStorage storage = new LocalFileStorage();
-    
+
+    // this test references the file src/integrationTest/resources/backup-test.sql as its backup
     @Test
     void testRestoreAzureWDS() throws Exception {
-        // Clean database of workspace-id value
-        instanceDao.dropSchema(UUID.fromString(workspaceId));
+        UUID sourceInstance = UUID.fromString(sourceWorkspaceId);
+        UUID destInstance = UUID.fromString(workspaceId);
+
+        // clean database of workspace-id value created from autowire
+        instanceDao.dropSchema(destInstance);
+        
+        // confirm neither source nor destination instance should exist in our list of schemas to start
+        List<UUID> instancesBefore = instanceDao.listInstanceSchemas();
+        assertThat(instancesBefore).doesNotContain(destInstance);
+        assertThat(instancesBefore).doesNotContain(sourceInstance);
+
+        // perform the restore
         var response = backupRestoreService.restoreAzureWDS(storage, "v0.2");
         assertTrue(response);
+
+        // after restore, confirm destination instance exists but source does not
+        List<UUID> instancesAfter = instanceDao.listInstanceSchemas();
+        assertThat(instancesAfter).contains(destInstance);
+        assertThat(instancesAfter).doesNotContain(sourceInstance);
+
+        // after restore, target instance should have one table named "test"
+        List<RecordType> tables = recordDao.getAllRecordTypes(destInstance);
+        assertThat(tables.size()).isEqualTo(1);
+        assertThat(tables).contains(RecordType.valueOf("test"));
     }
 }
 
