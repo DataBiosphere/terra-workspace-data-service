0c214d15f34a09bf3f1dcf27bc46e4fb85f626bc
David An <davidan@broadinstitute.org>
2023-06-28 15:35:59 -0400

    AJ-1142: json, array datatypes in TSV download (#287)

--- service/src/main/java/org/databiosphere/workspacedataservice/service/TsvSupport.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/TsvSupport.java
@@ -1,59 +1,91 @@
 package org.databiosphere.workspacedataservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.databiosphere.workspacedataservice.service.model.DataTypeMapping;
 import org.databiosphere.workspacedataservice.service.model.exception.UnexpectedTsvException;
 import org.databiosphere.workspacedataservice.shared.model.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.databiosphere.workspacedataservice.service.model.DataTypeMapping.JSON;
 
+@Component
 public class TsvSupport {
 
-	private TsvSupport() {
+	private final ObjectMapper objectMapper;
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private TsvSupport(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
 	}
 
-	public static void writeTsvToStream (Stream<Record> records, OutputStream stream, List<String> headers) throws IOException {
+	public void writeTsvToStream (Stream<Record> records, Map<String, DataTypeMapping> typeSchema, OutputStream stream, List<String> headers) throws IOException {
 
 		CsvSchema tsvHeaderSchema = CsvSchema.emptySchema()
-		.withEscapeChar('\\')
-		.withColumnSeparator('\t');
+				.withEscapeChar('\\')
+				.withColumnSeparator('\t');
 
 		final CsvMapper tsvMapper = CsvMapper.builder()
-		.enable(CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING)
-		.build();
+				.enable(CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING)
+				.build();
 
 		SequenceWriter seqW = tsvMapper.writer(tsvHeaderSchema)
-			.writeValues(stream);
+				.writeValues(stream);
 		seqW.write(headers);
 		// First header is Primary Key, and value is stored in rcd.id. Remove header here and add rcd.id manually.
 		headers.remove(0);
-		records.forEach(rcd -> writeRowToTsv(seqW, rcd, headers));
-		seqW.close();		
+		records.forEach(rcd -> writeRowToTsv(seqW, rcd, typeSchema, headers));
+		seqW.close();
 	}
 
-	private static void writeRowToTsv(SequenceWriter seqW, Record rcd, List<String> headers) {
+	private void writeRowToTsv(SequenceWriter seqW, Record rcd, Map<String, DataTypeMapping> typeSchema, List<String> headers) {
 		try {
-			List<String> row = recordToRow(rcd, headers);
+			List<String> row = recordToRow(rcd, typeSchema, headers);
 			seqW.write(row);
 		} catch (Exception e) {
 			throw new UnexpectedTsvException("Error writing TSV: " + e.getMessage());
 		}
 	}
 
-	private static List<String> recordToRow(Record rcd, List<String> headers) {
+	private List<String> recordToRow(Record rcd, Map<String, DataTypeMapping> typeSchema, List<String> headers) {
 		List<String> row = new ArrayList<>();
 		row.add(rcd.getId());
 		headers.forEach(h -> {
 			Object attr = rcd.getAttributeValue(h);
-			row.add(attr == null ? "" : attr.toString());
+			DataTypeMapping dataType = typeSchema.get(h);
+
+			// handle null/empty values
+			if (attr == null) {
+				// TODO: output [] for arrays and {} for json?
+				row.add("");
+			// handle arrays and json objects
+			} else if (dataType.isArrayType() || JSON.equals(dataType)) {
+				try {
+					row.add(objectMapper.writeValueAsString(attr));
+				} catch (JsonProcessingException e) {
+					logger.error("Could not create TSV: " + e.getMessage(), e);
+					throw new UnexpectedTsvException("Could not create TSV: " + e.getMessage());
+				}
+			// all other data types
+			} else {
+				row.add(attr.toString());
+			}
 		});
 		return row;
 	}
+
 }
