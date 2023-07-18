34fbba0245eecc18cc9d3131656207127b26bf38
David An <davidan@broadinstitute.org>
2023-07-18 12:03:07 -0400

    test stability

--- service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/BackupRestoreServiceFailureIntegrationTest.java
+++ service/src/integrationTest/java/org/databiosphere/workspacedataservice/service/BackupRestoreServiceFailureIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -21,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 @ActiveProfiles({"mock-storage", "local"})
 @ContextConfiguration(name = "mockStorage")
+@DirtiesContext
 @SpringBootTest
 @TestPropertySource(properties = {
         "twds.pg_dump.useAzureIdentity=false",
--- service/src/test/java/org/databiosphere/workspacedataservice/service/BackupRestoreServiceTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/BackupRestoreServiceTest.java
@@ -3,14 +3,16 @@ package org.databiosphere.workspacedataservice.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@DirtiesContext
 @SpringBootTest(properties = "spring.cache.type=NONE")
-@TestPropertySource(properties = {"twds.instance.workspace-id=123e4567-e89b-12d3-a456-426614174000",
+@TestPropertySource(properties = {"twds.instance.workspace-id=f7c83b8d-53f3-473e-b9b4-2663d13e2752",
         "twds.pg_dump.path=/unit/test/pg_dump",
         "twds.pg_dump.psqlPath=/unit/test/psql"})
 class BackupRestoreServiceTest {
@@ -21,7 +23,7 @@ class BackupRestoreServiceTest {
     void CheckBackupCommandLine() {
         List<String> commandList = backupRestoreService.generateCommandList(true);
         String command = String.join(" ", commandList);
-        assertThat(command).isEqualTo("/unit/test/pg_dump -b -n 123e4567-e89b-12d3-a456-426614174000 -h localhost -p 5432 -U wds -d wds -v -w");
+        assertThat(command).isEqualTo("/unit/test/pg_dump -b -n f7c83b8d-53f3-473e-b9b4-2663d13e2752 -h localhost -p 5432 -U wds -d wds -v -w");
     }
 
     @Test
