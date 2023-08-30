1f3762a0caf398dac537dbe676219289ff1f95a7
David An <davidan@broadinstitute.org>
2023-08-30 14:25:21 -0400

    AJ-1173: okhttp-based WDS client (#321)

--- service/src/main/java/org/databiosphere/workspacedataservice/sourcewds/HttpWorkspaceDataServiceClientFactory.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/sourcewds/HttpWorkspaceDataServiceClientFactory.java
@@ -1,5 +1,6 @@
 package org.databiosphere.workspacedataservice.sourcewds;
 
+import okhttp3.OkHttpClient;
 import org.databiosphere.workspacedata.api.CloningApi;
 import org.databiosphere.workspacedata.client.ApiClient;
 import org.apache.commons.lang3.StringUtils;
@@ -7,18 +8,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 
-import javax.ws.rs.client.Client;
 import java.util.Objects;
 
 import static org.databiosphere.workspacedataservice.sam.BearerTokenFilter.ATTRIBUTE_NAME_TOKEN;
 import static org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST;
 
 public class HttpWorkspaceDataServiceClientFactory implements WorkspaceDataServiceClientFactory {
-    private final Client commonHttpClient;
+    private final OkHttpClient commonHttpClient;
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpWorkspaceDataServiceClientFactory.class);
 
     public HttpWorkspaceDataServiceClientFactory() {
-        this.commonHttpClient = new ApiClient().getHttpClient();
+        this.commonHttpClient = new ApiClient()
+                .getHttpClient()
+                .newBuilder()
+                .build();;
     }
 
     private ApiClient getApiClient(String token, String workspaceDataServiceUrl) {
