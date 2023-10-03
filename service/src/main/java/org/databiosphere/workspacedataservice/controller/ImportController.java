280cccfeae9d8586506b90a1f7add73e0e49c180
David An <davidan@broadinstitute.org>
2023-10-03 10:58:00 -0400

    move v1 APIs to separate file, disambiguate models

--- service/src/main/java/org/databiosphere/workspacedataservice/controller/ImportController.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/controller/ImportController.java
@@ -2,7 +2,7 @@ package org.databiosphere.workspacedataservice.controller;
 
 import java.util.UUID;
 import org.databiosphere.workspacedataservice.generated.api.ImportApi;
-import org.databiosphere.workspacedataservice.generated.model.ImportRequest;
+import org.databiosphere.workspacedataservice.generated.model.ImportRequestServerModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImportController implements ImportApi {
 
   @Override
-  public ResponseEntity<Void> importV1(UUID instanceUuid, ImportRequest importRequest) {
+  public ResponseEntity<Void> importV1(UUID instanceUuid, ImportRequestServerModel importRequest) {
     // TODO: implementation for imports
     // return TOO_EARLY here as a proof-of-concept that we can override the implementation
     // in ImportApi (which returns NOT_IMPLEMENTED)
