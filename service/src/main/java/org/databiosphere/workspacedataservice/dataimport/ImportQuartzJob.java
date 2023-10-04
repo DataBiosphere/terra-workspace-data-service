57e7b556402ef5c4ec2e4572b5d7329bf4f48a3f
David An <davidan@broadinstitute.org>
2023-10-04 15:09:10 -0400

    include ImportDao in the Quartz job

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/ImportQuartzJob.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/ImportQuartzJob.java
@@ -1,6 +1,7 @@
 package org.databiosphere.workspacedataservice.dataimport;
 
 import java.util.concurrent.ThreadLocalRandom;
+import org.databiosphere.workspacedataservice.dao.ImportDao;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -14,12 +15,20 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ImportQuartzJob implements Job {
+
+  private final ImportDao importDao;
+
+  public ImportQuartzJob(ImportDao importDao) {
+    this.importDao = importDao;
+  }
+
   @Override
   public void execute(JobExecutionContext context) throws JobExecutionException {
 
     long sleepMillis = ThreadLocalRandom.current().nextLong(1000, 15000);
     try {
       Thread.sleep(sleepMillis);
+      importDao.updateStatus(context.getJobDetail().getKey().getName(), ImportStatus.SUCCEEDED);
     } catch (InterruptedException e) {
       throw new JobExecutionException(e);
     }
