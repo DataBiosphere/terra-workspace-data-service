4ac9d9167f44354ab3991b1bae4f143c93e0d2c4
Bria Morgan <bmorgan@broadinstitute.org>
2023-10-16 11:05:52 -0400

    flesh out more pacts

--- service/src/test/java/org/databiosphere/workspacedataservice/pact/TDRPactTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/pact/TDRPactTest.java
@@ -1,14 +1,17 @@
 package org.databiosphere.workspacedataservice.pact;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import bio.terra.datarepo.model.SnapshotModel;
 import java.util.UUID;
 import org.databiosphere.workspacedataservice.datarepo.DataRepoClientFactory;
 import org.databiosphere.workspacedataservice.datarepo.DataRepoDao;
@@ -51,6 +54,41 @@ class TDRPactTest {
         .toPact();
   }
 
+  @Pact(consumer = "wds-consumer", provider = "tdr-provider")
+  public RequestResponsePact noAccessToSnapshotPact(PactDslWithProvider builder) {
+    return builder
+        .given("user does not have access to snapshot")
+        .uponReceiving("a snapshot request")
+        .pathFromProviderState(
+            "/api/repository/v1/snapshots/${dummySnapshotId}",
+            String.format("/api/repository/v1/snapshots/%s", dummySnapshotId))
+        .query("include=TABLES")
+        .method("GET")
+        .willRespondWith()
+        .status(403)
+        .toPact();
+  }
+
+  @Pact(consumer = "wds-consumer", provider = "tdr-provider")
+  public RequestResponsePact userHasAccessToSnapshotPact(PactDslWithProvider builder) {
+    // Current use of datarepo snapshot only relies on id and name
+    // Future development should update expected response shape to include fields we need
+    var snapshotResponseShape =
+        new PactDslJsonBody().stringValue("id", dummySnapshotId).stringType("name");
+    return builder
+        .given("user has access to snapshot")
+        .uponReceiving("a snapshot request")
+        .pathFromProviderState(
+            "/api/repository/v1/snapshots/${dummySnapshotId}",
+            String.format("/api/repository/v1/snapshots/%s", dummySnapshotId))
+        .query("include=TABLES")
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .body(snapshotResponseShape)
+        .toPact();
+  }
+
   @Test
   @PactTestFor(pactMethod = "noSnapshotPact", pactVersion = PactSpecVersion.V3)
   void testNoSnapshot(MockServer mockServer) {
@@ -61,5 +99,29 @@ class TDRPactTest {
         DataRepoException.class,
         () -> dataRepoDao.getSnapshot(UUID.fromString(dummySnapshotId)),
         "nonexistent snapshot should return 404");
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "noAccessToSnapshotPact", pactVersion = PactSpecVersion.V3)
+  void testNoAccessToSnapshot(MockServer mockServer) {
+    DataRepoClientFactory clientFactory = new HttpDataRepoClientFactory(mockServer.getUrl());
+    DataRepoDao dataRepoDao = new DataRepoDao(clientFactory);
+
+    assertThrows(
+        DataRepoException.class,
+        () -> dataRepoDao.getSnapshot(UUID.fromString(dummySnapshotId)),
+        "nonexistent snapshot should return 403");
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "userHasAccessToSnapshotPact", pactVersion = PactSpecVersion.V3)
+  void testUserHasAccessToSnapshot(MockServer mockServer) {
+    DataRepoClientFactory clientFactory = new HttpDataRepoClientFactory(mockServer.getUrl());
+    DataRepoDao dataRepoDao = new DataRepoDao(clientFactory);
+
+    SnapshotModel snapshot = dataRepoDao.getSnapshot(UUID.fromString(dummySnapshotId));
+    assertNotNull(snapshot, "Snapshot request should return a snapshot");
+    assertNotNull(snapshot.getId(), "Snapshot response should have an id");
+    assertNotNull(snapshot.getName(), "Snapshot response should have a name");
   }
 }
