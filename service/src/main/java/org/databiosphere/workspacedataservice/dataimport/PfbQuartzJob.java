fecc81232456e868c83db396ea4d7daa67d6a6c1
Bria Morgan <bmorgan@broadinstitute.org>
2023-10-19 11:28:58 -0400

    start to flesh out

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
@@ -1,6 +1,14 @@
 package org.databiosphere.workspacedataservice.dataimport;
 
+import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_URL;
+
+import bio.terra.pfb.PfbReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
 import java.util.UUID;
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.generic.GenericRecord;
 import org.databiosphere.workspacedataservice.dao.JobDao;
 import org.databiosphere.workspacedataservice.jobexec.QuartzJob;
 import org.quartz.JobExecutionContext;
@@ -28,6 +36,18 @@ public class PfbQuartzJob extends QuartzJob {
   @Override
   protected void executeInternal(UUID jobId, JobExecutionContext context) {
     // TODO: AJ-1227? implement PFB import.
+    String url = (String) context.get(ARG_URL);
+    Set<String> snapshotIds = Collections.emptySet();
+    try (DataFileStream<GenericRecord> dataStream = PfbReader.getGenericRecordsStream(url)) {
+      while (dataStream.hasNext()) {
+        GenericRecord record = dataStream.next();
+        record.get("object");
+      }
+    } catch (IOException e) {
+      // Handle exceptions if necessary
+      e.printStackTrace();
+    }
+
     logger.info("TODO: implement PFB import.");
   }
 }
