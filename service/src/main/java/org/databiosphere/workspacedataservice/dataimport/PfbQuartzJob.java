9bbdb96dadf031bd0e1cb7bc69dd10ef88b6ac5b
Bria Morgan <bmorgan@broadinstitute.org>
2023-10-23 16:22:21 -0400

    a bit more fleshing out

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
@@ -2,15 +2,17 @@ package org.databiosphere.workspacedataservice.dataimport;
 
 import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_URL;
 
+import bio.terra.datarepo.model.SnapshotModel;
 import bio.terra.pfb.PfbReader;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import org.apache.avro.file.DataFileStream;
 import org.apache.avro.generic.GenericRecord;
 import org.databiosphere.workspacedataservice.dao.JobDao;
 import org.databiosphere.workspacedataservice.jobexec.QuartzJob;
+import org.databiosphere.workspacedataservice.workspacemanager.WorkspaceManagerDao;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +22,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class PfbQuartzJob extends QuartzJob {
 
+  public static final String SNAPSHOT_ID_IDENTIFIER = "source_datarepo_snapshot_id";
+
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   private final JobDao jobDao;
+  private final WorkspaceManagerDao wsmDao;
 
-  public PfbQuartzJob(JobDao jobDao) {
+  public PfbQuartzJob(JobDao jobDao, WorkspaceManagerDao wsmDao) {
     this.jobDao = jobDao;
+    this.wsmDao = wsmDao;
   }
 
   @Override
@@ -35,19 +41,35 @@ public class PfbQuartzJob extends QuartzJob {
 
   @Override
   protected void executeInternal(UUID jobId, JobExecutionContext context) {
-    // TODO: AJ-1227? implement PFB import.
     String url = (String) context.get(ARG_URL);
-    Set<String> snapshotIds = Collections.emptySet();
+    Set<String> snapshotIds = new HashSet();
     try (DataFileStream<GenericRecord> dataStream = PfbReader.getGenericRecordsStream(url)) {
       while (dataStream.hasNext()) {
         GenericRecord record = dataStream.next();
-        record.get("object");
+        // Records in a pfb are stored under the key "object"
+        GenericRecord recordObject = (GenericRecord) record.get("object");
+        if (recordObject == null) {
+          // TODO what type of exception/what message
+          throw new Exception("Record in pfb missing data");
+        }
+        Object snapId = recordObject.get(SNAPSHOT_ID_IDENTIFIER);
+        if (snapId != null) {
+          snapshotIds.add(snapId.toString());
+        }
       }
     } catch (IOException e) {
       // Handle exceptions if necessary
       e.printStackTrace();
+    } catch (Exception e) {
+      // TODO what to do with exceptions
+      e.printStackTrace();
     }
 
+    // TODO AJ-1371 pass snapshotIds to WSM
+    for (String id : snapshotIds) {
+      wsmDao.createDataRepoSnapshotReference(new SnapshotModel().id(UUID.fromString(id)));
+    }
+    // TODO: AJ-1227 implement PFB import.
     logger.info("TODO: implement PFB import.");
   }
 }
--- service/src/test/java/org/databiosphere/workspacedataservice/pfb/PfbJobTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/pfb/PfbJobTest.java
@@ -2,14 +2,24 @@ package org.databiosphere.workspacedataservice.pfb;
 
 import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_TOKEN;
 import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_URL;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
+import bio.terra.datarepo.model.SnapshotModel;
+import java.net.URL;
 import java.util.Map;
 import java.util.UUID;
 import org.databiosphere.workspacedataservice.dao.JobDao;
 import org.databiosphere.workspacedataservice.dataimport.PfbQuartzJob;
+import org.databiosphere.workspacedataservice.generated.GenericJobServerModel;
+import org.databiosphere.workspacedataservice.workspacemanager.WorkspaceManagerDao;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -17,6 +27,7 @@ import org.quartz.JobKey;
 import org.quartz.impl.JobDetailImpl;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext
@@ -24,22 +35,64 @@ import org.springframework.test.annotation.DirtiesContext;
 public class PfbJobTest {
 
   @MockBean JobDao jobDao;
+  @SpyBean WorkspaceManagerDao wsmDao;
+
+  @Test
+  void doNotFailOnMissingSnapshotId() throws JobExecutionException {
+    JobExecutionContext mockContext = mock(JobExecutionContext.class);
+    when(mockContext.getMergedJobDataMap())
+        .thenReturn(new JobDataMap(Map.of(ARG_TOKEN, "expectedToken")));
+    // TODO is there a smarter/better way to get the correct actual path to this file
+    URL resourceUrl = getClass().getResource("/test.avdl");
+    when(mockContext.get(ARG_URL)).thenReturn(resourceUrl.toString());
+
+    JobDetailImpl jobDetail = new JobDetailImpl();
+    UUID jobId = UUID.randomUUID();
+    jobDetail.setKey(new JobKey(jobId.toString(), "bar"));
+    when(mockContext.getJobDetail()).thenReturn(jobDetail);
+
+    new PfbQuartzJob(jobDao, wsmDao).execute(mockContext);
+
+    // Should not call wsm dao
+    verify(wsmDao, times(0)).createDataRepoSnapshotReference(any());
+    GenericJobServerModel job = jobDao.getJob(jobId);
+    assertEquals(
+        "Job should be failed without snapshot id",
+        GenericJobServerModel.StatusEnum.ERROR,
+        job.getStatus().getValue());
+  }
 
   @Test
   void snapshotIdsAreParsed() throws JobExecutionException {
     JobExecutionContext mockContext = mock(JobExecutionContext.class);
     when(mockContext.getMergedJobDataMap())
-        .thenReturn(
-            new JobDataMap(
-                Map.of(
-                    ARG_TOKEN,
-                    "expectedToken",
-                    ARG_URL,
-                    "/Users/bmorgan/dev/workbench/terra-workspace-data-service/service/src/test/resources/test.avro")));
+        .thenReturn(new JobDataMap(Map.of(ARG_TOKEN, "expectedToken")));
+    // TODO is there a smarter/better way to get the correct actual path to this file
+    URL resourceUrl = getClass().getResource("/test.avdl");
+    when(mockContext.get(ARG_URL)).thenReturn(resourceUrl.toString());
     JobDetailImpl jobDetail = new JobDetailImpl();
     jobDetail.setKey(new JobKey(UUID.randomUUID().toString(), "bar"));
     when(mockContext.getJobDetail()).thenReturn(jobDetail);
 
-    new PfbQuartzJob(jobDao).execute(mockContext);
+    new PfbQuartzJob(jobDao, wsmDao).execute(mockContext);
+
+    // This is the snapshotId given in the test pfb
+    verify(wsmDao)
+        .createDataRepoSnapshotReference(
+            ArgumentMatchers.argThat(
+                new SnapshotModelMatcher(UUID.fromString("790795c4-49b1-4ac8-a060-207b92ea08c5"))));
+  }
+
+  private class SnapshotModelMatcher implements ArgumentMatcher<SnapshotModel> {
+    private final UUID expectedSnapshotId;
+
+    public SnapshotModelMatcher(UUID expectedSnapshotId) {
+      this.expectedSnapshotId = expectedSnapshotId;
+    }
+
+    @Override
+    public boolean matches(SnapshotModel exampleSnapshot) {
+      return exampleSnapshot.getId().equals(expectedSnapshotId);
+    }
   }
 }
