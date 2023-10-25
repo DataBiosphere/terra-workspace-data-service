7393b8dd41620fb5feabd4b2f0967a14608ded44
Josh Ladieu <ladieu@broadinstitute.org>
2023-10-25 10:02:43 -0400

    Use helmchart names for pact identifiers.
    This pairs with https://github.com/broadinstitute/sam/pull/1234, where `sam` also renames its reference from `sam-consumer` to just `sam`.
    
    * See [thread](https://broadinstitute.slack.com/archives/C043YJ40719/p1698068076950019) discussing the `-consumer` and `-provider` suffix anti-pattern.
    * See [thread](https://broadinstitute.slack.com/archives/C043YJ40719/p1698241095600099) discussing the recommendation to use the helmchart name.

--- service/src/test/java/org/databiosphere/workspacedataservice/pact/SamPactTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/pact/SamPactTest.java
@@ -37,7 +37,7 @@ class SamPactTest {
     RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact statusApiPact(PactDslWithProvider builder) {
     return builder
         .given("Sam is ok")
@@ -50,7 +50,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact downStatusApiPact(PactDslWithProvider builder) {
     return builder
         .given("Sam is not ok")
@@ -63,7 +63,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact writeNoPermissionPact(PactDslWithProvider builder) {
     return builder
         .given("user does not have write permission", Map.of("dummyResourceId", dummyResourceId))
@@ -78,7 +78,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact writePermissionPact(PactDslWithProvider builder) {
     return builder
         .given("user has write permission", Map.of("dummyResourceId", dummyResourceId))
@@ -93,7 +93,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact deletePermissionPact(PactDslWithProvider builder) {
     return builder
         .given("user has delete permission", Map.of("dummyResourceId", dummyResourceId))
@@ -108,7 +108,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact deleteNoPermissionPact(PactDslWithProvider builder) {
     return builder
         .given("user does not have delete permission", Map.of("dummyResourceId", dummyResourceId))
@@ -123,7 +123,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact userStatusPact(PactDslWithProvider builder) {
     var userResponseShape =
         new PactDslJsonBody()
@@ -142,7 +142,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact noUserStatusPact(PactDslWithProvider builder) {
     return builder
         .given("user status info request without access token")
@@ -154,7 +154,7 @@ class SamPactTest {
         .toPact();
   }
 
-  @Pact(consumer = "wds-consumer", provider = "sam-provider")
+  @Pact(consumer = "wds", provider = "sam")
   public RequestResponsePact petTokenPact(PactDslWithProvider builder) {
     PactDslJsonRootValue responseBody = PactDslJsonRootValue.stringType("aToken");
 
