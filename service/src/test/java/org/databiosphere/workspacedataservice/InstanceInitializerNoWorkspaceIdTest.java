8c3d88c7e6c65de30cc0a68ebe645aafea556732
David An <davidan@broadinstitute.org>
2023-10-27 10:41:44 -0400

    AJ-1160: reusable retry functions for REST clients (#385)

--- service/src/test/java/org/databiosphere/workspacedataservice/InstanceInitializerNoWorkspaceIdTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/InstanceInitializerNoWorkspaceIdTest.java
@@ -13,6 +13,7 @@ import java.util.concurrent.locks.Lock;
 import org.databiosphere.workspacedataservice.activitylog.ActivityLoggerConfig;
 import org.databiosphere.workspacedataservice.dao.*;
 import org.databiosphere.workspacedataservice.leonardo.LeonardoConfig;
+import org.databiosphere.workspacedataservice.retry.RestClientRetry;
 import org.databiosphere.workspacedataservice.sam.MockSamClientFactoryConfig;
 import org.databiosphere.workspacedataservice.sam.SamConfig;
 import org.databiosphere.workspacedataservice.service.BackupRestoreService;
@@ -53,7 +54,8 @@ import org.springframework.test.context.TestPropertySource;
       WorkspaceManagerConfig.class,
       ActivityLoggerConfig.class,
       SamConfig.class,
-      MockSamClientFactoryConfig.class
+      MockSamClientFactoryConfig.class,
+      RestClientRetry.class
     })
 class InstanceInitializerNoWorkspaceIdTest {
 
