afcedacac0f0669597c836b4087d41e399e0fd44
David An <davidan@broadinstitute.org>
2023-10-30 15:37:30 -0400

    AJ-1371: link snapshots during AnVIL pfb import (#386)

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/TdrSnapshotSupport.java
@@ -0,0 +1,126 @@
+package org.databiosphere.workspacedataservice.dataimport;
+
+import bio.terra.workspace.model.DataRepoSnapshotAttributes;
+import bio.terra.workspace.model.ResourceAttributesUnion;
+import bio.terra.workspace.model.ResourceDescription;
+import bio.terra.workspace.model.ResourceList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.databiosphere.workspacedataservice.retry.RestClientRetry;
+import org.databiosphere.workspacedataservice.service.model.exception.PfbImportException;
+import org.databiosphere.workspacedataservice.workspacemanager.WorkspaceManagerDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TdrSnapshotSupport {
+
+  private final UUID workspaceId;
+  private final WorkspaceManagerDao wsmDao;
+  private final RestClientRetry restClientRetry;
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  public TdrSnapshotSupport(
+      UUID workspaceId, WorkspaceManagerDao wsmDao, RestClientRetry restClientRetry) {
+    this.workspaceId = workspaceId;
+    this.wsmDao = wsmDao;
+    this.restClientRetry = restClientRetry;
+  }
+
+  /**
+   * Query WSM for the full list of referenced snapshots in this workspace, then return the list of
+   * unique snapshotIds from those references.
+   *
+   * @param pageSize how many references to return in each paginated request to WSM
+   * @return the list of unique ids for all pre-existing snapshot references
+   */
+  protected List<UUID> existingPolicySnapshotIds(int pageSize) {
+    return extractSnapshotIds(listAllSnapshots(pageSize));
+  }
+
+  /**
+   * Given a ResourceList, find all the valid ids of referenced snapshots in that list
+   *
+   * @param resourceList the list in which to look for snapshotIds
+   * @return the list of unique ids in the provided list
+   */
+  protected List<UUID> extractSnapshotIds(ResourceList resourceList) {
+    return resourceList.getResources().stream()
+        .map(this::safeGetSnapshotId)
+        .filter(Objects::nonNull)
+        .distinct()
+        .toList();
+  }
+
+  /**
+   * Get the full list of all snapshot references for the current workspace. WSM returns these
+   * results paginated; this method retrieves pages from WSM and aggregates the results.
+   *
+   * @param pageSize number of results to return from WSM at once
+   * @return the full list of all snapshot references for the workspace.
+   */
+  protected ResourceList listAllSnapshots(int pageSize) {
+    final AtomicInteger offset = new AtomicInteger(0);
+    final int hardLimit =
+        10000; // under no circumstances return more than this many snapshots from WSM
+
+    ResourceList finalList = new ResourceList(); // collect our results
+
+    while (offset.get() < hardLimit) {
+      // get a page of results from WSM
+      RestClientRetry.RestCall<ResourceList> restCall =
+          (() -> wsmDao.enumerateDataRepoSnapshotReferences(workspaceId, offset.get(), pageSize));
+      ResourceList thisPage =
+          restClientRetry.withRetryAndErrorHandling(
+              restCall, "WSM.enumerateDataRepoSnapshotReferences");
+
+      // add this page of results to our collector
+      finalList.getResources().addAll(thisPage.getResources());
+
+      if (thisPage.getResources().size() < pageSize) {
+        // fewer results from WSM than we requested; this is the last page of results
+        return finalList;
+      } else {
+        // bump our offset and request another page of results
+        offset.addAndGet(pageSize);
+      }
+    }
+
+    throw new PfbImportException(
+        "Exceeded hard limit of %d for number of pre-existing snapshot references"
+            .formatted(hardLimit));
+  }
+
+  /**
+   * Given a ResourceDescription representing a snapshot reference, retrieve that snapshot's UUID.
+   *
+   * @param resourceDescription the WSM object in which to find a snapshotId
+   * @return the snapshotId if found, else null
+   */
+  protected UUID safeGetSnapshotId(ResourceDescription resourceDescription) {
+    ResourceAttributesUnion resourceAttributes = resourceDescription.getResourceAttributes();
+    if (resourceAttributes != null) {
+      DataRepoSnapshotAttributes dataRepoSnapshot = resourceAttributes.getGcpDataRepoSnapshot();
+      if (dataRepoSnapshot != null) {
+        String snapshotIdStr = dataRepoSnapshot.getSnapshot();
+        try {
+          return UUID.fromString(snapshotIdStr);
+        } catch (Exception e) {
+          String resourceId = "unknown";
+          try {
+            resourceId = resourceDescription.getMetadata().getResourceId().toString();
+          } catch (Exception inner) {
+            // something is exceptionally funky about this resource.
+            resourceId = inner.getMessage();
+          }
+          logger.warn(
+              "Processed a ResourceDescription [%s] for workspace %s that did not contain a valid snapshotId"
+                  .formatted(resourceId, workspaceId));
+        }
+      }
+    }
+    return null;
+  }
+}
--- service/src/test/java/org/databiosphere/workspacedataservice/pfb/PfbJobTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/pfb/PfbJobTest.java
@@ -3,17 +3,21 @@ package org.databiosphere.workspacedataservice.pfb;
 import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_TOKEN;
 import static org.databiosphere.workspacedataservice.shared.model.Schedulable.ARG_URL;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import bio.terra.datarepo.model.SnapshotModel;
+import bio.terra.workspace.client.ApiException;
+import bio.terra.workspace.model.ResourceList;
 import java.net.URL;
 import java.util.Map;
 import java.util.UUID;
 import org.databiosphere.workspacedataservice.dao.JobDao;
 import org.databiosphere.workspacedataservice.dataimport.PfbQuartzJob;
+import org.databiosphere.workspacedataservice.retry.RestClientRetry;
 import org.databiosphere.workspacedataservice.workspacemanager.WorkspaceManagerDao;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatcher;
@@ -23,19 +27,22 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.JobKey;
 import org.quartz.impl.JobDetailImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext
 @SpringBootTest
+// TODO: move these tests into PfbQuartzJobTest?
 class PfbJobTest {
 
   @MockBean JobDao jobDao;
   @MockBean WorkspaceManagerDao wsmDao;
+  @Autowired RestClientRetry restClientRetry;
 
   @Test
-  void doNotFailOnMissingSnapshotId() throws JobExecutionException {
+  void doNotFailOnMissingSnapshotId() throws JobExecutionException, ApiException {
     JobExecutionContext mockContext = mock(JobExecutionContext.class);
     // This uses a non-TDR file so does not have the snapshotId
     URL resourceUrl = getClass().getResource("/minimal_data.avro");
@@ -48,7 +55,11 @@ class PfbJobTest {
     jobDetail.setKey(new JobKey(jobId.toString(), "bar"));
     when(mockContext.getJobDetail()).thenReturn(jobDetail);
 
-    new PfbQuartzJob(jobDao, wsmDao).execute(mockContext);
+    // WSM should report no snapshots already linked to this workspace
+    when(wsmDao.enumerateDataRepoSnapshotReferences(any(), anyInt(), anyInt()))
+        .thenReturn(new ResourceList());
+
+    new PfbQuartzJob(jobDao, wsmDao, restClientRetry, UUID.randomUUID()).execute(mockContext);
 
     // Should not call wsm dao
     verify(wsmDao, times(0)).createDataRepoSnapshotReference(any());
@@ -57,7 +68,7 @@ class PfbJobTest {
   }
 
   @Test
-  void snapshotIdsAreParsed() throws JobExecutionException {
+  void snapshotIdsAreParsed() throws JobExecutionException, ApiException {
     JobExecutionContext mockContext = mock(JobExecutionContext.class);
     URL resourceUrl = getClass().getResource("/test.avro");
     when(mockContext.getMergedJobDataMap())
@@ -69,7 +80,11 @@ class PfbJobTest {
     jobDetail.setKey(new JobKey(jobId.toString(), "bar"));
     when(mockContext.getJobDetail()).thenReturn(jobDetail);
 
-    new PfbQuartzJob(jobDao, wsmDao).execute(mockContext);
+    // WSM should report no snapshots already linked to this workspace
+    when(wsmDao.enumerateDataRepoSnapshotReferences(any(), anyInt(), anyInt()))
+        .thenReturn(new ResourceList());
+
+    new PfbQuartzJob(jobDao, wsmDao, restClientRetry, UUID.randomUUID()).execute(mockContext);
 
     // This is the snapshotId given in the test pfb
     verify(wsmDao)
