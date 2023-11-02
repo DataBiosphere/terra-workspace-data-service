b53199b8343aa42f5d5804ced4359dac722e558b
Adina Shanholtz <adinas@microsoft.com>
2023-11-01 17:32:07 -0700

    wrap wds function too

--- service/src/main/java/org/databiosphere/workspacedataservice/sourcewds/WorkspaceDataServiceDao.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/sourcewds/WorkspaceDataServiceDao.java
@@ -4,19 +4,24 @@ import java.util.UUID;
 import org.databiosphere.workspacedata.client.ApiException;
 import org.databiosphere.workspacedata.model.BackupJob;
 import org.databiosphere.workspacedata.model.BackupRestoreRequest;
+import org.databiosphere.workspacedataservice.retry.RestClientRetry;
+import org.databiosphere.workspacedataservice.retry.RestClientRetry.RestCall;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WorkspaceDataServiceDao {
   private final WorkspaceDataServiceClientFactory workspaceDataServiceClientFactory;
+  private final RestClientRetry restClientRetry;
 
   private String workspaceDataServiceUrl;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(WorkspaceDataServiceDao.class);
 
   public WorkspaceDataServiceDao(
-      WorkspaceDataServiceClientFactory workspaceDataServiceClientFactory) {
+      WorkspaceDataServiceClientFactory workspaceDataServiceClientFactory,
+      RestClientRetry restClientRetry) {
     this.workspaceDataServiceClientFactory = workspaceDataServiceClientFactory;
+    this.restClientRetry = restClientRetry;
   }
 
   public void setWorkspaceDataServiceUrl(String endpointUrl) {
@@ -25,16 +30,12 @@ public class WorkspaceDataServiceDao {
 
   /** Triggers a backup in source workspace data service. */
   public BackupJob triggerBackup(String token, UUID requesterWorkspaceId) {
-    try {
-      var backupClient =
-          this.workspaceDataServiceClientFactory.getBackupClient(token, workspaceDataServiceUrl);
-      BackupRestoreRequest body = new BackupRestoreRequest();
-      body.setRequestingWorkspaceId(requesterWorkspaceId);
-      return backupClient.createBackup(body, "v0.2");
-    } catch (ApiException e) {
-      LOGGER.warn("Error from remote WDS: {}", e.getMessage());
-      throw new WorkspaceDataServiceException(e);
-    }
+    var backupClient =
+        this.workspaceDataServiceClientFactory.getBackupClient(token, workspaceDataServiceUrl);
+    BackupRestoreRequest body = new BackupRestoreRequest();
+    body.setRequestingWorkspaceId(requesterWorkspaceId);
+    RestCall<BackupJob> createBackupFunction = () -> backupClient.createBackup(body, "v0.2");
+    return restClientRetry.withRetryAndErrorHandling(createBackupFunction, "WDS.createBackup");
   }
 
   /** Checks status of a backup in source workspace data service. */
