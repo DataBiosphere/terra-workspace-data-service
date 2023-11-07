bebcad7a85f41ed4b80bb28e592bb0e77df79d23
David An <davidan@broadinstitute.org>
2023-11-07 16:43:25 -0500

    refactor/reorganize

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/PfbQuartzJob.java
@@ -4,7 +4,6 @@ import static org.databiosphere.workspacedataservice.shared.model.Schedulable.AR
 
 import bio.terra.datarepo.model.SnapshotModel;
 import bio.terra.pfb.PfbReader;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.Objects;
@@ -71,62 +70,102 @@ public class PfbQuartzJob extends QuartzJob {
 
   @Override
   protected void executeInternal(UUID jobId, JobExecutionContext context) {
+    // Grab the PFB url from the job's data map
     JobDataMap jobDataMap = context.getMergedJobDataMap();
     URL url = getJobDataUrl(jobDataMap, ARG_URL);
+
+    // Find all the snapshot ids in the PFB, then create or verify references from the
+    // workspace to the snapshot for each of those snapshot ids.
+    logger.info("Linking snapshots for this PFB.");
+    withPfbStream(url, this::findAndLinkSnapshots);
+
+    // Import all the tables and rows inside the PFB.
+    logger.info("Importing tables and rows from this PFB.");
+    withPfbStream(url, this::importTables);
+  }
+
+  /**
+   * definition for some function that consumes a PFB stream (as a DataFileStream<GenericRecord>)
+   */
+  @FunctionalInterface
+  public interface PfbStreamConsumer {
+    void run(DataFileStream<GenericRecord> dataStream) throws Exception;
+  }
+
+  /**
+   * convenience wrapper function to execute a PfbStreamConsumer on a PFB at a given url, handling
+   * opening and closing of a DataFileStream for that PFB.
+   *
+   * @param url location of the PFB
+   * @param consumer code to execute against the PFB's contents
+   */
+  void withPfbStream(URL url, PfbStreamConsumer consumer) {
     try (DataFileStream<GenericRecord> dataStream =
         PfbReader.getGenericRecordsStream(url.toString())) {
-      // translate the Avro DataFileStream into a Java stream
-      Stream<GenericRecord> recordStream =
-          StreamSupport.stream(
-              Spliterators.spliteratorUnknownSize(dataStream.iterator(), Spliterator.ORDERED),
-              false);
-
-      // process the stream into a list of unique snapshotIds
-      List<UUID> snapshotIds =
-          recordStream
-              .map(rec -> rec.get("object")) // Records in a pfb are stored under the key "object"
-              .filter(GenericRecord.class::isInstance) // which we expect to be a GenericRecord
-              .map(GenericRecord.class::cast)
-              .filter(
-                  obj ->
-                      obj.hasField(SNAPSHOT_ID_IDENTIFIER)) // avoid exception if field nonexistent
-              .map(obj -> obj.get(SNAPSHOT_ID_IDENTIFIER)) // within the GenericRecord, find the
-              // source_datarepo_snapshot_id
-              .filter(Objects::nonNull) // expect source_datarepo_snapshot_id to be non-null
-              .map(obj -> maybeUuid(obj.toString()))
-              .filter(Objects::nonNull)
-              .distinct() // find only the unique snapshotids
-              .toList();
-
-      // link the found snapshots to the workspace, skipping any that were previously linked
-      linkSnapshots(snapshotIds);
-
-    } catch (IOException e) {
-      throw new PfbParsingException("Error processing PFB", e);
-    }
-
-    logger.info("Parsing PFB.");
-
-    try (DataFileStream<GenericRecord> dataStream =
-        PfbReader.getGenericRecordsStream(url.toString())) {
-      BatchWriteResult result =
-          batchWriteService.batchWritePfbStream(dataStream, workspaceId, Optional.of("id"));
-
-      result.entrySet().stream()
-          .forEach(
-              entry -> {
-                RecordType recordType = entry.getKey();
-                int quantity = entry.getValue();
-                activityLogger.saveEventForCurrentUser(
-                    user ->
-                        user.upserted().record().withRecordType(recordType).ofQuantity(quantity));
-              });
-
+      consumer.run(dataStream);
     } catch (Exception e) {
       throw new PfbParsingException("Error processing PFB", e);
     }
   }
 
+  /**
+   * Given a DataFileStream representing a PFB, import all the tables and rows inside that PFB.
+   *
+   * @param dataStream stream representing the PFB.
+   */
+  void importTables(DataFileStream<GenericRecord> dataStream) {
+    BatchWriteResult result =
+        batchWriteService.batchWritePfbStream(dataStream, workspaceId, Optional.of("id"));
+
+    result
+        .entrySet()
+        .forEach(
+            entry -> {
+              RecordType recordType = entry.getKey();
+              int quantity = entry.getValue();
+              activityLogger.saveEventForCurrentUser(
+                  user -> user.upserted().record().withRecordType(recordType).ofQuantity(quantity));
+            });
+  }
+
+  /**
+   * Given a DataFileStream representing a PFB, find all the snapshot ids in the PFB, then create or
+   * verify references from the workspace to the snapshot for each of those snapshot ids.
+   *
+   * @param dataStream stream representing the PFB.
+   */
+  void findAndLinkSnapshots(DataFileStream<GenericRecord> dataStream) {
+    // translate the Avro DataFileStream into a Java stream
+    Stream<GenericRecord> recordStream =
+        StreamSupport.stream(
+            Spliterators.spliteratorUnknownSize(dataStream.iterator(), Spliterator.ORDERED), false);
+
+    // process the stream into a list of unique snapshotIds
+    List<UUID> snapshotIds =
+        recordStream
+            .map(rec -> rec.get("object")) // Records in a pfb are stored under the key "object"
+            .filter(GenericRecord.class::isInstance) // which we expect to be a GenericRecord
+            .map(GenericRecord.class::cast)
+            .filter(
+                obj -> obj.hasField(SNAPSHOT_ID_IDENTIFIER)) // avoid exception if field nonexistent
+            .map(obj -> obj.get(SNAPSHOT_ID_IDENTIFIER)) // within the GenericRecord, find the
+            // source_datarepo_snapshot_id
+            .filter(Objects::nonNull) // expect source_datarepo_snapshot_id to be non-null
+            .map(obj -> maybeUuid(obj.toString()))
+            .filter(Objects::nonNull)
+            .distinct() // find only the unique snapshotids
+            .toList();
+
+    // link the found snapshots to the workspace, skipping any that were previously linked
+    linkSnapshots(snapshotIds);
+  }
+
+  /**
+   * Given a list of snapshot ids, create references from the workspace to the snapshot for each id
+   * that does not already have a reference.
+   *
+   * @param snapshotIds the list of snapshot ids to create or verify references.
+   */
   protected void linkSnapshots(List<UUID> snapshotIds) {
     // list existing snapshots linked to this workspace
     TdrSnapshotSupport tdrSnapshotSupport =
