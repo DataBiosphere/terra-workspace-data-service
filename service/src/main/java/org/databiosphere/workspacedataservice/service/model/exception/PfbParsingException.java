0b7737200d6b423d0183ccdf6592779e1b005884
David An <davidan@broadinstitute.org>
2023-11-18 16:16:15 -0500

    unused constructor

--- service/src/main/java/org/databiosphere/workspacedataservice/service/model/exception/PfbParsingException.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/model/exception/PfbParsingException.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class PfbParsingException extends IllegalArgumentException {
 
-  public PfbParsingException(String message) {
-    super(message);
-  }
-
   public PfbParsingException(String message, Exception e) {
     super(message, e);
   }
