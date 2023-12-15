38ee50f5b2e0d6eee832f80fa29f84bd958f918e
David An <davidan@broadinstitute.org>
2023-12-15 16:24:10 -0500

    capabilities API

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/controller/CapabilitiesController.java
@@ -0,0 +1,41 @@
+package org.databiosphere.workspacedataservice.controller;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import org.databiosphere.workspacedataservice.generated.CapabilitiesApi;
+import org.databiosphere.workspacedataservice.generated.CapabilitiesServerModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Controller for capabilities-related APIs */
+@RestController
+public class CapabilitiesController implements CapabilitiesApi {
+
+  private final CapabilitiesServerModel capabilitiesServerModel;
+
+  public CapabilitiesController(ObjectMapper objectMapper) {
+    // read the "capabilities.json" file from resources, and parse it into a CapabilitiesServerModel
+    InputStream inputStream = getClass().getResourceAsStream("/capabilities.json");
+    CapabilitiesServerModel tempModel;
+    Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    try {
+      tempModel = objectMapper.readValue(inputStream, CapabilitiesServerModel.class);
+      logger.info("Capabilities initialized: " + tempModel.entrySet());
+    } catch (IOException e) {
+      logger.error("Could not initialize capabilities: " + e.getMessage(), e);
+      tempModel = new CapabilitiesServerModel();
+    }
+    capabilitiesServerModel = tempModel;
+  }
+
+  @Override
+  @JsonPropertyOrder(alphabetic = true)
+  public ResponseEntity<CapabilitiesServerModel> capabilities() {
+    return new ResponseEntity<>(capabilitiesServerModel, HttpStatus.OK);
+  }
+}
---
+++ service/src/main/java/org/databiosphere/workspacedataservice/generated/CapabilitiesServerModel.java
@@ -0,0 +1,89 @@
+package org.databiosphere.workspacedataservice.generated;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.HashMap;
+import java.util.Map;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * CapabilitiesServerModel
+ */
+
+@JsonTypeName("Capabilities")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public class CapabilitiesServerModel extends HashMap<String, Boolean> {
+
+  private Boolean capabilities;
+
+  public CapabilitiesServerModel capabilities(Boolean capabilities) {
+    this.capabilities = capabilities;
+    return this;
+  }
+
+  /**
+   * Get capabilities
+   * @return capabilities
+  */
+  
+  @Schema(name = "capabilities", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("capabilities")
+  public Boolean getCapabilities() {
+    return capabilities;
+  }
+
+  public void setCapabilities(Boolean capabilities) {
+    this.capabilities = capabilities;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CapabilitiesServerModel capabilities = (CapabilitiesServerModel) o;
+    return Objects.equals(this.capabilities, capabilities.capabilities) &&
+        super.equals(o);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(capabilities, super.hashCode());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class CapabilitiesServerModel {\n");
+    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+    sb.append("    capabilities: ").append(toIndentedString(capabilities)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
