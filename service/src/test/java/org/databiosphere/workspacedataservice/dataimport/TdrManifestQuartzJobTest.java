34f799f32600245d824457bd1b37c2d153fb2224
David An <davidan@broadinstitute.org>
2023-12-15 11:51:06 -0500

    test cleanup

--- service/src/test/java/org/databiosphere/workspacedataservice/dataimport/TdrManifestQuartzJobTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/dataimport/TdrManifestQuartzJobTest.java
@@ -59,28 +59,34 @@ public class TdrManifestQuartzJobTest {
     SnapshotExportResponseModel snapshotExportResponseModel =
         tdrManifestQuartzJob.parseManifest(manifestAzure.getURL());
 
-    // this manifest describes tables for project, edges, test_result, genome in the snapshot
-    // schema,
+    // URLs for use in the manifest below
+    List<URL> projectParquetUrls =
+        List.of(
+            new URL(
+                "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/project.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus"));
+    List<URL> edgesParquetUrls =
+        List.of(
+            new URL(
+                "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/edges.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus"),
+            new URL(
+                "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/edges.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-2.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus"));
+    List<URL> testResultParquetUrls =
+        List.of(
+            new URL(
+                "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/test_result.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus"));
+
+    // this manifest describes tables for project, edges, test_result, genome in the snapshot,
     // but only contains export data files for project, edges, and test_result.
     List<TdrManifestImportTable> expected =
         List.of(
             // single primary key
             new TdrManifestImportTable(
-                RecordType.valueOf("project"),
-                "project_id",
-                List.of(
-                    new URL(
-                        "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/project.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus")),
-                List.of()),
+                RecordType.valueOf("project"), "project_id", projectParquetUrls, List.of()),
             // null primary key
             new TdrManifestImportTable(
                 RecordType.valueOf("edges"),
                 "datarepo_row_id",
-                List.of(
-                    new URL(
-                        "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/edges.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus"),
-                    new URL(
-                        "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/edges.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-2.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus")),
+                edgesParquetUrls,
                 List.of(
                     new RelationshipModel()
                         .name("from_edges.project_id_to_project.project_id")
@@ -91,9 +97,7 @@ public class TdrManifestQuartzJobTest {
             new TdrManifestImportTable(
                 RecordType.valueOf("test_result"),
                 "datarepo_row_id",
-                List.of(
-                    new URL(
-                        "https://mysnapshotsa.blob.core.windows.net/metadata/parquet/9516afec-583f-11ec-bf63-0242ac130002/test_result.parquet/F0EE365B-314D-4E19-A177-E8F63D883716_9274_0-1.parquet?sp=r&st=2022-08-04T15:31:55Z&se=2022-08-06T23:31:55Z&spr=https&sv=2021-06-08&sr=b&sig=bogus")),
+                testResultParquetUrls,
                 List.of()));
 
     List<TdrManifestImportTable> actual =
