b61b420df35e93c7a3df65cfde1aea0f6dd5e3a7
Josh Ladieu <111856+jladieu@users.noreply.github.com>
2023-12-22 11:50:25 -0500

    AJ-1500: Enable Prometheus metrics (#439)
    For format and usage, see:
    https://docs.spring.io/spring-boot/docs/current/actuator-api/htmlsingle/#prometheus
    
    * By default, disallow all metrics.  This is to help manage egress cost.
    * Allowlist any metrics starting with `MetricsConfig.ALLOWED_PREFIXES`;
    this list currently includes everything enabled out of the box, plus a
    `wds` prefix for our custom metrics.
    * Add a tag to every gathered metric which includes WDS version.
    * Note: we're not actually tagging metrics with a workspace ID, as this would be a high cardinality metric if aggregated globally.
    
    To see metrics while running locally:
    
    ```
    # see all metrics
    curl http://localhost:8080/prometheus
    
    # filter down to specific names
    curl http://localhost:8080/prometheus?includedNames=jvm_memory_used_bytes%2Cjvm_memory_committed_bytes
    ```

---
+++ service/src/test/java/org/databiosphere/workspacedataservice/metrics/MetricsConfigTest.java
@@ -0,0 +1,84 @@
+package org.databiosphere.workspacedataservice.metrics;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Set;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DirtiesContext
+@SpringBootTest(properties = "management.metrics.export.prometheus.enabled=true")
+@AutoConfigureMockMvc
+class MetricsConfigTest {
+  @Autowired private BuildProperties buildProperties;
+  @Autowired private MockMvc mockMvc;
+  @Autowired private MeterRegistry metrics;
+
+  @Test
+  void metricsAreTaggedWithVersion() throws Exception {
+    String expectedVersion = buildProperties.getVersion();
+    mockMvc
+        .perform(get("/prometheus"))
+        .andExpect(status().isOk())
+        .andExpect(
+            content().string(containsString("wds_version=\"%s\"".formatted(expectedVersion))));
+  }
+
+  private static Set<String> allowedPrefixes() {
+    return MetricsConfig.ALLOWED_PREFIXES;
+  }
+
+  @ParameterizedTest(name = "metrics with a \"{0}\" prefix are included in prometheus output")
+  @MethodSource("allowedPrefixes")
+  void allowlistedMetricsAreVisible(String prefix) throws Exception {
+    String randomMetricName = RandomStringUtils.randomAlphanumeric(10);
+    String expectedPrometheusString = "%s_%s".formatted(prefix, randomMetricName);
+
+    mockMvc
+        .perform(get("/prometheus"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(not(containsString(expectedPrometheusString))));
+
+    metrics.counter("%s.%s".formatted(prefix, randomMetricName)).increment();
+
+    mockMvc
+        .perform(get("/prometheus"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString(expectedPrometheusString)));
+  }
+
+  @ParameterizedTest(name = "\"{0}\" is excluded from prometheus output")
+  @ValueSource(
+      strings = {
+        "some.random.metric",
+        "foo",
+        "bar",
+        "foobar",
+        // wds is an allowed prefix, but doesn't count if it's embedded deeper in the name
+        "metric.containing.but.not.starting.with.wds"
+      })
+  void excludeMetricsThatAreNotOnAllowlist(String disallowedMetricName) throws Exception {
+    String unexpectedPrometheusString = disallowedMetricName.replace('.', '_');
+
+    metrics.counter(disallowedMetricName).increment();
+
+    mockMvc
+        .perform(get("/prometheus"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(not(containsString(unexpectedPrometheusString))));
+  }
+}
