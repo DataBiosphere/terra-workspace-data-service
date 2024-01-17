fe231e7177d73e969fc5d8294e04513c71c9ba03
Nick Watts <nwatts@broadinstitute.org>
2024-01-17 10:22:09 -0500

    Add RecordTypeSchema.getAttributeSchema

--- service/src/test/java/org/databiosphere/workspacedataservice/service/model/RecordTypeSchemaTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/model/RecordTypeSchemaTest.java
@@ -1,10 +1,13 @@
 package org.databiosphere.workspacedataservice.service.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,5 +39,18 @@ class RecordTypeSchemaTest {
   void testContainsAttribute() {
     assertTrue(schema.containsAttribute("attr1"));
     assertFalse(schema.containsAttribute("doesNotExist"));
+  }
+
+  @Test
+  void testGetAttributeSchema() {
+    assertEquals(schema.getAttributeSchema("attr1"), new AttributeSchema("attr1", "STRING", null));
+  }
+
+  @Test
+  void testGetNonExistentAttributeSchema() {
+    assertThrows(
+        NoSuchElementException.class,
+        () -> schema.getAttributeSchema("doesNotExist"),
+        "getAttributeSchema should have thrown an error");
   }
 }
