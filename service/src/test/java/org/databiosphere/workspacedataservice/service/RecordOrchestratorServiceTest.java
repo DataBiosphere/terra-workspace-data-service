19a0155056241ce2e89aa5e5e0e2215dca5bf2cf
Nick Watts <nwatts@broadinstitute.org>
2024-01-25 10:09:42 -0500

    Move test argument providers to RecordOrchestratorServiceTest

--- service/src/test/java/org/databiosphere/workspacedataservice/service/RecordOrchestratorServiceTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/RecordOrchestratorServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -29,6 +30,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -204,12 +206,12 @@ class RecordOrchestratorServiceTest {
 
   @ParameterizedTest(name = "update attribute data type from {1} to {2}")
   @MethodSource({
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#stringConversions",
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#stringArrayConversions",
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#numberConversions",
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#numberArrayConversions",
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#booleanConversions",
-    "org.databiosphere.workspacedataservice.DataTypeConversionTestCases#booleanArrayConversions"
+    "updateAttributeDataTypeStringConversions",
+    "updateAttributeDataTypeStringArrayConversions",
+    "updateAttributeDataTypeNumberConversions",
+    "updateAttributeDataTypeNumberArrayConversions",
+    "updateAttributeDataTypeBooleanConversions",
+    "updateAttributeDataTypeBooleanArrayConversions"
   })
   void updateAttributeDataType(
       Object attributeValue,
@@ -245,6 +247,202 @@ class RecordOrchestratorServiceTest {
         "expected %s %s to be converted to %s %s"
             .formatted(
                 expectedInitialDataType, attributeValue, newDataType, expectedFinalAttributeValue));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeStringConversions() {
+    return Stream.of(
+        // Number
+        Arguments.of(
+            "123", DataTypeMapping.STRING, DataTypeMapping.NUMBER, BigDecimal.valueOf(123)),
+        // Boolean
+        Arguments.of("yes", DataTypeMapping.STRING, DataTypeMapping.BOOLEAN, Boolean.TRUE),
+        Arguments.of("no", DataTypeMapping.STRING, DataTypeMapping.BOOLEAN, Boolean.FALSE),
+        // String array
+        Arguments.of(
+            "foo", DataTypeMapping.STRING, DataTypeMapping.ARRAY_OF_STRING, new String[] {"foo"}),
+        // Number array
+        Arguments.of(
+            "123",
+            DataTypeMapping.STRING,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(123)}),
+        // Boolean array
+        Arguments.of(
+            "yes",
+            DataTypeMapping.STRING,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.TRUE}));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeStringArrayConversions() {
+    return Stream.of(
+        // String
+        Arguments.of(
+            List.of("foo"), DataTypeMapping.ARRAY_OF_STRING, DataTypeMapping.STRING, "foo"),
+        // Number
+        Arguments.of(
+            List.of("123"),
+            DataTypeMapping.ARRAY_OF_STRING,
+            DataTypeMapping.NUMBER,
+            BigDecimal.valueOf(123)),
+        // Boolean
+        Arguments.of(
+            List.of("yes"), DataTypeMapping.ARRAY_OF_STRING, DataTypeMapping.BOOLEAN, Boolean.TRUE),
+        // Number array
+        Arguments.of(
+            List.of("123"),
+            DataTypeMapping.ARRAY_OF_STRING,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(123)}),
+        // Boolean array
+        Arguments.of(
+            List.of("yes", "no"),
+            DataTypeMapping.ARRAY_OF_STRING,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.TRUE, Boolean.FALSE}));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeNumberConversions() {
+    return Stream.of(
+        // String
+        Arguments.of(
+            BigDecimal.valueOf(123), DataTypeMapping.NUMBER, DataTypeMapping.STRING, "123"),
+        // Boolean
+        Arguments.of(
+            BigDecimal.valueOf(1), DataTypeMapping.NUMBER, DataTypeMapping.BOOLEAN, Boolean.TRUE),
+        Arguments.of(
+            BigDecimal.valueOf(0), DataTypeMapping.NUMBER, DataTypeMapping.BOOLEAN, Boolean.FALSE),
+        // String array
+        Arguments.of(
+            BigDecimal.valueOf(123),
+            DataTypeMapping.NUMBER,
+            DataTypeMapping.ARRAY_OF_STRING,
+            new String[] {"123"}),
+        // Number array
+        Arguments.of(
+            BigDecimal.valueOf(123),
+            DataTypeMapping.NUMBER,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(123)}),
+        // Boolean array
+        Arguments.of(
+            BigDecimal.valueOf(1),
+            DataTypeMapping.NUMBER,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.TRUE}));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeNumberArrayConversions() {
+    return Stream.of(
+        // String
+        Arguments.of(
+            List.of(BigDecimal.valueOf(123)),
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            DataTypeMapping.STRING,
+            "123"),
+        // Number
+        Arguments.of(
+            List.of(BigDecimal.valueOf(123)),
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            DataTypeMapping.NUMBER,
+            BigDecimal.valueOf(123)),
+        // Boolean
+        Arguments.of(
+            List.of(BigDecimal.valueOf(1)),
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            DataTypeMapping.BOOLEAN,
+            Boolean.TRUE),
+        // String array
+        Arguments.of(
+            List.of(BigDecimal.valueOf(123)),
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            DataTypeMapping.ARRAY_OF_STRING,
+            new String[] {"123"}),
+        // Boolean array
+        Arguments.of(
+            List.of(BigDecimal.valueOf(1), BigDecimal.valueOf(0)),
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.TRUE, Boolean.FALSE}));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeBooleanConversions() {
+    return Stream.of(
+        // String
+        Arguments.of(Boolean.TRUE, DataTypeMapping.BOOLEAN, DataTypeMapping.STRING, "true"),
+        Arguments.of(Boolean.FALSE, DataTypeMapping.BOOLEAN, DataTypeMapping.STRING, "false"),
+        // Number
+        Arguments.of(
+            Boolean.TRUE, DataTypeMapping.BOOLEAN, DataTypeMapping.NUMBER, BigDecimal.valueOf(1)),
+        Arguments.of(
+            Boolean.FALSE, DataTypeMapping.BOOLEAN, DataTypeMapping.NUMBER, BigDecimal.valueOf(0)),
+        // String array
+        Arguments.of(
+            Boolean.TRUE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_STRING,
+            new String[] {"true"}),
+        Arguments.of(
+            Boolean.FALSE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_STRING,
+            new String[] {"false"}),
+        // Number array
+        Arguments.of(
+            Boolean.TRUE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(1)}),
+        Arguments.of(
+            Boolean.FALSE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(0)}),
+        // Boolean array
+        Arguments.of(
+            Boolean.TRUE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.TRUE}),
+        Arguments.of(
+            Boolean.FALSE,
+            DataTypeMapping.BOOLEAN,
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            new Boolean[] {Boolean.FALSE}));
+  }
+
+  static Stream<Arguments> updateAttributeDataTypeBooleanArrayConversions() {
+    return Stream.of(
+        // String
+        Arguments.of(
+            List.of(Boolean.TRUE),
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            DataTypeMapping.STRING,
+            "true"),
+        // Number
+        Arguments.of(
+            List.of(Boolean.TRUE),
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            DataTypeMapping.NUMBER,
+            BigDecimal.valueOf(1)),
+        // Boolean
+        Arguments.of(
+            List.of(Boolean.TRUE),
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            DataTypeMapping.BOOLEAN,
+            Boolean.TRUE),
+        // String array
+        Arguments.of(
+            List.of(Boolean.TRUE, Boolean.FALSE),
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            DataTypeMapping.ARRAY_OF_STRING,
+            new String[] {"true", "false"}),
+        // Number array
+        Arguments.of(
+            List.of(Boolean.TRUE, Boolean.FALSE),
+            DataTypeMapping.ARRAY_OF_BOOLEAN,
+            DataTypeMapping.ARRAY_OF_NUMBER,
+            new BigDecimal[] {BigDecimal.valueOf(1), BigDecimal.valueOf(0)}));
   }
 
   @Test
