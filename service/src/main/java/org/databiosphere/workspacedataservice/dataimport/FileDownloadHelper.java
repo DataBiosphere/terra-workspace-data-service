31de46d774063aa9c39ba0fa1cb15c460ce019ab
Bria Morgan <bmorgan@broadinstitute.org>
2024-01-25 12:48:18 -0500

    use permissions to satisfy sonar

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/FileDownloadHelper.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/FileDownloadHelper.java
@@ -7,8 +7,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.EnumSet;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
 import org.apache.commons.io.FileUtils;
 import org.databiosphere.workspacedataservice.service.model.exception.TdrManifestImportException;
@@ -20,30 +21,32 @@ public class FileDownloadHelper {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
   private final Path tempFileDir;
   private final Multimap<String, File> fileMap;
-  private final Set<PosixFilePermission> permissions = EnumSet.of(PosixFilePermission.OWNER_READ);
+  private final FileAttribute<Set<PosixFilePermission>> permissions =
+      PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwx------"));
 
   public FileDownloadHelper(String dirName) throws IOException {
-    this.tempFileDir = Files.createTempDirectory(dirName);
+    this.tempFileDir = Files.createTempDirectory(dirName, permissions);
     this.fileMap = HashMultimap.create();
   }
 
   public void downloadFileFromURL(String tableName, URL pathToRemoteFile) {
     try {
-      File tempFile = File.createTempFile(/* prefix= */ "tdr-", /* suffix= */ "download");
-      logger.info("downloading to temp file {} ...", tempFile.getPath());
-      FileUtils.copyURLToFile(pathToRemoteFile, tempFile);
+      Path tempFilePath =
+          Files.createTempFile(
+              tempFileDir, /* prefix= */ "tdr-", /* suffix= */ "download", permissions);
+      logger.info("downloading to temp file {} ...", tempFilePath.toString());
+      FileUtils.copyURLToFile(pathToRemoteFile, tempFilePath.toFile());
       // In the TDR manifest, for Azure snapshots only,
       // the first file in the list will always be a directory.
       // Attempting to import that directory
       // will fail; it has no content. To avoid those failures,
       // check files for length and ignore any that are empty
-      if (tempFile.length() == 0) {
+      if (tempFilePath.toFile().length() == 0) {
         logger.info("Empty file in parquet, skipping");
-        Files.delete(tempFile.toPath());
+        Files.delete(tempFilePath);
       } else {
         // Once the remote file has been copied to the temp file, make it read-only
-        Files.setPosixFilePermissions(tempFile.toPath(), permissions);
-        fileMap.put(tableName, tempFile);
+        fileMap.put(tableName, tempFilePath.toFile());
       }
     } catch (IOException e) {
       throw new TdrManifestImportException(e.getMessage(), e);
