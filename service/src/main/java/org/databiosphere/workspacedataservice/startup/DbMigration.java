af6afb11a31a543d5979541e06c8930eeca173de
David An <davidan@broadinstitute.org>
2024-01-31 17:03:34 -0500

    update job and instance tables

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/startup/DbMigration.java
@@ -0,0 +1,38 @@
+package org.databiosphere.workspacedataservice.startup;
+
+import bio.terra.common.db.WriteTransaction;
+import jakarta.annotation.PostConstruct;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbMigration {
+
+  @Value("${twds.instance.workspace-id}")
+  private UUID workspaceId;
+
+  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+  public DbMigration(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+  }
+
+  @PostConstruct
+  @WriteTransaction
+  void migrateInstances() {
+    var params = new MapSqlParameterSource("workspaceId", workspaceId);
+    namedParameterJdbcTemplate.update(
+        "update sys_wds.instance set workspace_id = :workspaceId where workspace_id is null",
+        params);
+    namedParameterJdbcTemplate.update(
+        "update sys_wds.instance set name = 'default' where name is null and id = :workspaceId",
+        params);
+    namedParameterJdbcTemplate.update(
+        "update sys_wds.instance set name = left(id::text,8) where name is null;", params);
+
+    // after these updates, it is safe to set the table to be non-null
+  }
+}
