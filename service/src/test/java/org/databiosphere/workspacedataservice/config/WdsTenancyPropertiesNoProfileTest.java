09ccfd40b958a70f397d110cde94afc73fb90d70
David An <davidan@broadinstitute.org>
2024-02-08 09:15:57 -0500

    make profile explicit in the 'neither' test

--- service/src/test/java/org/databiosphere/workspacedataservice/config/WdsTenancyPropertiesNoProfileTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/config/WdsTenancyPropertiesNoProfileTest.java
@@ -5,12 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * when neither the "data-plane" nor "control-plane" profile is active, WdsTenancyProperties are
  * null
  */
 @SpringBootTest
+@ActiveProfiles("neither-data-plane-nor-control-plane")
 public class WdsTenancyPropertiesNoProfileTest {
 
   @Autowired WdsTenancyProperties wdsTenancyProperties;
