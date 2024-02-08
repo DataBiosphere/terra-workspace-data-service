f5b4cb1d28ccf2ffdb8feb3020b18e8666244af5
David An <davidan@broadinstitute.org>
2024-02-08 08:40:30 -0500

    javadoc

--- service/src/main/java/org/databiosphere/workspacedataservice/config/WdsTenancyProperties.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/config/WdsTenancyProperties.java
@@ -2,11 +2,23 @@ package org.databiosphere.workspacedataservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * Configuration properties related to multi- or single-tenancy of workspaces within this WDS
+ * deployment.
+ */
 @ConfigurationProperties(prefix = "twds.tenancy")
 public class WdsTenancyProperties {
   private Boolean allowVirtualCollections;
   private Boolean requireEnvWorkspace;
 
+  /**
+   * Does this WDS deployment allow virtual collections? That is, does it allow for a collection
+   * (f/k/a "instance") which does not have a corresponding row in the sys_wds.instance table? The
+   * primary use case for virtual collections is when running as cWDS for imports only. Virtual
+   * collections will always have collection id == workspace id.
+   *
+   * @return Does this WDS deployment allow virtual collections?
+   */
   public Boolean getAllowVirtualCollections() {
     return allowVirtualCollections;
   }
@@ -15,6 +27,13 @@ public class WdsTenancyProperties {
     this.allowVirtualCollections = allowVirtualCollections;
   }
 
+  /**
+   * Does this WDS deployment require a $WORKSPACE_ID environment variable to be set? When this env
+   * var is set, the WDS deployment is tied to a single workspace, and all collections within this
+   * WDS will be associated with that workspace.
+   *
+   * @return Does this WDS deployment require a $WORKSPACE_ID environment variable to be set?
+   */
   public Boolean getRequireEnvWorkspace() {
     return requireEnvWorkspace;
   }
