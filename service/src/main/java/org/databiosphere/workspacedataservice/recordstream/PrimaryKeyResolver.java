efada1aa5e1b949d84f2ca35ed936475bb0fa143
Josh Ladieu <ladieu@broadinstitute.org>
2024-02-09 11:12:11 -0500

    Expand clarifying comment on `PrimaryKeyResolver`

--- service/src/main/java/org/databiosphere/workspacedataservice/recordstream/PrimaryKeyResolver.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/recordstream/PrimaryKeyResolver.java
@@ -2,7 +2,8 @@ package org.databiosphere.workspacedataservice.recordstream;
 
 /**
  * Simple interface to clarify the additional responsibility that {@link TsvStreamWriteHandler}
- * provides.
+ * provides. This interface is not strictly necessary, but I wanted it to be a clear marker of where
+ * the additional responsibility is used to make it easier to refactor and tidy up later.
  */
 public interface PrimaryKeyResolver {
   String getPrimaryKey();
