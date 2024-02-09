3b725b86cdcc4e28247e27ed58c33ec5407a16b7
Nick Watts <nwatts@broadinstitute.org>
2024-02-09 10:49:48 -0500

    Add helper to create empty RelationCollection

--- service/src/main/java/org/databiosphere/workspacedataservice/service/model/RelationCollection.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/model/RelationCollection.java
@@ -1,5 +1,10 @@
 package org.databiosphere.workspacedataservice.service.model;
 
+import java.util.Collections;
 import java.util.Set;
 
-public record RelationCollection(Set<Relation> relations, Set<Relation> relationArrays) {}
+public record RelationCollection(Set<Relation> relations, Set<Relation> relationArrays) {
+  public static RelationCollection empty() {
+    return new RelationCollection(Collections.emptySet(), Collections.emptySet());
+  }
+}
