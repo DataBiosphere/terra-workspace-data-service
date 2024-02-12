449a718f8925e5457196ee75e44d8dd22cb8766f
David An <davidan@broadinstitute.org>
2024-02-12 14:59:59 -0500

    Revert "re-add EnableConfigurationProperties"
    This reverts commit a8754718e687993e4d6a26889dadd6d314fa0c29.

--- service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
@@ -1,10 +1,8 @@
 package org.databiosphere.workspacedataservice;
 
 import com.microsoft.applicationinsights.attach.ApplicationInsights;
-import org.databiosphere.workspacedataservice.config.TwdsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,7 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableRetry
 @EnableTransactionManagement
 @EnableCaching
-@EnableConfigurationProperties(TwdsProperties.class)
 public class WorkspaceDataServiceApplication {
 
   public static void main(String[] args) {
