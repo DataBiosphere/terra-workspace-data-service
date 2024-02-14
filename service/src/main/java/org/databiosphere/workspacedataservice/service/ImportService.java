a2457f05a15322aea211a862ae6c8c1a6a98a682
David An <davidan@broadinstitute.org>
2024-02-14 12:41:00 -0500

    spotless

--- service/src/main/java/org/databiosphere/workspacedataservice/service/ImportService.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/ImportService.java
@@ -110,8 +110,8 @@ public class ImportService {
       UUID jobId,
       Map<String, Serializable> arguments) {
     return switch (importType) {
-      case TDRMANIFEST ->
-          new TdrManifestSchedulable(jobId.toString(), "TDR manifest import", arguments);
+      case TDRMANIFEST -> new TdrManifestSchedulable(
+          jobId.toString(), "TDR manifest import", arguments);
       case PFB -> new PfbSchedulable(jobId.toString(), "TODO: PFB import", arguments);
     };
   }
