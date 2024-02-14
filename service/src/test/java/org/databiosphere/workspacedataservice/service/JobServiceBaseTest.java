e1f5090cd3056644bd3afe256409428a0925e441
David An <davidan@broadinstitute.org>
2024-02-14 16:16:57 -0500

    cleaner import for JobServiceBaseTest

--- service/src/test/java/org/databiosphere/workspacedataservice/service/JobServiceBaseTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/JobServiceBaseTest.java
@@ -7,21 +7,19 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 import org.databiosphere.workspacedataservice.generated.GenericJobServerModel;
+import org.databiosphere.workspacedataservice.generated.GenericJobServerModel.StatusEnum;
 import org.databiosphere.workspacedataservice.shared.model.CollectionId;
 
 public abstract class JobServiceBaseTest {
 
-  List<String> allStatuses =
-      Arrays.stream(GenericJobServerModel.StatusEnum.values())
-          .map(GenericJobServerModel.StatusEnum::toString)
-          .toList();
+  List<String> allStatuses = Arrays.stream(StatusEnum.values()).map(StatusEnum::toString).toList();
 
   GenericJobServerModel makeJob(UUID jobId, CollectionId collectionId) {
     return new GenericJobServerModel(
         jobId,
         GenericJobServerModel.JobTypeEnum.DATA_IMPORT,
         collectionId.id(),
-        GenericJobServerModel.StatusEnum.RUNNING,
+        StatusEnum.RUNNING,
         // set created and updated to now, but in UTC because that's how Postgres stores it
         OffsetDateTime.now(ZoneId.of("Z")),
         OffsetDateTime.now(ZoneId.of("Z")));
