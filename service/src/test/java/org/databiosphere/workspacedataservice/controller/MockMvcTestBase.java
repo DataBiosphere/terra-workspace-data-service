d9b99c48157f32fdc633b78aa2487e545d7a54af
Josh Ladieu <ladieu@broadinstitute.org>
2024-02-21 17:29:39 -0500

    Don't add mock-sam to MockMvcTestBase

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/MockMvcTestBase.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/MockMvcTestBase.java
@@ -9,13 +9,11 @@ import org.databiosphere.workspacedataservice.common.TestBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@ActiveProfiles({"mock-sam"})
 class MockMvcTestBase extends TestBase {
   @Autowired private ObjectMapper mapper;
   @Autowired protected MockMvc mockMvc;
