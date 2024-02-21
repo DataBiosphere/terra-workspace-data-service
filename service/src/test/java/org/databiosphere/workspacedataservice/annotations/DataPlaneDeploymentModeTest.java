235e9e8dcea5ad321443fb8cbf274533ce73ce0c
Adina Shanholtz <adinas@microsoft.com>
2024-02-21 12:13:20 -0800

    logback and disable changes

--- service/src/test/java/org/databiosphere/workspacedataservice/annotations/DataPlaneDeploymentModeTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/annotations/DataPlaneDeploymentModeTest.java
@@ -1,10 +1,13 @@
 package org.databiosphere.workspacedataservice.annotations;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.databiosphere.workspacedataservice.annotations.DeploymentMode.DataPlane;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({"data-plane"})
@@ -16,11 +19,10 @@ class DataPlaneDeploymentModeTest extends DeploymentModeTestBase {
     assertNotNull(context.findAnnotationOnBean(beanName, DataPlane.class));
   }
 
-  /* TODO: Enable when we have parameters
-   @ParameterizedTest(name = "{0} is disabled for DeploymentMode data-plane")
-   @MethodSource("controlPlaneConditionalBeans")
-   void beansDisabledForDataPlane(String beanName) {
-       assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(beanName));
-   }
-  */
+  @Disabled("Test disabled until we have parameters in controlPlaneConditionalBeans")
+  @ParameterizedTest(name = "{0} is disabled for DeploymentMode data-plane")
+  @MethodSource("controlPlaneConditionalBeans")
+  void beansDisabledForDataPlane(String beanName) {
+    assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(beanName));
+  }
 }
