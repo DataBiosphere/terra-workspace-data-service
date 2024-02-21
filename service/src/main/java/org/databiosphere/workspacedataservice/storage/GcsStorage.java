361b24dbc302cf7a70feeb08e1b3bf0b332b8eee
yuliadub <yulia.dubinina@gmail.com>
2024-02-21 10:30:26 -0800

    bucket code almost ready

--- service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorage.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorage.java
@@ -1,7 +1,6 @@
 package org.databiosphere.workspacedataservice.storage;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
@@ -15,47 +14,36 @@ import org.springframework.beans.factory.annotation.Value;
 public class GcsStorage {
   private Storage storage;
 
-  @Value("${gcs-resource-test-bucket}")
+  @Value("${twds.bucket.name}")
   private String bucketName;
 
-  @Value("${gcs-resource-projectId}")
+  @Value("${twds.bucket.projectId}")
   private String projectId;
 
   public GcsStorage() throws IOException {
     GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
 
     StorageOptions storageOptions =
-    StorageOptions.newBuilder()
-        .setProjectId(projectId) //need to figure out what this should be)
-        .setCredentials(credentials)
-        .build();
+        StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build();
     this.storage = storageOptions.getService();
   }
 
-  public String getBlobContents(String projectId, String path) {
-    BlobId blobId = GcsUriUtils.parseBlobUri(path);
-    var blob = storage.get(blobId, Storage.BlobGetOption.userProject(projectId));
-    var contents = blob.getContent(Blob.BlobSourceOption.userProject(projectId));
+  public GcsStorage(Storage storage, String bucketName, String projectId) {
+    this.storage = storage;
+    this.bucketName = bucketName;
+    this.projectId = projectId;
+  }
+
+  public String getBlobContents(String blobName) {
+    BlobId blobId = BlobId.of(bucketName, blobName);
+    var blob = storage.get(blobId);
+    var contents = blob.getContent();
     return new String(contents, StandardCharsets.UTF_8);
   }
 
-  /**
-   * Create a file in GCS
-   *
-   * @param projectId project id for billing
-   * @return the Blob of the created file
-   */
-  public BlobId createGcsFile(String projectId, InputStream contents) {
+  public BlobId createGcsFile(InputStream contents) {
     BlobId blobId = BlobId.of(bucketName, UUID.randomUUID().toString());
     storage.create(BlobInfo.newBuilder(blobId).build(), contents);
     return blobId;
-  }
-
-  public static Blob getBlobFromGsPath(Storage storage, String gspath) {
-    BlobId locator = GcsUriUtils.parseBlobUri(gspath);
-    Storage.BlobGetOption[] getOptions = new Storage.BlobGetOption[0];
-    Blob sourceBlob = storage.get(locator, getOptions);
-
-    return sourceBlob;
   }
 }
--- service/src/test/java/org/databiosphere/workspacedataservice/storage/GcsStorageTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/storage/GcsStorageTest.java
@@ -1,51 +1,32 @@
 package org.databiosphere.workspacedataservice.storage;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import java.io.IOException;
-import java.util.UUID;
+import com.google.cloud.storage.contrib.nio.testing.LocalStorageHelper;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.junit.jupiter.api.Test;
 
 public class GcsStorageTest {
-  private static final GoogleCredentials credentials;
 
-  static {
-    try {
-      credentials = GoogleCredentials.getApplicationDefault();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
+  private String bucketName = "test-bucket";
 
-  private final Storage storage =
-      StorageOptions.newBuilder()
-          .setProjectId("broad-dsp-gcr-public") // need to figure out what this should be)
-          .setCredentials(credentials)
-          .build()
-          .getService();
-  private final String projectId = StorageOptions.getDefaultProjectId();
+  private String projectId = "test-projectId";
 
-  public GcsStorageTest() throws IOException {}
+  private final Storage mockStorage = LocalStorageHelper.getOptions().getService();
 
   @Test
-  public void testGetBlobSimple() {
-    BlobId testBlob = BlobId.of("yuliadub-test-bucket", UUID.randomUUID().toString());
+  public void testCreateandGetBlobSimple() {
+    GcsStorage storage = new GcsStorage(mockStorage, bucketName, projectId);
+    String initialString = "text";
+    InputStream targetStream = new ByteArrayInputStream(initialString.getBytes());
+    BlobId blobId = storage.createGcsFile(targetStream);
 
-    storage.create(BlobInfo.newBuilder(testBlob).build());
-    Blob blob =
-        GcsStorage.getBlobFromGsPath(
-            storage, "gs://" + testBlob.getBucket() + "/" + testBlob.getName());
-    assertNotNull(blob);
+    assertEquals(bucketName, blobId.getBucket());
 
-    BlobId actualId = blob.getBlobId();
-    assertEquals(testBlob.getBucket(), actualId.getBucket());
-    assertEquals(testBlob.getName(), actualId.getName());
+    var contents = storage.getBlobContents(blobId.getName());
+    assertEquals(new String(contents), initialString);
   }
 }
