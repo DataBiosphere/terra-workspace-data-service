943b1f0db8e78f8fa981cba76fd682050c018ea3
Nick Watts <nwatts@broadinstitute.org>
2024-02-20 22:41:38 -0500

    Update test

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/RecordControllerMockMvcTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/RecordControllerMockMvcTest.java
@@ -774,7 +774,7 @@ class RecordControllerMockMvcTest extends MockMvcTestBase {
                     "attr1")
                 .content(toJson(recordRequest))
                 .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated());
+        .andExpect(status().isBadRequest());
   }
 
   @Test
