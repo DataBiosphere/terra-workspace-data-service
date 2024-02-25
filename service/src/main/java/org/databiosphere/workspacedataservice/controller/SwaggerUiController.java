c3b591d845480afeb87648e5ec311e31ecee6a9c
David An <davidan@broadinstitute.org>
2024-02-25 10:18:04 -0500

    remove static html; add redirects

--- service/src/main/java/org/databiosphere/workspacedataservice/controller/SwaggerUiController.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/controller/SwaggerUiController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SwaggerUiController {
@@ -19,7 +21,13 @@ public class SwaggerUiController {
   @Value("classpath:swagger-ui-template.html")
   Resource swaggerUiResource;
 
-  @GetMapping("/api")
+  // redirect "/", "/swagger" and "/swagger/" to "/swagger/swagger-ui.html"
+  @GetMapping({"/", "/swagger", "/swagger/"})
+  public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
+    return new ModelAndView("redirect:/swagger/swagger-ui.html", model);
+  }
+
+  @GetMapping("/swagger/swagger-ui.html")
   public ResponseEntity<String> getSwaggerUi() throws IOException {
     String template = new String(Files.readAllBytes(swaggerUiResource.getFile().toPath()));
 
