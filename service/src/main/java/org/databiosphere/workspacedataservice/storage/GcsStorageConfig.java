86285eae595718c9b731a004d0b1dd4d8efdedbc
yuliadub <yulia.dubinina@gmail.com>
2024-02-26 19:40:32 -0800

    more adjusting

--- service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import org.databiosphere.workspacedataservice.config.DataImportProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GcsStorageConfig {
   @Bean
-  @Profile("control-plane")
   public GcsStorage GcsStorage(DataImportProperties properties) throws IOException {
     return new GcsStorage(properties);
   }
--- service/src/test/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
@@ -2,10 +2,12 @@ package org.databiosphere.workspacedataservice.storage;
 
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.contrib.nio.testing.LocalStorageHelper;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SpringBootTest
 public class GcsStorageConfig {
   private String bucketName = "test-bucket";
 
