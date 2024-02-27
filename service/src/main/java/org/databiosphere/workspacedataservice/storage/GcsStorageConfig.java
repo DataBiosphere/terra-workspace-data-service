02f23cd94822cf791c5a7e53fc332e1c6b5e5e77
yuliadub <yulia.dubinina@gmail.com>
2024-02-27 14:19:39 -0800

    set autowire to false

--- service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
@@ -1,13 +1,17 @@
 package org.databiosphere.workspacedataservice.storage;
 
 import java.io.IOException;
+import org.databiosphere.workspacedataservice.annotations.DeploymentMode.ControlPlane;
 import org.databiosphere.workspacedataservice.config.DataImportProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ControlPlane
 public class GcsStorageConfig {
   @Bean
+  @Autowired(required = false)
   public GcsStorage GcsStorage(DataImportProperties properties) throws IOException {
     return new GcsStorage(properties);
   }
