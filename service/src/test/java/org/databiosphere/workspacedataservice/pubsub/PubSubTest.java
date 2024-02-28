edc286beac5bf7ce4ca1cb16c731ee0ee151bf04
Bria Morgan <bmorgan@broadinstitute.org>
2024-02-28 10:35:14 -0500

    directly use google cloud pubsub

--- service/src/test/java/org/databiosphere/workspacedataservice/pubsub/PubSubTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/pubsub/PubSubTest.java
@@ -1,5 +1,7 @@
 package org.databiosphere.workspacedataservice.pubsub;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,7 @@ public class PubSubTest {
   @Autowired PubSub pubSub;
 
   @Test
-  void testPubSub() {
+  void testPubSub() throws IOException, ExecutionException, InterruptedException {
     pubSub.publish("testing");
   }
 }
