1fff4b5853a82b44ecc437d75bee3d4a3e15f1c2
Josh Ladieu <111856+jladieu@users.noreply.github.com>
2024-03-05 17:59:06 -0500

    AJ-1665: Enable null-safety warnings.
    * Add `@NonNullApi` and `@NonNullFields` to each package, giving
    the IDE a hint to guide all code to be explicit about nullability.
    * Add `generate-package-info.sh` to automatically regenerate
    `package-info.java` files, which would otherwise be very tedious in the
    event we want to change their contents globally.

--- service/src/main/java/bio/terra/common/db/package-info.java
+++ service/src/main/java/bio/terra/common/db/package-info.java
@@ -1,6 +1,6 @@
 @NonNullApi
 @NonNullFields
-package org.databiosphere.workspacedataservice.sam;
+package bio.terra.common.db;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
