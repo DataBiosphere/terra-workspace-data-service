cffc81054b9163061421971db328c0e51d7d4e15
Nick Watts <nwatts@broadinstitute.org>
2024-03-13 09:49:17 -0400

    Add configuration for allowed import sources

--- service/src/main/java/org/databiosphere/workspacedataservice/config/DataImportProperties.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/config/DataImportProperties.java
@@ -1,11 +1,30 @@
 package org.databiosphere.workspacedataservice.config;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import com.google.common.collect.Sets;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /** Properties that dictate how data import processes should behave. */
 public class DataImportProperties {
   private RecordSinkMode batchWriteRecordSink;
   private String projectId;
   private String rawlsBucketName;
   private boolean succeedOnCompletion;
+  private final Set<AllowedImportSource> defaultAllowedImportSources =
+      Set.of(
+          new AllowedImportSource("storage.googleapis.com"),
+          new AllowedImportSource("*.core.windows.net"),
+          // S3 allows multiple URL formats
+          // https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html
+          new AllowedImportSource("s3.amazonaws.com"), // path style legacy global endpoint
+          new AllowedImportSource("*.s3.amazonaws.com") // virtual host style legacy global endpoint
+          );
+  private Set<AllowedImportSource> allowedImportSources = Collections.emptySet();
 
   /** Where to write records after import, options are defined by {@link RecordSinkMode} */
   public RecordSinkMode getBatchWriteRecordSink() {
@@ -50,6 +69,23 @@ public class DataImportProperties {
     this.succeedOnCompletion = succeedOnCompletion;
   }
 
+  /**
+   * Accepted sources for imported files. This includes configured sources as well as default /
+   * always allowed sources (GCS buckets, Azure storage containers, and S3 buckets).
+   */
+  public Set<AllowedImportSource> getAllowedImportSources() {
+    return Sets.union(defaultAllowedImportSources, allowedImportSources);
+  }
+
+  public void setAllowedImportSources(String allowedImportSources) {
+    this.allowedImportSources =
+        isBlank(allowedImportSources)
+            ? Collections.emptySet()
+            : Arrays.stream(allowedImportSources.split(","))
+                .map(AllowedImportSource::new)
+                .collect(Collectors.toSet());
+  }
+
   /** Dictates the sink where BatchWriteService should write records after import. */
   public enum RecordSinkMode {
     WDS("wds"),
@@ -67,6 +103,22 @@ public class DataImportProperties {
         }
       }
       throw new RuntimeException("Unknown RecordSinkMode value: %s".formatted(value));
+    }
+  }
+
+  public class AllowedImportSource {
+    private String hostPattern;
+
+    public AllowedImportSource(String hostPattern) {
+      this.hostPattern = hostPattern;
+    }
+
+    public boolean matchesUrl(URI url) {
+      if (hostPattern.startsWith("*")) {
+        return url.getHost().endsWith(hostPattern.substring(1));
+      } else {
+        return url.getHost().equals(hostPattern);
+      }
     }
   }
 }
