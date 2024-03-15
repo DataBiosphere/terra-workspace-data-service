66f900b06186eb68c5c68c49bf54ad6e02e626de
Nick Watts <nwatts@broadinstitute.org>
2024-03-15 15:03:24 -0400

    Stored allowed schemes/host instead of data import properties

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/ImportSourceValidator.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/ImportSourceValidator.java
@@ -1,6 +1,8 @@
 package org.databiosphere.workspacedataservice.dataimport;
 
 import java.net.URI;
+import java.util.Set;
+import java.util.regex.Pattern;
 import org.databiosphere.workspacedataservice.config.DataImportProperties;
 import org.databiosphere.workspacedataservice.generated.ImportRequestServerModel;
 import org.databiosphere.workspacedataservice.service.model.exception.ValidationException;
@@ -8,16 +10,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ImportSourceValidator {
-  private final DataImportProperties dataImportProperties;
+  private final Set<String> allowedSchemes;
+  private final Set<Pattern> allowedHosts;
 
   public ImportSourceValidator(DataImportProperties dataImportProperties) {
-    this.dataImportProperties = dataImportProperties;
+    this.allowedSchemes = dataImportProperties.getAllowedSchemes();
+    this.allowedHosts = dataImportProperties.getAllowedHosts();
   }
 
   public void validateImportRequest(ImportRequestServerModel importRequest) {
     URI importUrl = importRequest.getUrl();
 
-    if (!dataImportProperties.getAllowedSchemes().contains(importUrl.getScheme())) {
+    if (!allowedSchemes.contains(importUrl.getScheme())) {
       throw new ValidationException(
           "Files may not be imported from %s URLs.".formatted(importUrl.getScheme()));
     }
@@ -25,7 +29,7 @@ public class ImportSourceValidator {
     // File URLs don't have a host to validate.
     boolean isFileUrl = importUrl.getScheme().equals("file");
     if (!isFileUrl
-        && dataImportProperties.getAllowedHosts().stream()
+        && allowedHosts.stream()
             .noneMatch(allowedHost -> allowedHost.matcher(importUrl.getHost()).matches())) {
       throw new ValidationException(
           "Files may not be imported from %s.".formatted(importUrl.getHost()));
