8b3b6fd3f9378fd8acacca4cd1c115266427c644
Nick Watts <nwatts@broadinstitute.org>
2024-03-20 14:21:03 -0400

    Simplify test configuration

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/PubSubControllerMockMvcTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/PubSubControllerMockMvcTest.java
@@ -6,37 +6,26 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.Map;
 import java.util.UUID;
-import org.databiosphere.workspacedataservice.activitylog.ActivityLogger;
 import org.databiosphere.workspacedataservice.dao.CollectionDao;
 import org.databiosphere.workspacedataservice.dao.JobDao;
-import org.databiosphere.workspacedataservice.dataimport.snapshotsupport.RawlsSnapshotSupportFactory;
-import org.databiosphere.workspacedataservice.dataimport.snapshotsupport.SnapshotSupportFactory;
 import org.databiosphere.workspacedataservice.generated.GenericJobServerModel;
-import org.databiosphere.workspacedataservice.pubsub.PubSub;
 import org.databiosphere.workspacedataservice.pubsub.PubSubMessage;
 import org.databiosphere.workspacedataservice.pubsub.PubSubRequest;
-import org.databiosphere.workspacedataservice.rawls.RawlsClient;
-import org.databiosphere.workspacedataservice.recordsink.RawlsRecordSinkFactory;
-import org.databiosphere.workspacedataservice.recordsink.RecordSinkFactory;
-import org.databiosphere.workspacedataservice.retry.RestClientRetry;
 import org.databiosphere.workspacedataservice.shared.model.CollectionId;
-import org.databiosphere.workspacedataservice.storage.GcsStorage;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@ActiveProfiles(profiles = {"control-plane", "mock-sam"})
+@ActiveProfiles(
+    inheritProfiles = false,
+    profiles = {"control-plane", "mock-sam"})
 @TestPropertySource(
     properties = {
       // turn off pubsub autoconfiguration for tests
@@ -45,27 +34,6 @@ import org.springframework.test.context.TestPropertySource;
 class PubSubControllerMockMvcTest extends MockMvcTestBase {
   @MockBean private JobDao jobDao;
   @MockBean private CollectionDao collectionDao;
-
-  // Since we're running with both control-plane and data-plane profiles simultaneously, Spring
-  // does not know what to do with beans which are satisfied by two different mutually exclusive
-  // implementations. In this @TestConfiguration, we explicitly specify @Primary beans for any
-  // conflicts.
-  @TestConfiguration
-  static class SpecifyConflictingBeans {
-    @Primary
-    @Bean("overrideRecordSinkFactory")
-    RecordSinkFactory overrideRecordSinkFactory(
-        ObjectMapper mapper, GcsStorage storage, PubSub pubSub) {
-      return new RawlsRecordSinkFactory(mapper, storage, pubSub);
-    }
-
-    @Primary
-    @Bean("overrideSnapshotSupportFactory")
-    SnapshotSupportFactory overrideSnapshotSupportFactory(
-        RestClientRetry restClientRetry, ActivityLogger activityLogger, RawlsClient rawlsClient) {
-      return new RawlsSnapshotSupportFactory(restClientRetry, activityLogger, rawlsClient);
-    }
-  }
 
   @Test
   void statusUpdateNotification() throws Exception {
