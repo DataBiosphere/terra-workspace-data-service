588d7c03d54ddd00395b5a9a65a52a66d31cf331
Nick Watts <1156625+nawatts@users.noreply.github.com>
2024-03-28 12:22:25 -0400

    [AJ-1705] Specify cloning instructions and purpose when linking snapshots through Rawls (#688)
    * Specify cloning instructions and purpose when linking snapshots through Rawls
    
    * Resolve Sonar issue
    
    * Remove unnecessary test configuration
    
    * Remove inaccurate comment

--- service/src/main/java/org/databiosphere/workspacedataservice/rawls/NamedDataRepoSnapshot.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/rawls/NamedDataRepoSnapshot.java
@@ -1,5 +1,7 @@
 package org.databiosphere.workspacedataservice.rawls;
 
+import bio.terra.workspace.model.CloningInstructionsEnum;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -8,12 +10,24 @@ import java.util.UUID;
  * @param name name for this reference
  * @param description description for this reference
  * @param snapshotId the UUID of the snapshot to reference
+ * @param cloningInstructions cloning semantics for this snapshot
+ * @param properties key-value pairs to associate with the snapshot
  */
-public record NamedDataRepoSnapshot(String name, String description, UUID snapshotId) {
+public record NamedDataRepoSnapshot(
+    String name,
+    String description,
+    UUID snapshotId,
+    CloningInstructionsEnum cloningInstructions,
+    Map<String, String> properties) {
 
   public static NamedDataRepoSnapshot forSnapshotId(UUID snapshotId) {
     String referenceName = "%s-policy".formatted(snapshotId);
     String referenceDescription = "created at %s".formatted(System.currentTimeMillis());
-    return new NamedDataRepoSnapshot(referenceName, referenceDescription, snapshotId);
+    return new NamedDataRepoSnapshot(
+        referenceName,
+        referenceDescription,
+        snapshotId,
+        CloningInstructionsEnum.REFERENCE,
+        Map.of("purpose", "policy"));
   }
 }
