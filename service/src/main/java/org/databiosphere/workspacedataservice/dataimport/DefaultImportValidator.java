f4ba361908c3adfb295b892267c23ddd73cfc27d
Nick Watts <nwatts@broadinstitute.org>
2024-04-26 20:50:59 -0400

    Support gs:// URLs for Rawls JSON imports in ImportValidator

--- service/src/main/java/org/databiosphere/workspacedataservice/dataimport/DefaultImportValidator.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/DefaultImportValidator.java
@@ -1,13 +1,22 @@
 package org.databiosphere.workspacedataservice.dataimport;
 
+import static java.util.Collections.emptySet;
+
 import com.google.common.collect.Sets;
 import java.net.URI;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 import org.databiosphere.workspacedataservice.generated.ImportRequestServerModel;
+import org.databiosphere.workspacedataservice.generated.ImportRequestServerModel.TypeEnum;
 import org.databiosphere.workspacedataservice.service.model.exception.ValidationException;
 
 public class DefaultImportValidator implements ImportValidator {
+  private static final Map<TypeEnum, Set<String>> SUPPORTED_URL_SCHEMES_BY_IMPORT_TYPE =
+      Map.of(
+          TypeEnum.PFB, Set.of("https"),
+          TypeEnum.RAWLSJSON, Set.of("gs"),
+          TypeEnum.TDRMANIFEST, Set.of("https"));
   private static final Set<Pattern> ALWAYS_ALLOWED_HOSTS =
       Set.of(
           Pattern.compile("storage\\.googleapis\\.com"),
@@ -24,15 +33,21 @@ public class DefaultImportValidator implements ImportValidator {
   }
 
   public void validateImport(ImportRequestServerModel importRequest) {
+    TypeEnum importType = importRequest.getType();
+    Set<String> schemesSupportedForImportType =
+        SUPPORTED_URL_SCHEMES_BY_IMPORT_TYPE.getOrDefault(importType, emptySet());
+
     URI importUrl = importRequest.getUrl();
     String urlScheme = importUrl.getScheme();
-    if (!urlScheme.equals("https")) {
-      throw new ValidationException(
-          "Files may not be imported from %s URLs.".formatted(importUrl.getScheme()));
+    if (!schemesSupportedForImportType.contains(urlScheme)) {
+      throw new ValidationException("Files may not be imported from %s URLs.".formatted(urlScheme));
     }
 
-    if (allowedHosts.stream()
-        .noneMatch(allowedHost -> allowedHost.matcher(importUrl.getHost()).matches())) {
+    // Imports should be allowed from any GCS bucket.
+    boolean skipHostValidation = urlScheme.equals("gs");
+    if (!skipHostValidation
+        && allowedHosts.stream()
+            .noneMatch(allowedHost -> allowedHost.matcher(importUrl.getHost()).matches())) {
       throw new ValidationException(
           "Files may not be imported from %s.".formatted(importUrl.getHost()));
     }
