e06e49a8499e58713eb619f6b82caea87c4630a9
David An <davidan@broadinstitute.org>
2024-04-29 14:32:23 -0400

    spotless

--- service/src/main/java/org/databiosphere/workspacedataservice/shared/model/attributes/ArrayAttribute.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/shared/model/attributes/ArrayAttribute.java
@@ -4,4 +4,4 @@ package org.databiosphere.workspacedataservice.shared.model.attributes;
  * An array attribute - that is, an attribute that can have multiple values, all of which are the
  * same datatype.
  */
-public interface ArrayAttribute<T> extends Attribute {}
+public abstract class ArrayAttribute<T> implements Attribute {}
