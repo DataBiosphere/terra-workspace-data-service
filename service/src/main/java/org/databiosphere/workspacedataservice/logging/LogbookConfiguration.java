9c737daff8cd36fb2ff63d46aa7dc2a35c7c0119
yuliadub <yulia.dubinina@gmail.com>
2024-04-29 13:55:48 -0700

    adjust log format

--- service/src/main/java/org/databiosphere/workspacedataservice/logging/LogbookConfiguration.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/logging/LogbookConfiguration.java
@@ -6,12 +6,17 @@ import static org.zalando.logbook.core.Conditions.requestTo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.CommonsLogFormatSink;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
 
 @Configuration
 public class LogbookConfiguration {
 
   @Bean
   public Logbook logbook() {
-    return Logbook.builder().condition(exclude(requestTo("/status/**"))).build();
+    return Logbook.builder()
+        .condition(exclude(requestTo("/status/liveness"), requestTo("/status/readiness")))
+        .sink(new CommonsLogFormatSink(new DefaultHttpLogWriter()))
+        .build();
   }
 }
