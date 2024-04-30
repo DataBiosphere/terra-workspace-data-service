c8e7efe4ae5a752385564a9fd050b83bf2026983
David An <davidan@broadinstitute.org>
2024-04-30 13:48:11 -0400

    remove JsonAttribute constructor for null ObjectMapper

--- service/src/main/java/org/databiosphere/workspacedataservice/shared/model/attributes/JsonAttribute.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/shared/model/attributes/JsonAttribute.java
@@ -1,36 +1,32 @@
 package org.databiosphere.workspacedataservice.shared.model.attributes;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.Nullable;
 
 public class JsonAttribute extends ScalarAttribute<JsonNode> {
 
-  @Nullable private final ObjectMapper mapper;
-
-  JsonAttribute(JsonNode value) {
-    super(value);
-    this.mapper = null;
-  }
+  private final ObjectMapper mapper;
 
   public JsonAttribute(JsonNode value, ObjectMapper mapper) {
     super(value);
     this.mapper = mapper;
   }
 
+  @JsonValue
+  public JsonNode jsonValue() {
+    return this.value;
+  }
+
   @Override
   public String toString() {
     // if this JsonAttribute has an ObjectMapper reference, use that ObjectMapper
     // to generate the toString value. Else, use the built-in toString.
-    if (mapper == null) {
-      return value.toString();
-    } else {
-      try {
-        return mapper.writeValueAsString(this.value);
-      } catch (JsonProcessingException e) {
-        throw new RuntimeException(e);
-      }
+    try {
+      return mapper.writeValueAsString(this.value);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
     }
   }
 }
