fd129145d80e0eb91f302b9823ca553cbc280b89
Nick Watts <nwatts@broadinstitute.org>
2024-05-02 12:04:46 -0400

    Appease Sonar

--- service/src/main/java/org/databiosphere/workspacedataservice/policy/PolicyUtils.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/policy/PolicyUtils.java
@@ -2,12 +2,16 @@ package org.databiosphere.workspacedataservice.policy;
 
 import bio.terra.workspace.model.WsmPolicyInput;
 
-public abstract class PolicyUtils {
+public final class PolicyUtils {
   public static final String TERRA_NAMESPACE = "terra";
   public static final String PROTECTED_DATA_POLICY_NAME = "protected-data";
 
   public static boolean isProtectedDataPolicy(WsmPolicyInput wsmPolicyInput) {
     return wsmPolicyInput.getNamespace().equals(TERRA_NAMESPACE)
         && wsmPolicyInput.getName().equals(PROTECTED_DATA_POLICY_NAME);
+  }
+
+  private PolicyUtils() {
+    throw new IllegalStateException("Utility class");
   }
 }
