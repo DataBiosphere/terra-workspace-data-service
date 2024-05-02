7cb0bb5a4347c4fa203468c6f6246c916687c2fa
Nick Watts <nwatts@broadinstitute.org>
2024-05-02 11:04:40 -0400

    Make PolicyUtils abstract

--- service/src/main/java/org/databiosphere/workspacedataservice/policy/PolicyUtils.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/policy/PolicyUtils.java
@@ -1,10 +1,6 @@
 package org.databiosphere.workspacedataservice.policy;
 
-public class PolicyUtils {
+public abstract class PolicyUtils {
   public static final String TERRA_NAMESPACE = "terra";
   public static final String PROTECTED_DATA_POLICY_NAME = "protected-data";
-
-  private PolicyUtils() {
-    throw new IllegalStateException("Utility class");
-  }
 }
