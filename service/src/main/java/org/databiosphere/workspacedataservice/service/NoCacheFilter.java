4ee7a690c2fe61d34a0288f9445a0803386a2511
Nick Watts <nwatts@broadinstitute.org>
2024-05-09 10:42:12 -0400

    Remove wildcard import

--- service/src/main/java/org/databiosphere/workspacedataservice/service/NoCacheFilter.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/NoCacheFilter.java
@@ -1,6 +1,7 @@
 package org.databiosphere.workspacedataservice.service;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
