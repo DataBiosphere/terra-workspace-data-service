9fc1b01d3c5672239030e9114ed54ca5c9d7974c
Nick Watts <1156625+nawatts@users.noreply.github.com>
2024-05-10 10:49:32 -0400

    [AJ-1777] Fix check for protected workspace (#785)

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/dataimport/protecteddatasupport/WsmProtectedDataSupport.java
@@ -0,0 +1,23 @@
+package org.databiosphere.workspacedataservice.dataimport.protecteddatasupport;
+
+import bio.terra.workspace.model.WorkspaceDescription;
+import org.databiosphere.workspacedataservice.annotations.DeploymentMode.DataPlane;
+import org.databiosphere.workspacedataservice.policy.PolicyUtils;
+import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
+import org.databiosphere.workspacedataservice.workspacemanager.WorkspaceManagerDao;
+import org.springframework.stereotype.Component;
+
+@Component
+@DataPlane
+public class WsmProtectedDataSupport implements ProtectedDataSupport {
+  private final WorkspaceManagerDao wsmDao;
+
+  public WsmProtectedDataSupport(WorkspaceManagerDao wsmDao) {
+    this.wsmDao = wsmDao;
+  }
+
+  public boolean workspaceSupportsProtectedDataPolicy(WorkspaceId workspaceId) {
+    WorkspaceDescription workspace = wsmDao.getWorkspace(workspaceId.id());
+    return PolicyUtils.containsProtectedDataPolicy(workspace.getPolicies());
+  }
+}
