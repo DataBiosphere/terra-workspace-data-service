ce922318e60e58f2dd175ec292ba121c52419787
Nick Watts <nwatts@broadinstitute.org>
2024-05-10 10:25:29 -0400

    Join fields list once

--- service/src/main/java/org/databiosphere/workspacedataservice/rawls/RawlsClient.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/rawls/RawlsClient.java
@@ -9,6 +9,10 @@ import org.springframework.web.client.RestClientResponseException;
 
 /** Client to make REST calls to Rawls */
 public class RawlsClient {
+  /* RawlsWorkspaceDetails only includes the subset of workspace fields needed by CWDS.
+   * Thus, RawlsClient should request only those fields when getting workspace details. */
+  private final String GET_WORKSPACE_DETAILS_FIELDS_PARAM =
+      String.join(",", RawlsWorkspaceDetails.SUPPORTED_FIELDS);
   private final RawlsApi rawlsApi;
   private final RestClientRetry restClientRetry;
 
@@ -20,11 +24,7 @@ public class RawlsClient {
   public RawlsWorkspaceDetails getWorkspaceDetails(UUID workspaceId) {
     try {
       RestCall<RawlsWorkspaceDetails> restCall =
-          () ->
-              rawlsApi.getWorkspaceDetails(
-                  workspaceId,
-                  /* RawlsWorkspaceDetails only includes the subset of workspace fields needed by CWDS. */
-                  /* fields */ String.join(",", RawlsWorkspaceDetails.SUPPORTED_FIELDS));
+          () -> rawlsApi.getWorkspaceDetails(workspaceId, GET_WORKSPACE_DETAILS_FIELDS_PARAM);
 
       return restClientRetry.withRetryAndErrorHandling(restCall, "Rawls.getWorkspaceDetails");
     } catch (RestClientResponseException restException) {
