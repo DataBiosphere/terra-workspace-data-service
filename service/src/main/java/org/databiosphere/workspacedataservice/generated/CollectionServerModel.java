cf5cb02c1e65b470fbc74d35cd775354096c1162
David An <davidan@broadinstitute.org>
2024-07-16 10:14:14 -0400

    AJ-1923: proposal signatures for v1 collection APIs (#857)

---
+++ service/src/main/java/org/databiosphere/workspacedataservice/generated/CollectionServerModel.java
@@ -0,0 +1,146 @@
+package org.databiosphere.workspacedataservice.generated;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.UUID;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * CollectionServerModel
+ */
+
+@JsonTypeName("Collection")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.7.0")
+public class CollectionServerModel {
+
+  private UUID id;
+
+  private String name;
+
+  private String description;
+
+  public CollectionServerModel() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public CollectionServerModel(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  public CollectionServerModel id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  @Valid 
+  @Schema(name = "id", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("id")
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public CollectionServerModel name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Letters, numbers, dash and underscore only. Max 128 characters.
+   * @return name
+   */
+  @NotNull @Pattern(regexp = "[a-zA-Z0-9-_]{1,128}") 
+  @Schema(name = "name", description = "Letters, numbers, dash and underscore only. Max 128 characters.", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("name")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public CollectionServerModel description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * Get description
+   * @return description
+   */
+  @NotNull 
+  @Schema(name = "description", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("description")
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CollectionServerModel collection = (CollectionServerModel) o;
+    return Objects.equals(this.id, collection.id) &&
+        Objects.equals(this.name, collection.name) &&
+        Objects.equals(this.description, collection.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, description);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class CollectionServerModel {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
