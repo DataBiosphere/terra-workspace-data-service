6f322ef5e5cafb2e4585a2d2ba8ad391552984dc
David An <davidan@broadinstitute.org>
2024-08-05 17:33:30 -0400

    deeper into test coverage

--- service/src/test/java/org/databiosphere/workspacedataservice/controller/AllControllersPermissionsTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/AllControllersPermissionsTest.java
@@ -1,7 +1,11 @@
 package org.databiosphere.workspacedataservice.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.databiosphere.workspacedataservice.shared.model.job.JobType.SYNC_BACKUP;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.spy;
@@ -9,8 +13,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
@@ -25,10 +30,19 @@ import org.databiosphere.workspacedataservice.service.BackupRestoreService;
 import org.databiosphere.workspacedataservice.service.CollectionService;
 import org.databiosphere.workspacedataservice.service.JobService;
 import org.databiosphere.workspacedataservice.service.PermissionService;
+import org.databiosphere.workspacedataservice.service.model.exception.AuthenticationMaskableException;
+import org.databiosphere.workspacedataservice.service.model.exception.AuthorizationException;
+import org.databiosphere.workspacedataservice.shared.model.BackupResponse;
+import org.databiosphere.workspacedataservice.shared.model.BackupRestoreRequest;
+import org.databiosphere.workspacedataservice.shared.model.CloneResponse;
+import org.databiosphere.workspacedataservice.shared.model.CloneStatus;
 import org.databiosphere.workspacedataservice.shared.model.CollectionId;
 import org.databiosphere.workspacedataservice.shared.model.Record;
 import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
+import org.databiosphere.workspacedataservice.shared.model.job.Job;
+import org.databiosphere.workspacedataservice.shared.model.job.JobInput;
+import org.databiosphere.workspacedataservice.shared.model.job.JobStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -39,6 +53,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @DirtiesContext
@@ -53,11 +68,13 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MockBean BackupRestoreService backupRestoreService;
   @Autowired TwdsProperties twdsProperties;
 
+  private final SamAuthorizationDao samAuthorizationDao = spy(MockSamAuthorizationDao.allowAll());
+
+  // ========== exemplar data to ensure the internals of APIs succeed
   private static final WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
   private static final CollectionId collectionId = CollectionId.of(UUID.randomUUID());
   private static final UUID jobId = UUID.randomUUID();
-
-  private final SamAuthorizationDao samAuthorizationDao = spy(MockSamAuthorizationDao.allowAll());
+  private static final UUID backupTrackingId = UUID.randomUUID();
 
   private static final RecordType RECORD_TYPE = RecordType.valueOf("mytype");
   private static final String RECORD_ID = "myid";
@@ -73,6 +90,36 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
 
   private static final Record MOCK_RECORD = new Record(RECORD_ID, RECORD_TYPE);
 
+  private static final BackupResponse MOCK_BACKUPRESPONSE =
+      new BackupResponse("filename", UUID.randomUUID(), "description");
+
+  private static final CloneResponse MOCK_CLONERESPONSE =
+      new CloneResponse(UUID.randomUUID(), CloneStatus.BACKUPSUCCEEDED);
+
+  private static final Job<JobInput, BackupResponse> MOCK_BACKUPJOB =
+      new Job<>(
+          UUID.randomUUID(),
+          SYNC_BACKUP,
+          /* collectionId= */ null, // backup jobs do not execute within a single collection
+          JobStatus.QUEUED,
+          "",
+          Timestamp.from(Instant.now()).toLocalDateTime(),
+          Timestamp.from(Instant.now()).toLocalDateTime(),
+          JobInput.empty(),
+          MOCK_BACKUPRESPONSE);
+
+  private static final Job<JobInput, CloneResponse> MOCK_CLONEJOB =
+      new Job<>(
+          UUID.randomUUID(),
+          SYNC_BACKUP,
+          /* collectionId= */ null, // backup jobs do not execute within a single collection
+          JobStatus.QUEUED,
+          "",
+          Timestamp.from(Instant.now()).toLocalDateTime(),
+          Timestamp.from(Instant.now()).toLocalDateTime(),
+          JobInput.empty(),
+          MOCK_CLONERESPONSE);
+
   @BeforeEach
   void beforeEach() {
     // mocks so APIs don't hit any internal errors
@@ -84,11 +131,15 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
     when(recordDao.getPrimaryKeyColumn(RECORD_TYPE, collectionId.id())).thenReturn("sys_name");
 
     when(jobService.getJob(jobId)).thenReturn(MOCK_JOB);
-    //
-    //    when(backupRestoreService.backupAzureWDS(
-    //            eq("v0.2"), eq(backupTrackingId), any(BackupRestoreRequest.class)))
-    //        .thenReturn(new Job<>());
+
+    when(backupRestoreService.backupAzureWDS(
+            eq("v0.2"), eq(backupTrackingId), any(BackupRestoreRequest.class)))
+        .thenReturn(MOCK_BACKUPJOB);
+
+    when(backupRestoreService.checkCloneStatus()).thenReturn(MOCK_CLONEJOB);
   }
+
+  // ========== API definitions: which require read, which require write?
 
   // these APIs are public; they require no auth
   private static Stream<Arguments> publicApis() {
@@ -100,38 +151,6 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
         arguments(named("GET /status/liveness", get("/status/liveness"))),
         arguments(named("GET /status/readiness", get("/status/readiness"))),
         arguments(named("GET /prometheus", get("/prometheus"))));
-  }
-
-  // these APIs require write permission
-  private static Stream<Arguments> writeApis() {
-    return Stream.of(
-        arguments(
-            named(
-                "PUT /{instanceid}/records/v0.2/{type}/{id}",
-                put("/{instanceid}/records/v0.2/{type}/{id}", collectionId, RECORD_TYPE, RECORD_ID)
-                    .content("{\"attributes\": {}}")
-                    .contentType(MediaType.APPLICATION_JSON))));
-    // TODO tests for these APIs:
-    /*
-     POST /{instanceid}/batch/{v}/{type}
-     PATCH /{instanceid}/records/{v}/{type}/{id}
-     DELETE /{instanceid}/records/{v}/{type}/{id}
-
-     POST /{instanceid}/tsv/{v}/{type}
-
-     POST /{instanceUuid}/import/v1
-
-     POST /collections/v1/{workspaceId}
-
-     PUT /collections/v1/{workspaceId}/{collectionId}
-     DELETE /collections/v1/{workspaceId}/{collectionId}
-
-     DELETE /instances/{v}/{instanceid}
-
-     DELETE /{instanceid}/types/{v}/{type}
-     PATCH /{instanceid}/types/{v}/{type}/{attribute}
-     DELETE /{instanceid}/types/{v}/{type}/{attribute}
-    */
   }
 
   // these APIs require read permission
@@ -178,14 +197,56 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
                     .contentType(MediaType.APPLICATION_JSON))));
   }
 
+  // these APIs require write permission
+  private static Stream<Arguments> writeApis() {
+    return Stream.of(
+        arguments(
+            named(
+                "PUT /{instanceid}/records/v0.2/{type}/{id}",
+                put("/{instanceid}/records/v0.2/{type}/{id}", collectionId, RECORD_TYPE, RECORD_ID)
+                    .content("{\"attributes\": {}}")
+                    .contentType(MediaType.APPLICATION_JSON))));
+    // TODO tests for these APIs:
+    /*
+     POST /{instanceid}/batch/{v}/{type}
+     PATCH /{instanceid}/records/{v}/{type}/{id}
+     DELETE /{instanceid}/records/{v}/{type}/{id}
+
+     POST /{instanceid}/tsv/{v}/{type}
+
+     POST /{instanceUuid}/import/v1
+
+     POST /collections/v1/{workspaceId}
+
+     PUT /collections/v1/{workspaceId}/{collectionId}
+     DELETE /collections/v1/{workspaceId}/{collectionId}
+
+     DELETE /instances/{v}/{instanceid}
+
+     DELETE /{instanceid}/types/{v}/{type}
+     PATCH /{instanceid}/types/{v}/{type}/{attribute}
+     DELETE /{instanceid}/types/{v}/{type}/{attribute}
+    */
+  }
+
   // these single-tenant APIs require read permission. When single-tenant APIs are fully retired,
   // this method and any tests that use it should be deleted.
   private static Stream<Arguments> singleTenantReadOnlyApis() {
-    return Stream.of(arguments(named("GET /instances/v0.2", get("/instances/v0.2"))));
-    // TODO tests for these APIs:
-    // get("/backup/v0.2"),
-    // get("/backup/v0.2/{trackingId}", UUID.randomUUID()),
-    // get("/clone/v0.2"));
+    return Stream.of(
+        arguments(named("GET /instances/v0.2", get("/instances/v0.2"))),
+        arguments(
+            named(
+                "POST /backup/v0.2",
+                post("/backup/v0.2")
+                    .content(
+                        "{\"requestingWorkspaceId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\","
+                            + "  \"description\": \"string\"}")
+                    .contentType(MediaType.APPLICATION_JSON))),
+        arguments(named("GET /clone/v0.2", get("/clone/v0.2"))),
+        arguments(
+            named(
+                "GET /backup/v0.2/{trackingId}",
+                get("/backup/v0.2/{trackingId}", UUID.randomUUID()))));
   }
 
   // these single-tenant APIs require write permission. When single-tenant APIs are fully retired,
@@ -204,21 +265,21 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MethodSource("writeApis")
   void writeApiWritePermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanWrite(workspaceId);
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "write API {0} is forbidden with read-only permission")
   @MethodSource("writeApis")
   void writeApiReadOnlyPermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanRead(workspaceId);
-    mockMvc.perform(request).andExpect(status().isForbidden());
+    requestShouldBeForbidden(request);
   }
 
   @ParameterizedTest(name = "write API {0} is not found with no permission")
   @MethodSource("writeApis")
   void writeApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(workspaceId);
-    mockMvc.perform(request).andExpect(status().isNotFound());
+    requestShouldBeMaskedNotFound(request);
   }
 
   // ========== read-only APIs
@@ -227,21 +288,21 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MethodSource("readOnlyApis")
   void readApiWritePermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanWrite(workspaceId);
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "read-only API {0} succeeds with read-only permission")
   @MethodSource("readOnlyApis")
   void readApiReadOnlyPermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanRead(workspaceId);
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "read-only API {0} is not found with no permission")
   @MethodSource("readOnlyApis")
   void readApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(workspaceId);
-    mockMvc.perform(request).andExpect(status().isNotFound());
+    requestShouldBeMaskedNotFound(request);
   }
 
   // ========== single-tenant write APIs
@@ -250,7 +311,7 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MethodSource("singleTenantWriteApis")
   void singleTenantWriteApiWritePermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanWrite(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "single-tenant write API {0} is forbidden with read-only permission")
@@ -258,14 +319,14 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   void singleTenantWriteApiReadOnlyPermission(MockHttpServletRequestBuilder request)
       throws Exception {
     userCanRead(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().isForbidden());
+    requestShouldBeForbidden(request);
   }
 
   @ParameterizedTest(name = "single-tenant write API {0} is not found with no permission")
   @MethodSource("singleTenantWriteApis")
   void singleTenantWriteApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().isNotFound());
+    requestShouldBeMaskedNotFound(request);
   }
 
   // ========== single-tenant read-only APIs
@@ -274,7 +335,7 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MethodSource("singleTenantReadOnlyApis")
   void singleTenantReadApiWritePermission(MockHttpServletRequestBuilder request) throws Exception {
     userCanWrite(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "single-tenant read-only API {0} succeeds with read-only permission")
@@ -282,14 +343,14 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   void singleTenantReadApiReadOnlyPermission(MockHttpServletRequestBuilder request)
       throws Exception {
     userCanRead(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
   @ParameterizedTest(name = "single-tenant read-only API {0} fails with no permission")
   @MethodSource("singleTenantReadOnlyApis")
   void singleTenantReadApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(twdsProperties.workspaceId());
-    mockMvc.perform(request).andExpect(status().isNotFound());
+    requestShouldBeMaskedNotFound(request);
   }
 
   // ========== public APIs
@@ -298,12 +359,51 @@ class AllControllersPermissionsTest extends MockMvcTestBase {
   @MethodSource("publicApis")
   void publicApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(workspaceId);
-    mockMvc.perform(request).andExpect(status().is2xxSuccessful());
+    requestShouldSucceed(request);
   }
 
-  // TODO: tests for single-tenant write-only APIs
+  // ========== helpers
+  private void requestShouldSucceed(MockHttpServletRequestBuilder request) throws Exception {
+    mockMvc.perform(request).andDo(this::assertSuccessfulPermissionCheck);
+  }
 
-  // helpers
+  private void requestShouldBeForbidden(MockHttpServletRequestBuilder request) throws Exception {
+    mockMvc
+        .perform(request)
+        .andExpect(
+            result ->
+                assertInstanceOf(AuthorizationException.class, result.getResolvedException()));
+  }
+
+  private void requestShouldBeMaskedNotFound(MockHttpServletRequestBuilder request)
+      throws Exception {
+    mockMvc
+        .perform(request)
+        .andExpect(
+            result ->
+                assertInstanceOf(
+                    AuthenticationMaskableException.class, result.getResolvedException()));
+  }
+
+  private void assertSuccessfulPermissionCheck(MvcResult mvcResult) {
+    /* We don't bother mocking out all the internals of each API within this test class.
+       Therefore, it is quite likely that APIs will fail for business logic reasons. All we
+       care about for this test class is that the permission checks pass. We consider a passing
+       test to be either:
+        - no exception was thrown, and response code is 2xx
+        - an exception was thrown, but that exception is neither AuthenticationMaskableException
+            nor AuthorizationException; these are the two exceptions that would be thrown by
+            permission checks.
+    */
+    if (mvcResult.getResolvedException() == null) {
+      assertThat(mvcResult.getResponse().getStatus() / 100).isEqualTo(2);
+    } else {
+      Exception ex = mvcResult.getResolvedException();
+      assertThat(ex).isNotInstanceOf(AuthenticationMaskableException.class);
+      assertThat(ex).isNotInstanceOf(AuthorizationException.class);
+    }
+  }
+
   private void userCanWrite(WorkspaceId workspaceId) {
     stubWorkspacePermissions(workspaceId, true, true);
   }
