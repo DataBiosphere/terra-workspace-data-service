4e722f0325db57ba0c6b9ab3a3c33b6a93cf2692
David An <davidan@broadinstitute.org>
2024-08-07 10:19:31 -0400

    paramterize the test

--- service/src/test/java/org/databiosphere/workspacedataservice/search/QueryParserTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/search/QueryParserTest.java
@@ -5,116 +5,49 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class QueryParserTest {
 
-  @Test
-  void parseSingleField() {
-    String query = "column1:foo";
+  private static Stream<Arguments> singleColumnTerms() {
+    return Stream.of(
+        Arguments.of("foo", "foo"),
+        // Lucene query syntax uses * and ? as wildcards
+        Arguments.of("foo*", "foo*"),
+        Arguments.of("fo?o", "fo?o"),
+        // Wildcards can be escaped
+        Arguments.of("fo\\?o\\*", "fo?o*"),
+        // SQL "like" uses % as a wildcard. The SQL we currently generate does not use "like", so
+        // end users can input a wildcard, but it won't have the effect they may have wanted
+        Arguments.of("%foo%", "%foo%"),
+        // whitespace in terms requires quoting the term
+        Arguments.of("\"this is a phrase\"", "this is a phrase"),
+        // because the query is a string, numbers come through as strings too
+        Arguments.of("42", "42"));
+  }
+
+  // test expected parsing for a single column and its filter term
+  @ParameterizedTest(name = "Single-column query term [{0}]")
+  @MethodSource("singleColumnTerms")
+  void parseSingleColumnTerm(String queryTerm, String expectedResult) {
+    String query = "column1:" + queryTerm;
 
     WhereClausePart actual = new QueryParser().parse(query);
 
     WhereClausePart expected =
         new WhereClausePart(
             List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "foo"),
+            Map.of("filterquery0", expectedResult),
             List.of("column1"));
 
     assertEquals(expected, actual);
   }
 
-  @Test
-  void parseStarWildcard() {
-    String query = "column1:foo*";
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "foo*"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void parseQuestionWildcard() {
-    String query = "column1:fo?o";
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "fo?o"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void parseEscapedWildcards() {
-    String query = "column1:fo\\?o\\*";
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "fo?o*"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void parseSqlWildcard() {
-    /*
-    note this is allowed. The SQL where clause we generate will not respect the wildcard;
-    it does not use LIKE. So even though we allow this, it won't have the effect the user
-    desired.
-    */
-    String query = "column1:%foo%";
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "%foo%"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void parseSingleQuoted() {
-    String query = "column1:\"this is a phrase\"";
-
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "this is a phrase"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
-  @Test
-  void parseSingleNumber() {
-    String query = "column1:42";
-
-    WhereClausePart actual = new QueryParser().parse(query);
-
-    WhereClausePart expected =
-        new WhereClausePart(
-            List.of("\"column1\" = :filterquery0"),
-            Map.of("filterquery0", "42"),
-            List.of("column1"));
-
-    assertEquals(expected, actual);
-  }
-
+  // we don't support ranges as of this writing
   @Test
   void parseSingleNumberRange() {
     String query = "column1:[23 TO 45]";
@@ -122,6 +55,7 @@ class QueryParserTest {
     assertThrows(InvalidQueryException.class, () -> queryParser.parse(query));
   }
 
+  // we don't support multiple columns as of this writing
   @Test
   void parseMultipleFields() {
     String query = "column1:foo AND column2:bar";
@@ -129,6 +63,7 @@ class QueryParserTest {
     assertThrows(InvalidQueryException.class, () -> queryParser.parse(query));
   }
 
+  // we don't support table-wide terms as of this writing
   @Test
   void parseNoFieldSpecified() {
     String query = "searchterm";
