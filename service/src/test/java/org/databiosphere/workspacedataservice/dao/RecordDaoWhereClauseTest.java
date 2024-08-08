ea0b0830c536bbbe7567c90c201fa3f5f5ee5021
David An <davidan@broadinstitute.org>
2024-08-08 10:54:57 -0400

    consolidate some tests

--- service/src/test/java/org/databiosphere/workspacedataservice/dao/RecordDaoWhereClauseTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/dao/RecordDaoWhereClauseTest.java
@@ -14,6 +14,11 @@ import org.junit.jupiter.api.Test;
 /**
  * Tests for RecordDao.generateQueryWhereClause() These tests don't require any Spring context, so
  * it's nice to have them in their own class
+ *
+ * <p>See also org.databiosphere.workspacedataservice.search.QueryParserTest. QueryParserTest has
+ * detailed tests for the filter.query portion of the where clause, and we don't need to replicate
+ * those here. Here, we should be testing the interactions between filter.query and filter.ids, as
+ * well as any other higher-level logic.
  */
 class RecordDaoWhereClauseTest {
 
@@ -48,7 +53,7 @@ class RecordDaoWhereClauseTest {
   }
 
   @Test
-  void emptyColumnFilters() {
+  void emptyFilterQuery() {
     SearchFilter searchFilter = new SearchFilter(Optional.empty(), Optional.of(""));
 
     WhereClause actual =
@@ -58,7 +63,7 @@ class RecordDaoWhereClauseTest {
   }
 
   @Test
-  void oneColumnFilter() {
+  void oneColumnFilterQuery() {
     SearchFilter searchFilter = new SearchFilter(Optional.empty(), Optional.of("col1:col1value"));
 
     WhereClause actual =
@@ -69,35 +74,22 @@ class RecordDaoWhereClauseTest {
   }
 
   @Test
-  void oneColumnCaseInsensitive() {
-    SearchFilter searchFilter = new SearchFilter(Optional.empty(), Optional.of("col1:UPPER"));
+  void idsAndOneColumnFilterQuery() {
+    List<String> ids = List.of("one", "two", "three");
+    SearchFilter searchFilter = new SearchFilter(Optional.of(ids), Optional.of("col1:col1value"));
 
     WhereClause actual =
         RecordDao.generateQueryWhereClause(
             "my-pk-col", Map.of("col1", DataTypeMapping.STRING), Optional.of(searchFilter));
-    assertEquals(" where LOWER(\"col1\") = :filterquery0", actual.sql());
-    assertEquals(Map.of("filterquery0", "upper"), actual.params().getValues());
+    assertEquals(
+        " where \"my-pk-col\" in (:filterIds) and LOWER(\"col1\") = :filterquery0", actual.sql());
+    assertEquals(
+        Map.of("filterIds", ids, "filterquery0", "col1value"), actual.params().getValues());
   }
 
   @Disabled("we don't support multiple columns yet")
   @Test
-  void multipleColumnFilters() {
-    SearchFilter searchFilter =
-        new SearchFilter(
-            Optional.empty(), Optional.of("col1:col1value AND col2:col2value AND col3:col3value"));
-
-    WhereClause actual =
-        RecordDao.generateQueryWhereClause("my-pk-col", Map.of(), Optional.of(searchFilter));
-    assertEquals(
-        " where \"col1\" = :filter0 and \"col2\" = :filter1 and \"col3\" = :filter2", actual.sql());
-    assertEquals(
-        Map.of("filter0", "col1value", "filter1", "col2value", "filter2", "col3value"),
-        actual.params().getValues());
-  }
-
-  @Disabled("we don't support multiple columns yet")
-  @Test
-  void idsAndColumnFilters() {
+  void idsAndMultipleColumnFiltersQuery() {
     List<String> ids = List.of("one", "two", "three");
     SearchFilter searchFilter =
         new SearchFilter(
