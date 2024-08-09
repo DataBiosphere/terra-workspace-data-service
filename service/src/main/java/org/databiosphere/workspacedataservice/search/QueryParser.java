5c4d1982f146c0bf2e8ac14dccf694cb2e39356f
David An <davidan@broadinstitute.org>
2024-08-09 09:36:55 -0400

    AJ-1238: filter by column enhancements (#879)

--- service/src/main/java/org/databiosphere/workspacedataservice/search/QueryParser.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/search/QueryParser.java
@@ -2,6 +2,7 @@ package org.databiosphere.workspacedataservice.search;
 
 import static org.databiosphere.workspacedataservice.dao.SqlUtils.quote;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,16 +12,23 @@ import org.apache.lucene.queryparser.flexible.core.QueryNodeParseException;
 import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
 import org.apache.lucene.queryparser.flexible.standard.parser.StandardSyntaxParser;
+import org.databiosphere.workspacedataservice.service.model.DataTypeMapping;
 
 public class QueryParser {
 
   public static final String DEFAULT_ALL_COLUMNS_NAME = "sys_all_columns";
 
+  private final Map<String, DataTypeMapping> schema;
+
+  public QueryParser(Map<String, DataTypeMapping> schema) {
+    this.schema = schema;
+  }
+
   public WhereClausePart parse(String query) {
     // query should not be blank by the time we get here, but if it is, short-circuit and return
     // an empty clause.
     if (StringUtils.isBlank(query)) {
-      return new WhereClausePart(List.of(), Map.of(), List.of());
+      return new WhereClausePart(List.of(), Map.of());
     }
     // create a Lucene parser
     StandardSyntaxParser standardSyntaxParser = new StandardSyntaxParser();
@@ -33,27 +41,65 @@ public class QueryParser {
       throw new InvalidQueryException();
     }
 
-    // even if we parsed correctly, ensure the query does not use any syntax we don't support
+    // even if the query parsed correctly via the Lucene library, ensure the query does not use any
+    // syntax that WDS doesn't support. For now, we only support a single FieldQueryNode.
     if (parsed instanceof FieldQueryNode fieldQueryNode) {
       String column = fieldQueryNode.getFieldAsString();
       String value = fieldQueryNode.getTextAsString();
 
       validateColumnName(column);
 
+      // determine the datatype of the column on which we are filtering.
+      var datatype = schema.get(column);
+
+      // init our return values. In the future we want to support filtering on multiple columns,
+      // so we need to support a list of clause fragments.
       List<String> clauses = new ArrayList<>();
-      Map<String, String> values = new HashMap<>();
-      List<String> columns = new ArrayList<>();
+      Map<String, Object> values = new HashMap<>();
 
-      // bind parameter names have syntax limitations, so we use an artificial one
+      // bind parameter names have syntax limitations, so we use artificial ones below
       var paramName = "filterquery0";
-      clauses.add(quote(column) + " = :" + paramName);
-      values.put(paramName, value);
-      columns.add(column);
 
-      return new WhereClausePart(clauses, values, columns);
+      // based on the datatype of the column, build relevant SQL
+      switch (datatype) {
+        case STRING:
+          // LOWER("mycolumn") = 'mysearchterm'
+          clauses.add("LOWER(" + quote(column) + ") = :" + paramName);
+          values.put(paramName, value.toLowerCase());
+          break;
+        case ARRAY_OF_STRING:
+          // 'mysearchterm' ILIKE ANY("mycolumn")
+          clauses.add(":" + paramName + " ILIKE ANY(" + quote(column) + ")");
+          values.put(paramName, value.toLowerCase());
+          break;
+        case NUMBER:
+          // "mycolumn" = 42
+          clauses.add(quote(column) + " = :" + paramName);
+          values.put(paramName, parseNumericValue(value));
+          break;
+        case ARRAY_OF_NUMBER:
+          // 42 = ANY("mycolumn")
+          clauses.add(":" + paramName + " = ANY(" + quote(column) + ")");
+          values.put(paramName, parseNumericValue(value));
+          break;
+        default:
+          throw new InvalidQueryException("Column specified in query must be a string type");
+      }
+
+      return new WhereClausePart(clauses, values);
     } else {
       throw new InvalidQueryException();
     }
+  }
+
+  private Double parseNumericValue(String value) {
+    BigDecimal parsedNumber;
+    try {
+      parsedNumber = new BigDecimal(value);
+    } catch (NumberFormatException nfe) {
+      throw new InvalidQueryException("Query value for numeric column must be a number");
+    }
+    return parsedNumber.doubleValue();
   }
 
   private void validateColumnName(String columnName) {
@@ -63,6 +109,11 @@ public class QueryParser {
     // the end user to specify a column name.
     if (DEFAULT_ALL_COLUMNS_NAME.equals(columnName)) {
       throw new InvalidQueryException("Query must specify a column name");
+    }
+    // does this column exist in the record type?
+    if (!schema.containsKey(columnName)) {
+      throw new InvalidQueryException(
+          "Column specified in query does not exist in this record type");
     }
   }
 }
