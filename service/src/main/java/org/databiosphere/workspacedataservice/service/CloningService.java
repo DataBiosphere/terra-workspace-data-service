0f5a73c7d3ec9c9864d1964090875d1861a7604c
David An <davidan@broadinstitute.org>
2024-08-22 16:37:33 -0400

    more WIP

--- service/src/main/java/org/databiosphere/workspacedataservice/service/CloningService.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/service/CloningService.java
@@ -1,13 +1,14 @@
 package org.databiosphere.workspacedataservice.service;
 
+import bio.terra.common.db.WriteTransaction;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import org.apache.commons.collections4.IteratorUtils;
 import org.databiosphere.workspacedataservice.generated.CollectionRequestServerModel;
 import org.databiosphere.workspacedataservice.generated.CollectionServerModel;
-import org.databiosphere.workspacedataservice.service.model.Relation;
 import org.databiosphere.workspacedataservice.shared.model.DefaultCollectionCreationResult;
-import org.databiosphere.workspacedataservice.shared.model.RecordType;
 import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedAcyclicGraph;
@@ -32,7 +33,7 @@ public class CloningService {
     this.namedTemplate = namedTemplate;
   }
 
-  @SuppressWarnings("UnstableApiUsage") // Guava's Graph feature is marked @Beta
+  @WriteTransaction
   public DefaultCollectionCreationResult clone(
       WorkspaceId sourceWorkspaceId, WorkspaceId targetWorkspaceId) {
 
@@ -62,7 +63,7 @@ public class CloningService {
             // this is a non-default collection in the source workspace; create a non-default
             // collection in the target workspace
             logger.info(
-                "... source collection is non-default; creating target collection '{}",
+                "... source collection is non-default; creating target collection '{}'",
                 sourceCollection.getName());
             CollectionRequestServerModel collectionRequest =
                 new CollectionRequestServerModel(
@@ -79,39 +80,56 @@ public class CloningService {
           //    list all source db tables in the source collection
           List<String> tableNames = getTableNames(sourceCollection.getId());
           //    list all foreign keys in the source collection
-          Map<String, String> foreignKeys = getForeignKeys(sourceCollection.getId());
+          List<ForeignKeyEdge> foreignKeys = getForeignKeys(sourceCollection.getId());
           // instantiate a graph, which will allow us to traverse tables in the proper order.
-          // when processing tables, we need to insert foreign key targets before foreign key
+          // when cloning tables, we need to insert foreign key targets before foreign key
           // sources.
           DirectedAcyclicGraph<String, DefaultEdge> graph =
               new DirectedAcyclicGraph<>(DefaultEdge.class);
           // add each table as a node in the graph
           tableNames.forEach(graph::addVertex);
           // add each foreign key as an edge in the graph
-          foreignKeys.forEach(graph::addEdge);
+          foreignKeys.forEach(fkEdge -> graph.addEdge(fkEdge.fromTable, fkEdge.toTable));
 
-          // traverse the graph in topological order
+          // TODO AJ-1952: what to do about cyclic table structures? One option is to disable FK
+          //   validation in Postgres, do the inserts, then turn FK validation back on.
+
+          // traverse the graph in REVERSE topological order. JGraphT's topological order is from
+          // the root node to the leaves; we need to insert leaf tables first so they exist
+          // before anything that has a foreign key to them. So, we reverse the topo order.
           TopologicalOrderIterator<String, DefaultEdge> topologicalOrderIterator =
               new TopologicalOrderIterator<>(graph);
+          List<String> topoOrder = IteratorUtils.toList(topologicalOrderIterator);
+          Collections.reverse(topoOrder);
 
-          while (topologicalOrderIterator.hasNext()) {
-            String tableName = topologicalOrderIterator.next();
+          topoOrder.forEach(
+              tableName -> {
+                logger.info("... processing table {}.{}", sourceCollectionId, tableName);
 
-            logger.info("... processing table {}.{}", sourceCollectionId, tableName);
+                // create table in target
+                // CREATE TABLE target_table (LIKE source_table INCLUDING ALL);
+                // TODO AJ-1952: this does NOT include foreign key constraints!
+                String createSql =
+                    "CREATE TABLE \"%s\".\"%s\" (LIKE \"%s\".\"%s\" INCLUDING ALL);"
+                        .formatted(targetCollectionId, tableName, sourceCollectionId, tableName);
 
-            // copy table from source to target
-            // CREATE TABLE copy_table AS SELECT * FROM original_table;
-            String sql =
-                "CREATE TABLE \"%s\".\"%s\" AS SELECT * FROM \"%s\".\"%s\""
-                    .formatted(targetCollectionId, tableName, sourceCollectionId, tableName);
+                namedTemplate.update(createSql, Map.of());
 
-            namedTemplate.update(sql, Map.of());
+                // copy data from source to target
+                // CREATE TABLE copy_table AS SELECT * FROM original_table;
+                String copySql =
+                    "INSERT INTO \"%s\".\"%s\" SELECT * FROM \"%s\".\"%s\";"
+                        .formatted(targetCollectionId, tableName, sourceCollectionId, tableName);
 
-            logger.info("... successfully cloned to table {}.{}", targetCollectionId, tableName);
-          }
+                namedTemplate.update(copySql, Map.of());
+
+                logger.info(
+                    "... successfully cloned to table {}.{}", targetCollectionId, tableName);
+              });
         });
 
-    // TODO AJ-1952: populate this better
+    // TODO AJ-1952: populate this better, it should have more info like number of
+    //   collections/tables/rows copied
     DefaultCollectionCreationResult result =
         new DefaultCollectionCreationResult(true, new CollectionServerModel("clone", "clone"));
 
@@ -120,7 +138,7 @@ public class CloningService {
 
   // similar to RecordDao.getAllRecordTypes, but this method includes sys_ tables as well, since
   // we need to clone the join tables for arrays of relations
-  private List<String> getTableNames(UUID collectionId) {
+  List<String> getTableNames(UUID collectionId) {
     return namedTemplate.queryForList(
         "select tablename from pg_tables WHERE schemaname = :workspaceSchema order by tablename",
         new MapSqlParameterSource("workspaceSchema", collectionId.toString()),
@@ -133,25 +151,22 @@ public class CloningService {
    * green->red. This allows the topological graph sort to function in the order we need.
    *
    * <p>Similar to RecordDao.getRelationCols, but this method operates on all tables in the
-   * collection
+   * collection and only returns table information, not column information
    *
-   * @param collectionId
-   * @return
+   * @param collectionId collection to inspect
+   * @return list of table-to-table relations
    */
   // TODO AJ-1952: implement
-  private Map<String, String> getForeignKeys(UUID collectionId) {
-    return Map.of();
+  List<ForeignKeyEdge> getForeignKeys(UUID collectionId) {
+    return namedTemplate.query(
+        "SELECT tc.table_name as from_table, ccu.table_name as to_table "
+            + "FROM information_schema.table_constraints tc "
+            + "JOIN information_schema.constraint_column_usage ccu "
+            + "ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema "
+            + "WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = :collectionId",
+        Map.of("collectionId", collectionId.toString()),
+        (rs, rowNum) -> new ForeignKeyEdge(rs.getString("from_table"), rs.getString("to_table")));
   }
 
-  public List<Relation> getRelationCols(UUID collectionId, RecordType recordType) {
-    return namedTemplate.query(
-        "SELECT kcu.column_name, ccu.table_name FROM information_schema.table_constraints tc JOIN information_schema.key_column_usage kcu "
-            + "ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema "
-            + "JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema "
-            + "WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = :workspace AND tc.table_name= :tableName",
-        Map.of("workspace", collectionId.toString(), "tableName", recordType.getName()),
-        (rs, rowNum) ->
-            new Relation(
-                rs.getString("column_name"), RecordType.valueOf(rs.getString("table_name"))));
-  }
+  record ForeignKeyEdge(String fromTable, String toTable) {}
 }
--- service/src/test/java/org/databiosphere/workspacedataservice/service/CloningServiceTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/CloningServiceTest.java
@@ -1,11 +1,23 @@
 package org.databiosphere.workspacedataservice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.databiosphere.workspacedataservice.TestUtils;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import org.databiosphere.workspacedataservice.generated.CollectionRequestServerModel;
+import org.databiosphere.workspacedataservice.generated.CollectionServerModel;
+import org.databiosphere.workspacedataservice.service.CloningService.ForeignKeyEdge;
+import org.databiosphere.workspacedataservice.shared.model.CollectionId;
+import org.databiosphere.workspacedataservice.shared.model.RecordAttributes;
+import org.databiosphere.workspacedataservice.shared.model.RecordRequest;
+import org.databiosphere.workspacedataservice.shared.model.RecordType;
+import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
+import org.databiosphere.workspacedataservice.shared.model.attributes.RelationAttribute;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 import org.jgrapht.traverse.TopologicalOrderIterator;
@@ -25,12 +37,15 @@ import org.springframework.test.context.TestPropertySource;
     properties = {
       // turn off pubsub autoconfiguration for tests
       "spring.cloud.gcp.pubsub.enabled=false",
+      // disable virtual collections
+      "twds.tenancy.allow-virtual-collections=false",
       // Rawls url must be valid, else context initialization (Spring startup) will fail
       "rawlsUrl=https://localhost/"
     })
 @DirtiesContext
 public class CloningServiceTest {
 
+  @Autowired CloningService cloningService;
   @Autowired CollectionService collectionService;
   @Autowired NamedParameterJdbcTemplate namedTemplate;
   @Autowired RecordService recordService;
@@ -38,15 +53,16 @@ public class CloningServiceTest {
 
   @BeforeEach
   @AfterEach
-  void cleanup() {}
+  void cleanup() {
+    //    TestUtils.cleanAllCollections(collectionService, namedTemplate);
+  }
 
+  // this tests the behavior of the JGraphT library. We don't need this test - I made it to help ME
+  // understand how the library works!
   @Test
   void graphOrdering() {
 
     List<String> tableNames = List.of("red", "green", "blue");
-    // foreign keys - graph edges - are added IN REVERSE. If a table "red" has a foreign key to
-    // table "green", we add an edge of green->red. This allows the topological graph sort to
-    // function in the order we need.
     Map<String, String> foreignKeys = Map.of("green", "red");
 
     // instantiate a graph, which will allow us to traverse tables in the proper order.
@@ -75,5 +91,104 @@ public class CloningServiceTest {
     // expected ordering:
     // there exists an edge green->red, so green must come before red
     assertThat(vertexIndices.get("green")).isLessThan(vertexIndices.get("red"));
+  }
+
+  @Test
+  void getTableNamesTest() {
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
+    CollectionServerModel collection =
+        collectionService.save(workspaceId, new CollectionRequestServerModel("unit", "test"));
+    CollectionId collectionId = CollectionId.of(collection.getId());
+
+    RecordType red = RecordType.valueOf("red");
+    RecordType green = RecordType.valueOf("green");
+    RecordType blue = RecordType.valueOf("blue");
+
+    // create record type "red" and record "red-one"
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        red,
+        "red-one",
+        Optional.empty(),
+        new RecordRequest(RecordAttributes.empty()));
+
+    // create record type "blue" and record "blue-one" with a scalar relation to red
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        blue,
+        "blue-one",
+        Optional.empty(),
+        new RecordRequest(
+            new RecordAttributes(Map.of("rel", new RelationAttribute(red, "red-one")))));
+
+    // add table green, with an array of relations to blue
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        green,
+        "green-one",
+        Optional.empty(),
+        new RecordRequest(
+            new RecordAttributes(Map.of("rel", List.of(new RelationAttribute(blue, "blue-one"))))));
+
+    // list table names
+    List<String> actual = cloningService.getTableNames(collectionId.id());
+
+    // should be red, green, blue, and join table from green to blue
+    Set<String> expected = Set.of("red", "green", "blue", "sys_green_rel");
+
+    assertEquals(expected, Set.copyOf(actual));
+  }
+
+  @Test
+  void getForeignKeysTest() {
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
+    CollectionServerModel collection =
+        collectionService.save(workspaceId, new CollectionRequestServerModel("unit", "test"));
+    CollectionId collectionId = CollectionId.of(collection.getId());
+
+    RecordType red = RecordType.valueOf("red");
+    RecordType green = RecordType.valueOf("green");
+    RecordType blue = RecordType.valueOf("blue");
+
+    // create record type "red" and record "red-one"
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        red,
+        "red-one",
+        Optional.empty(),
+        new RecordRequest(RecordAttributes.empty()));
+
+    // create record type "blue" and record "blue-one" with a scalar relation to red
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        blue,
+        "blue-one",
+        Optional.empty(),
+        new RecordRequest(
+            new RecordAttributes(Map.of("rel", new RelationAttribute(red, "red-one")))));
+
+    // add table green, with an array of relations to blue
+    recordService.upsertSingleRecord(
+        collectionId.id(),
+        green,
+        "green-one",
+        Optional.empty(),
+        new RecordRequest(
+            new RecordAttributes(Map.of("rel", List.of(new RelationAttribute(blue, "blue-one"))))));
+
+    // list foreign keys
+    List<ForeignKeyEdge> actual = cloningService.getForeignKeys(collectionId.id());
+
+    // we expect relations of:
+    // blue->red
+    // sys_green_rel->green
+    // sys_green_rel->blue
+    List<ForeignKeyEdge> expected =
+        List.of(
+            new ForeignKeyEdge("blue", "red"),
+            new ForeignKeyEdge("sys_green_rel", "green"),
+            new ForeignKeyEdge("sys_green_rel", "blue"));
+
+    assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
   }
 }
