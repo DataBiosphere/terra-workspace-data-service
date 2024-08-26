3da96375bf6529e4ffb67d141fc50bdeff995a7f
David An <davidan@broadinstitute.org>
2024-08-26 14:52:52 -0400

    another integration test fix

--- service/src/integrationTest/java/org/databiosphere/workspacedataservice/startup/CollectionInitializerCloneTest.java
+++ service/src/integrationTest/java/org/databiosphere/workspacedataservice/startup/CollectionInitializerCloneTest.java
@@ -159,7 +159,10 @@ class CollectionInitializerCloneTest extends IntegrationServiceTestBase {
     // default collection should exist, with a single table named "thing" in it
     // the "thing" table is defined in WDS-integrationTest-LocalFileStorage-input.sql.
     UUID workspaceUuid = workspaceId.id();
-    List<CollectionId> actualCollections = collectionDao.listCollectionSchemas();
+    List<CollectionId> actualCollections =
+        collectionService.list(workspaceId).stream()
+            .map(coll -> CollectionId.of(coll.getId()))
+            .toList();
     assertEquals(List.of(CollectionId.of(workspaceUuid)), actualCollections);
     List<RecordType> actualTypes = recordDao.getAllRecordTypes(workspaceUuid);
     assertEquals(List.of(RecordType.valueOf("thing")), actualTypes);
