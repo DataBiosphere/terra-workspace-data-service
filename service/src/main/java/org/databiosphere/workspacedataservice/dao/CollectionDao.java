07875f1f657b7774e008214ed0119529505ee0cd
David An <davidan@broadinstitute.org>
2024-08-26 11:50:25 -0400

    mark CollectionDao as deprecated

--- service/src/main/java/org/databiosphere/workspacedataservice/dao/CollectionDao.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/dao/CollectionDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.databiosphere.workspacedataservice.shared.model.CollectionId;
 import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
 
+@Deprecated(forRemoval = true, since = "v0.18.0")
 public interface CollectionDao {
   boolean collectionSchemaExists(CollectionId collectionId);
 
