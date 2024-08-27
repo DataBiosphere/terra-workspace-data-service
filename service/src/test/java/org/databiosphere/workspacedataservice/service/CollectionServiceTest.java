d460491d22b8843ea7d3d88e71ec0dcbf9ca4918
David An <davidan@broadinstitute.org>
2024-08-27 14:25:15 -0400

    new CollectionService.updateId() method

--- service/src/test/java/org/databiosphere/workspacedataservice/service/CollectionServiceTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/CollectionServiceTest.java
@@ -2,10 +2,14 @@ package org.databiosphere.workspacedataservice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Map;
+import java.util.UUID;
 import org.databiosphere.workspacedataservice.TestUtils;
-import org.databiosphere.workspacedataservice.common.TestBase;
 import org.databiosphere.workspacedataservice.config.TwdsProperties;
+import org.databiosphere.workspacedataservice.generated.CollectionServerModel;
 import org.databiosphere.workspacedataservice.shared.model.CollectionId;
 import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
 import org.junit.jupiter.api.AfterEach;
@@ -15,10 +19,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("control-plane")
 @DirtiesContext
-@SpringBootTest
-class CollectionServiceTest extends TestBase {
+@SpringBootTest(
+    properties = { // turn off pubsub autoconfiguration for tests
+      "spring.cloud.gcp.pubsub.enabled=false",
+      // aggressive retry settings so unit tests don't run too long
+      "rest.retry.maxAttempts=2",
+      "rest.retry.backoff.delay=3",
+      // Rawls url must be valid, else context initialization (Spring startup) will fail
+      "rawlsUrl=https://localhost/"
+    })
+class CollectionServiceTest {
 
   @Autowired private CollectionService collectionService;
   @Autowired private NamedParameterJdbcTemplate namedTemplate;
@@ -33,7 +47,7 @@ class CollectionServiceTest extends TestBase {
 
   @Test
   void testCreateDefaultIsIdempotent() {
-    WorkspaceId workspaceId = twdsProperties.workspaceId();
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
     CollectionId collectionId = CollectionId.of(workspaceId.id());
 
     // at the start of the test, we expect the default collection does not exist
@@ -51,7 +65,7 @@ class CollectionServiceTest extends TestBase {
   @Test
   void testFindAndCreateDefault() {
 
-    WorkspaceId workspaceId = twdsProperties.workspaceId();
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
     CollectionId collectionId = CollectionId.of(workspaceId.id());
 
     // find should be empty to start
@@ -70,5 +84,61 @@ class CollectionServiceTest extends TestBase {
 
     // find should be empty again
     assertThat(collectionService.find(workspaceId, collectionId)).isEmpty();
+  }
+
+  @Test
+  void testUpdateId() {
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
+
+    // create a collection
+    CollectionServerModel oldColl = collectionService.save(workspaceId, "name", "desc");
+    CollectionId oldCollectionId = CollectionId.of(oldColl.getId());
+    assertTrue(
+        collectionService.exists(oldCollectionId),
+        "Collection row should exist for initial collection");
+    assertTrue(
+        postgresSchemaExists(oldCollectionId),
+        "Postgres schema should exist for initial collection");
+
+    // prepare to give this collection a new id
+    CollectionId newCollectionId = CollectionId.of(UUID.randomUUID());
+    assertFalse(
+        collectionService.exists(newCollectionId),
+        "Collection row should not exist yet for new id");
+    assertFalse(
+        postgresSchemaExists(newCollectionId), "Postgres schema should not exist yet for new id");
+
+    // perform the id change
+    collectionService.updateId(workspaceId, oldCollectionId, newCollectionId);
+
+    // old schema should not exist
+    assertFalse(
+        postgresSchemaExists(oldCollectionId),
+        "Postgres schema should not exist for original id after rename");
+    // new schema should exist
+    assertTrue(
+        postgresSchemaExists(newCollectionId),
+        "Postgres schema should exist for new id after rename");
+    // old collection should not exist
+    assertFalse(
+        collectionService.exists(oldCollectionId),
+        "Collection row should not exist for original id after rename");
+    // new collection should exist
+    assertTrue(
+        collectionService.exists(newCollectionId),
+        "Collection row should exist for new id after rename");
+
+    // name and description should not have changed
+    CollectionServerModel newColl = collectionService.get(workspaceId, newCollectionId);
+    assertEquals(oldColl.getName(), newColl.getName());
+    assertEquals(oldColl.getDescription(), newColl.getDescription());
+  }
+
+  private boolean postgresSchemaExists(CollectionId collectionId) {
+    // attempt to create the sc
+    return namedTemplate.queryForObject(
+        "select exists(select schema_name from information_schema.schemata where schema_name = :collectionId);",
+        Map.of("collectionId", collectionId.toString()),
+        Boolean.class);
   }
 }
