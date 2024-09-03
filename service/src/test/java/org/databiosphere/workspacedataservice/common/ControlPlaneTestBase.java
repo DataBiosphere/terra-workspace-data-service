7402f06b6b6e21ce96e84553d6fa6cf112f3f85f
David An <davidan@broadinstitute.org>
2024-09-03 15:52:11 -0400

    ControlPlaneTestBase, and migrate one test to it

---
+++ service/src/test/java/org/databiosphere/workspacedataservice/common/ControlPlaneTestBase.java
@@ -0,0 +1,35 @@
+package org.databiosphere.workspacedataservice.common;
+
+import org.databiosphere.workspacedataservice.config.ConfigurationException;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * Optional base class for any test class annotated with {@link SpringBootTest} to automatically
+ * enable the "control-plane" profile.
+ *
+ * <p>Subclasses can append additional active profiles by using the {@link ActiveProfiles}
+ * annotation.
+ *
+ * <p>To explicitly override the "control-plane" profile, subclasses can use
+ * {@code @ActiveProfiles(value={"some-profile"}, inheritProfiles=false)}.
+ *
+ * <p>Note: extending this class also extends the {@link ConfigurationExceptionDetector} class,
+ * which will automatically provide a hint if it detects a test failure due to a {@link
+ * ConfigurationException}.
+ */
+@ActiveProfiles({"control-plane"})
+@TestPropertySource(
+    properties = {
+      // turn off pubsub autoconfiguration for tests
+      "spring.cloud.gcp.pubsub.enabled=false",
+      // aggressive retry settings so unit tests don't run too long
+      "rest.retry.maxAttempts=2",
+      "rest.retry.backoff.delay=3",
+      // Rawls url must be valid, else context initialization (Spring startup) will fail
+      "rawlsUrl=https://localhost/"
+    })
+@ExtendWith(ConfigurationExceptionDetector.class)
+public abstract class ControlPlaneTestBase {}
