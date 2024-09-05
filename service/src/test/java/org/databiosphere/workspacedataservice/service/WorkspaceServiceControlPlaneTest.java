0d4ec99be6e2e521aff5d92f23849161b254b9f9
Kevin Marete <kevinmmarete@gmail.com>
2024-09-04 23:36:19 -0400

    Added initSystemWorkspace tests

--- service/src/test/java/org/databiosphere/workspacedataservice/service/WorkspaceServiceControlPlaneTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/service/WorkspaceServiceControlPlaneTest.java
@@ -1,18 +1,27 @@
 package org.databiosphere.workspacedataservice.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
+import org.databiosphere.workspacedataservice.dao.JobDao;
+import org.databiosphere.workspacedataservice.dao.WorkspaceRepository;
 import org.databiosphere.workspacedataservice.rawls.RawlsClient;
 import org.databiosphere.workspacedataservice.rawls.RawlsWorkspaceDetails;
 import org.databiosphere.workspacedataservice.shared.model.WorkspaceId;
+import org.databiosphere.workspacedataservice.workspace.DataTableTypeInspector;
 import org.databiosphere.workspacedataservice.workspace.WorkspaceDataTableType;
+import org.databiosphere.workspacedataservice.workspace.WorkspaceRecord;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -34,6 +43,8 @@ class WorkspaceServiceControlPlaneTest {
 
   @MockBean RawlsClient rawlsClient;
   @Autowired WorkspaceService workspaceService;
+  @Mock private WorkspaceRepository workspaceRepository;
+  @Mock private DataTableTypeInspector dataTableTypeInspector;
 
   static Stream<Arguments> workspaceTypeArguments() {
     return Stream.of(
@@ -60,5 +71,65 @@ class WorkspaceServiceControlPlaneTest {
 
     WorkspaceDataTableType actual = workspaceService.getDataTableType(workspaceId);
     assertEquals(dataTableType, actual);
+  }
+
+  @ParameterizedTest(name = "Initialize `{0}` system workspace for `{1}-backed` data tables")
+  @MethodSource("workspaceTypeArguments")
+  void testInitSystemWorkspace_WhenWorkspaceNotExists(
+      RawlsWorkspaceDetails.RawlsWorkspace.WorkspaceType workspaceType,
+      WorkspaceDataTableType dataTableType) {
+
+    JobDao mockJobDao = Mockito.mock(JobDao.class);
+    CollectionService mockCollectionService = Mockito.mock(CollectionService.class);
+
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
+    RawlsWorkspaceDetails.RawlsWorkspace rawlsWorkspace =
+        new RawlsWorkspaceDetails.RawlsWorkspace("bucketName", workspaceType);
+    RawlsWorkspaceDetails rawlsWorkspaceDetails =
+        new RawlsWorkspaceDetails(rawlsWorkspace, List.of());
+    WorkspaceRecord workspaceRecord = new WorkspaceRecord(workspaceId, dataTableType, true);
+
+    when(rawlsClient.getWorkspaceDetails(workspaceId.id())).thenReturn(rawlsWorkspaceDetails);
+    when(workspaceRepository.findById(workspaceId)).thenReturn(Optional.empty());
+    when(dataTableTypeInspector.getWorkspaceDataTableType(workspaceId)).thenReturn(dataTableType);
+
+    WorkspaceService mockWorkspaceService =
+        new WorkspaceService(
+            mockJobDao, mockCollectionService, dataTableTypeInspector, workspaceRepository);
+
+    mockWorkspaceService.initSystemWorkspace(workspaceId);
+
+    verify(workspaceRepository).save(workspaceRecord); // verify that workspaceRecord is saved
+  }
+
+  @ParameterizedTest(
+      name = "Ignore Initializing `{0}` system workspace for existing `{1}-backed` data tables")
+  @MethodSource("workspaceTypeArguments")
+  void testInitSystemWorkspace_WhenWorkspaceExists(
+      RawlsWorkspaceDetails.RawlsWorkspace.WorkspaceType workspaceType,
+      WorkspaceDataTableType dataTableType) {
+
+    JobDao mockJobDao = Mockito.mock(JobDao.class);
+    CollectionService mockCollectionService = Mockito.mock(CollectionService.class);
+
+    WorkspaceId workspaceId = WorkspaceId.of(UUID.randomUUID());
+    RawlsWorkspaceDetails.RawlsWorkspace rawlsWorkspace =
+        new RawlsWorkspaceDetails.RawlsWorkspace("bucketName", workspaceType);
+    RawlsWorkspaceDetails rawlsWorkspaceDetails =
+        new RawlsWorkspaceDetails(rawlsWorkspace, List.of());
+    WorkspaceRecord workspaceRecord = new WorkspaceRecord(workspaceId, dataTableType, true);
+
+    when(rawlsClient.getWorkspaceDetails(workspaceId.id())).thenReturn(rawlsWorkspaceDetails);
+    when(workspaceRepository.findById(workspaceId)).thenReturn(Optional.of(workspaceRecord));
+    when(dataTableTypeInspector.getWorkspaceDataTableType(workspaceId)).thenReturn(dataTableType);
+
+    WorkspaceService mockWorkspaceService =
+        new WorkspaceService(
+            mockJobDao, mockCollectionService, dataTableTypeInspector, workspaceRepository);
+
+    mockWorkspaceService.initSystemWorkspace(workspaceId);
+
+    verify(workspaceRepository, never())
+        .save(workspaceRecord); // verify that workspaceRecord is not saved
   }
 }
