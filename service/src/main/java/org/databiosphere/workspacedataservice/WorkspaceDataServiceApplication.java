346fc9d32026e075e0c30ad86598fb4c6c2459d0
David An <davidan@broadinstitute.org>
2025-02-04 11:51:37 -0500

    remove application insights

--- service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/WorkspaceDataServiceApplication.java
@@ -1,9 +1,5 @@
 package org.databiosphere.workspacedataservice;
 
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -25,18 +21,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class WorkspaceDataServiceApplication {
 
   public static void main(String[] args) {
-    startApplicationInsights();
     SpringApplication.run(WorkspaceDataServiceApplication.class, args);
-  }
-
-  private static void startApplicationInsights() {
-    try {
-      if (StringUtils.isNotBlank(System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"))) {
-        ApplicationInsights.attach();
-      }
-    } catch (Exception e) {
-      Logger logger = LoggerFactory.getLogger(WorkspaceDataServiceApplication.class);
-      logger.warn("Error while attempting to start Application Insights: {}", e.getMessage());
-    }
   }
 }
