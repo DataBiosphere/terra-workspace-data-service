657776314d904157424b70031d602909b60c38aa
David An <davidan@broadinstitute.org>
2025-02-07 11:29:46 -0500

    remove backup/restore APIs

--- service/src/test/java/org/databiosphere/workspacedataservice/annotations/DeploymentModeTestBase.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/annotations/DeploymentModeTestBase.java
@@ -35,7 +35,6 @@ class DeploymentModeTestBase extends DataPlaneTestBase {
         "collectionInitializerBean",
         "wsmProtectedDataSupport",
         "capabilitiesController",
-        "cloningController",
         "collectionController",
         "recordController");
   }
--- service/src/test/java/org/databiosphere/workspacedataservice/controller/AllControllersPermissionsTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/controller/AllControllersPermissionsTest.java
@@ -244,26 +244,6 @@ class AllControllersPermissionsTest extends DataPlaneTestBase {
                     "myattribute"))));
   }
 
-  // these single-tenant APIs require read permission. When single-tenant APIs are fully retired,
-  // this method and any tests that use it should be deleted.
-  private static Stream<Arguments> singleTenantReadOnlyApis() {
-    return Stream.of(
-        // Cloning
-        arguments(
-            named(
-                "POST /backup/v0.2",
-                post("/backup/v0.2")
-                    .content(
-                        "{\"requestingWorkspaceId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\","
-                            + "  \"description\": \"string\"}")
-                    .contentType(MediaType.APPLICATION_JSON))),
-        arguments(
-            named(
-                "GET /backup/v0.2/{trackingId}",
-                get("/backup/v0.2/{trackingId}", UUID.randomUUID()))),
-        arguments(named("GET /clone/v0.2", get("/clone/v0.2"))));
-  }
-
   // ========== write API tests
 
   @ParameterizedTest(name = "write API {0} succeeds with write permission")
@@ -307,30 +287,6 @@ class AllControllersPermissionsTest extends DataPlaneTestBase {
   @MethodSource("readOnlyApis")
   void readApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
     userNoPermissions(workspaceId);
-    requestShouldBeMaskedNotFound(request);
-  }
-
-  // ========== single-tenant read-only API tests
-
-  @ParameterizedTest(name = "single-tenant read-only API {0} succeeds with write permission")
-  @MethodSource("singleTenantReadOnlyApis")
-  void singleTenantReadApiWritePermission(MockHttpServletRequestBuilder request) throws Exception {
-    userCanWrite(twdsProperties.workspaceId());
-    requestShouldSucceed(request);
-  }
-
-  @ParameterizedTest(name = "single-tenant read-only API {0} succeeds with read-only permission")
-  @MethodSource("singleTenantReadOnlyApis")
-  void singleTenantReadApiReadOnlyPermission(MockHttpServletRequestBuilder request)
-      throws Exception {
-    userCanRead(twdsProperties.workspaceId());
-    requestShouldSucceed(request);
-  }
-
-  @ParameterizedTest(name = "single-tenant read-only API {0} fails with no permission")
-  @MethodSource("singleTenantReadOnlyApis")
-  void singleTenantReadApiNoPermission(MockHttpServletRequestBuilder request) throws Exception {
-    userNoPermissions(twdsProperties.workspaceId());
     requestShouldBeMaskedNotFound(request);
   }
 
