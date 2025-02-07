90a7d4b7dfa56ea4ab0476c444428288ecb219fb
David An <davidan@broadinstitute.org>
2025-02-07 13:35:17 -0500

    remove leonardo dao

--- service/src/main/java/org/databiosphere/workspacedataservice/leonardo/LeonardoClientFactory.java
+++
@@ -1,7 +0,0 @@
-package org.databiosphere.workspacedataservice.leonardo;
-
-import org.broadinstitute.dsde.workbench.client.leonardo.api.AppsApi;
-
-public interface LeonardoClientFactory {
-  AppsApi getAppsV2Api(String token);
-}
--- service/src/main/java/org/databiosphere/workspacedataservice/leonardo/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package org.databiosphere.workspacedataservice.leonardo;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
