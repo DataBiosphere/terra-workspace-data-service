3858052506d9ee9a79a8a7c2972aac64360cc8bd
Kevin Marete <kevinmmarete@gmail.com>
2025-02-12 16:44:41 -0500

    Remove unused DrsHubException constructor

--- service/src/main/java/org/databiosphere/workspacedataservice/drshub/DrsHubException.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/drshub/DrsHubException.java
@@ -1,17 +1,10 @@
 package org.databiosphere.workspacedataservice.drshub;
 
-import com.google.common.annotations.VisibleForTesting;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.server.ResponseStatusException;
 
 public class DrsHubException extends ResponseStatusException {
   public DrsHubException(RestClientResponseException cause) {
     super(cause.getStatusCode(), cause.getMessage(), cause);
-  }
-
-  @VisibleForTesting
-  public DrsHubException(HttpStatusCode statusCode, String message) {
-    super(statusCode, message);
   }
 }
