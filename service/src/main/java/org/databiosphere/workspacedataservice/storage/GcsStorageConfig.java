a3b550373a0b3431c99aa8e95afab1d3efc6d633
David An <davidan@broadinstitute.org>
2025-02-18 11:12:57 -0500

    python tests to control plane

--- service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/storage/GcsStorageConfig.java
@@ -2,23 +2,23 @@ package org.databiosphere.workspacedataservice.storage;
 
 import com.google.cloud.spring.core.GcpProjectIdProvider;
 import java.io.IOException;
-import org.databiosphere.workspacedataservice.annotations.DeploymentMode.ControlPlane;
-import org.databiosphere.workspacedataservice.annotations.DeploymentMode.DataPlane;
 import org.databiosphere.workspacedataservice.config.DataImportProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GcsStorageConfig {
 
-  @ControlPlane
+  @ConditionalOnProperty(name = "spring.cloud.gcp.core.enabled", havingValue = "true")
   @Bean
   public GcsStorage gcsStorage(
       GcpProjectIdProvider projectIdProvider, DataImportProperties properties) throws IOException {
     return GcsStorageImpl.create(projectIdProvider.getProjectId(), properties);
   }
 
-  @DataPlane
+  // to support Python tests
+  @ConditionalOnProperty(name = "spring.cloud.gcp.core.enabled", havingValue = "false")
   @Bean
   public GcsStorage noopGcsStorage() {
     return new GcsStorageNoopImpl();
