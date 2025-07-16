17fb251880afd56b34f6c009c6fcbfb43bbb26da
Bria Morgan <bmorgan@broadinstitute.org>
2025-07-16 15:18:16 -0400

    revert

--- service/src/main/java/org/databiosphere/workspacedataservice/sam/SamAuthorizationDao.java
+++ service/src/main/java/org/databiosphere/workspacedataservice/sam/SamAuthorizationDao.java
@@ -2,22 +2,22 @@ package org.databiosphere.workspacedataservice.sam;
 
 public interface SamAuthorizationDao {
   /** Sam resource type name for Workspaces */
-  String RESOURCE_NAME_WORKSPACE = "workspace";
+  public static final String RESOURCE_NAME_WORKSPACE = "workspace";
 
   /** Sam action name for write permission */
-  String ACTION_WRITE = "write";
+  public static final String ACTION_WRITE = "write";
 
   /** Sam action name for read permission */
-  String ACTION_READ = "read";
+  public static final String ACTION_READ = "read";
 
   /** Sam policy name for read permission */
-  String READER_POLICY_NAME = "reader";
+  public static final String READER_POLICY_NAME = "reader";
 
   /* Resource type to specify when adding Sam member policies */
-  String RESOURCE_NAME_TDR_SNAPSHOT = "datasnapshot";
+  public static final String RESOURCE_NAME_TDR_SNAPSHOT = "datasnapshot";
 
   /** All roles to add reader policy */
-  String[] WORKSPACE_ROLES = {"reader", "writer", "owner", "project-owner"};
+  public static final String[] WORKSPACE_ROLES = {"reader", "writer", "owner", "project-owner"};
 
   /**
    * Check if the current user has permission to read the workspace resource from Sam
