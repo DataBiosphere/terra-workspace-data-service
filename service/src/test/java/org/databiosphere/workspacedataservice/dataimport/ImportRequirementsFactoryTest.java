6476949f7ff2de695cfc756bcd70527936cf6e21
Bria Morgan <bmorgan@broadinstitute.org>
2025-08-06 11:32:02 -0400

    [CORE-656] Add new bdc urls (#1045)

--- service/src/test/java/org/databiosphere/workspacedataservice/dataimport/ImportRequirementsFactoryTest.java
+++ service/src/test/java/org/databiosphere/workspacedataservice/dataimport/ImportRequirementsFactoryTest.java
@@ -62,10 +62,10 @@ class ImportRequirementsFactoryTest extends ControlPlaneTestBase {
     return Stream.of(
         Arguments.of(
             /* importUri */ URI.create("https://storage.googleapis.com/test-bucket/file.pfb"),
-            /* shouldReuqireProtectedDataPolicy */ false),
+            /* shouldRequireProtectedDataPolicy */ false),
         Arguments.of(
             /* importUri */ URI.create("https://files.terra.bio/file.pfb"),
-            /* shouldReuqireProtectedDataPolicy */ false));
+            /* shouldRequireProtectedDataPolicy */ false));
   }
 
   /*
@@ -97,6 +97,10 @@ class ImportRequirementsFactoryTest extends ControlPlaneTestBase {
         Arguments.of(
             /* importUri */ URI.create(
                 "https://s3.amazonaws.com/gen3-theanvil-io-pfb-export/file.pfb"),
+            /* shouldRequirePrivateWorkspace/ProtectedDataPolicy */ true),
+        Arguments.of(
+            /* importUri */ URI.create(
+                "https://nih-nhlbi-topmed-released-phs000810-abc.amazonaws.com/parent-EFG_-phs000810-hij2.avro"),
             /* shouldRequirePrivateWorkspace/ProtectedDataPolicy */ true));
   }
 }
